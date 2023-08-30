77142b06dca43f96eeef1aa2e28148b8602ebf40
Michael <michael.hladky@push-based.io>
2023-08-31 01:17:37 +0200

    refactor(models): format

--- packages/models/src/lib/persist.ts
+++ packages/models/src/lib/persist.ts
@@ -1,4 +1,4 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
 /**
  * Define Zod schema for the PersistConfig type
--- packages/models/src/lib/plugins.ts
+++ packages/models/src/lib/plugins.ts
@@ -1,4 +1,4 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
 // Define Zod schema for the PluginMetadata type
 const pluginMetadataSchema = z.object(
