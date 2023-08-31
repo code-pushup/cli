2e23aeb0afbdfe4e19b13fc815bc993cc74435e6
Michael <michael.hladky@push-based.io>
2023-08-31 15:10:27 +0200

    refactor(models): remove budgets from initial code base

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -7,7 +7,6 @@ export {
   categoryConfigSchema,
   CategoryConfigSchema,
 } from './lib/category-config';
-export { budgetSchema, BudgetSchema } from './lib/budgets';
 export {
   globalCliArgsSchema,
   GlobalCliArgsSchema,
