d52de863da59f610325e93d88ca7b0dfa348b552
Michael <michael.hladky@push-based.io>
2023-09-05 01:55:11 +0200

    refactor(models): format files

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,7 +1,7 @@
 export { coreConfigSchema, CoreConfigSchema } from './lib/core-config';
 export { uploadConfigSchema, UploadConfigSchema } from './lib/upload';
 export { pluginConfigSchema, PluginConfigSchema } from './lib/plugins';
-export { runnerOutputSchema, RunnerOutputSchema, runnerOutputAuditRefsPresentInPluginConfigs } from './lib/runner-output';
+export { runnerOutputSchema, RunnerOutputSchema, runnerOutputAuditRefsPresentInPluginConfigs, PluginsOutputSchema } from './lib/output';
 export { persistConfigSchema, PersistConfigSchema } from './lib/persist';
 export {
   categoryConfigSchema,
