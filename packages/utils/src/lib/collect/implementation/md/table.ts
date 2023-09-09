a927e293459af76a99595c8564bbbb6511dbbb20
Michael <michael.hladky@push-based.io>
2023-09-09 09:30:27 +0200

    fix(cli): fix md table

--- packages/utils/src/lib/collect/implementation/md/table.ts
+++ packages/utils/src/lib/collect/implementation/md/table.ts
@@ -1,5 +1,3 @@
-import { formatCode } from '../prettier';
-
 export type Alignment = 'l' | 'c' | 'r';
 const alignString = new Map<Alignment, string>([
   ['l', ':--'],
@@ -21,8 +19,5 @@ export function table(
   align = align || data[0]?.map(() => 'c');
   const _data = data.map(arr => arr.join('|'));
   const secondRow = align?.map(s => alignString.get(s)).join('|');
-  return formatCode(
-    _data.shift() + '\n' + secondRow + '\n' + _data.join('\n'),
-    'markdown',
-  );
+  return _data.shift() + '\n' + secondRow + '\n' + _data.join('\n')
 }
