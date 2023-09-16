fc79be244ddfc864c3af3085855165dee9aa648a
Michael <michael.hladky@push-based.io>
2023-09-16 16:25:39 -0600

    refactor(nx-plugin): extend vitest InitGenerator

--- packages/nx-plugin/src/generators/init/generator.ts
+++ packages/nx-plugin/src/generators/init/generator.ts
@@ -1,22 +1,74 @@
 import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
+  addDependenciesToPackageJson,
+  convertNxGenerator,
+  logger,
+  readJson,
+  readNxJson,
+  runTasksInSerial,
   Tree,
+  updateJson,
+  updateNxJson,
 } from '@nx/devkit';
-import * as path from 'path';
-import { InitGeneratorSchema } from './schema';
+import { initGenerator as viteInitGenerator } from '@nx/vite';
 
-export async function initGenerator(tree: Tree, options: InitGeneratorSchema) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+import { InitGeneratorSchema } from './schema';
+import {NxJsonConfiguration} from "nx/src/config/nx-json";
+
+function checkDependenciesInstalled(host: Tree, schema: InitGeneratorSchema) {
+  const packageJson = readJson(host, 'package.json');
+  const devDependencies = {};
+  const dependencies = {};
+  packageJson.dependencies = packageJson.dependencies || {};
+  packageJson.devDependencies = packageJson.devDependencies || {};
+
+  return addDependenciesToPackageJson(host, dependencies, devDependencies);
+}
+
+function moveToDevDependencies(tree: Tree) {
+  updateJson(tree, 'package.json', (packageJson) => {
+    packageJson.dependencies = packageJson.dependencies || {};
+    packageJson.devDependencies = packageJson.devDependencies || {};
+
+    return packageJson;
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
-  await formatFiles(tree);
+}
+
+export function createCodePushupConfig(tree: Tree) {
+  const nxJson: NxJsonConfiguration = readNxJson(tree) as NxJsonConfiguration;
+
+  nxJson.targetDefaults ??= {};
+  nxJson.namedInputs ??= {};
+
+  const productionFileSet = nxJson.namedInputs.production;
+  if (productionFileSet) {
+    productionFileSet.push(
+      '!{projectRoot}/**/?(*.)+(spec|test).[jt]s?(x)?(.snap)',
+      '!{projectRoot}/tsconfig.spec.json'
+    );
+
+    nxJson.namedInputs.production = Array.from(new Set(productionFileSet));
+  }
+
+  updateNxJson(tree, nxJson);
+}
+
+export async function initGenerator(tree: Tree, schema: InitGeneratorSchema) {
+  logger.info('code pushup initGenerator');
+  moveToDevDependencies(tree);
+  createCodePushupConfig(tree);
+  const tasks = [];
+
+  tasks.push(
+    await viteInitGenerator(tree, {
+      ...schema,
+      testEnvironment: 'node',
+      uiFramework: 'none'
+    })
+  );
+
+  tasks.push(checkDependenciesInstalled(tree, schema));
+  return runTasksInSerial(...tasks);
 }
 
 export default initGenerator;
+export const initSchematic = convertNxGenerator(initGenerator);
