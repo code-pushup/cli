4aa53ea3df5e3eea16a79f35984553fcb14ea040
Michael <michael.hladky@push-based.io>
2023-09-17 23:55:14 -0500

    feat(utils): add objectToCliArgs helper for plugin authors

--- packages/utils/src/lib/collect/implementation/execute-process.ts
+++ packages/utils/src/lib/collect/implementation/execute-process.ts
@@ -166,3 +166,38 @@ export function executeProcess(cfg: ProcessConfig): Promise<ProcessResult> {
     });
   });
 }
+
+/**
+ * Converts an object with different types of values into an array of command-line arguments.
+ *
+ * @example
+ * const args = objectToProcessArgs({
+ *   name: 'Juanita', // --name=Juanita
+ *   interactive: false, // --no-interactive
+ *   parallel: 5, // --parallel=5
+ *   formats: ['json', 'md'] // --format=json --format=md
+ * });
+ */
+export function objectToCliArgs(
+  params: Record<string, number | string | boolean | string[]>,
+): string[] {
+  return Object.entries(params).flatMap(([key, value]) => {
+    if (Array.isArray(value)) {
+      return value.map(v => `--${key}="${v}"`);
+    }
+
+    if (typeof value === 'string') {
+      return [`--${key}="${value}"`];
+    }
+
+    if (typeof value === 'number') {
+      return [`--${key}=${value}`];
+    }
+
+    if (typeof value === 'boolean') {
+      return [`--${value ? '' : 'no-'}${key}`];
+    }
+
+    throw new Error(`Unsupported type ${typeof value} for key ${key}`);
+  });
+}
