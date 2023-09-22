7c78d3029e688fe72ede4e2fa3fac92c9a6191bd
Michael <michael.hladky@push-based.io>
2023-09-22 14:46:14 -0700

    merge main 2

--- packages/utils/src/lib/collect/implementation/utils.ts
+++ packages/utils/src/lib/collect/implementation/utils.ts
@@ -8,13 +8,13 @@ export const reportOverviewTableHeaders = ['Category', 'Score', 'Audits'];
 
 // @TODO replace with real scoring logic
 export function sumRefs(refs: CategoryConfig['refs']) {
-  return refs.reduce((sum, { weight }) => sum + weight, refs.length);
+  return refs.reduce((sum, { weight }) => sum + weight, 0);
 }
 
 export function countWeightedRefs(refs: CategoryConfig['refs']) {
   return refs
     .filter(({ weight }) => weight > 0)
-    .reduce((sum, { weight }) => sum + weight, refs.length);
+    .reduce((sum, { weight }) => sum + weight, 0);
 }
 
 export class ReadPackageJsonError extends Error {
@@ -60,3 +60,7 @@ export function formatBytes(bytes: number, decimals = 2) {
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
 
+export function calcDuration(start: number, stop?: number): number {
+  stop = stop !== undefined ? stop : performance.now();
+  return Math.floor(stop - start);
+}
