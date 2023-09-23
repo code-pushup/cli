c5ab30853f33138bc1a286bd074d62b7ec01bbee
Michael <michael.hladky@push-based.io>
2023-09-22 18:06:45 -0700

    refactor(utils): add organization and project to core config

--- packages/models/test/schema.mock.ts
+++ packages/models/test/schema.mock.ts
@@ -251,6 +251,8 @@ export function mockUploadConfig(opt?: Partial<UploadConfig>): UploadConfig {
   return {
     apiKey: 'm0ck-API-k3y',
     server: 'http://test.server.io',
+    organization: 'code-pushup',
+    project: 'cli',
     ...opt,
   };
 }
