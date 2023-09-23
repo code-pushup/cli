9e98e659a714aaa167283a8da3dc071b81920431
Michael <michael.hladky@push-based.io>
2023-09-22 23:21:02 -0700

    feat(utils): add upload logic

---
+++ packages/utils/src/lib/upload/index.ts
@@ -0,0 +1,38 @@
+import { CoreConfig, GlobalCliArgs } from '@quality-metrics/models';
+import {
+  uploadToPortal,
+  MutationSaveReportArgs,
+  ReportFragment,
+} from '@code-pushup/portal-client';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+export type UploadOptions = GlobalCliArgs & CoreConfig;
+
+/**
+ * Uploads collected audits to the portal
+ * @param options
+ */
+export async function upload(options: UploadOptions): Promise<ReportFragment> {
+  if (options?.upload === undefined) {
+    throw new Error('upload config needs to be set');
+  }
+
+  const { apiKey, server, organization, project } = options.upload;
+  const { outputPath } = options.persist;
+
+  const report = JSON.parse(
+    readFileSync(join(outputPath, 'report.json')).toString(),
+  );
+
+  const data: MutationSaveReportArgs = {
+    ...report,
+    organization,
+    project,
+    commit: '',
+  };
+
+  return uploadToPortal({ apiKey, server, data }).catch(e => {
+    throw new Error('upload failed. ' + e.message);
+  });
+}
