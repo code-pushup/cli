f06b623ce35bd4cabf3c6fdb260f5d47a036fb0a
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2023-09-23 10:07:57 -0700

    Update packages/utils/src/lib/collect/implementation/utils.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/utils/src/lib/collect/implementation/utils.ts
+++ packages/utils/src/lib/collect/implementation/utils.ts
@@ -52,7 +52,7 @@ export function formatBytes(bytes: number, decimals = 2) {
 
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const sizes = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
