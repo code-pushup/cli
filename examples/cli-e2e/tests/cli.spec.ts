7b3e69ce5c09c3d62913cd989977ce23386da4d6
Matěj Chalk <matej.chalk@flowup.cz>
2023-09-27 12:51:15 +0200

    test(cli-e2e): import plugin packages so Nx builds them before e2e

--- examples/cli-e2e/tests/cli.spec.ts
+++ examples/cli-e2e/tests/cli.spec.ts
@@ -1,35 +1,37 @@
-import { describe, it, expect, beforeAll } from 'vitest';
 import { cli } from '@quality-metrics/cli';
-import { execSync } from 'child_process';
+import eslintPlugin from '@quality-metrics/eslint-plugin';
+import lighthousePlugin from '@quality-metrics/lighthouse-plugin';
 import { join } from 'path';
+import { describe, expect, it } from 'vitest';
 
 const configFile = (ext: 'ts' | 'js' | 'mjs') =>
   join(process.cwd(), `examples/cli-e2e/mocks/config.mock.${ext}`);
 
 describe('cli', () => {
-  beforeAll(() => {
-    // symlink NPM workspaces
-    execSync('npm install');
-  });
-
   it('should load .js config file', async () => {
     const argv = await cli(['--configPath', configFile('js'), '--verbose'])
       .argv;
-    expect(argv.plugins[0].slug).toEqual('eslint');
-    expect(argv.plugins[1].slug).toEqual('lighthouse');
+    expect(argv.plugins[0]).toEqual(eslintPlugin({ config: '.eslintrc.json' }));
+    expect(argv.plugins[1]).toEqual(
+      lighthousePlugin({ config: '.lighthouserc.json' }),
+    );
   });
 
   it('should load .mjs config file', async () => {
     const argv = await cli(['--configPath', configFile('mjs'), '--verbose'])
       .argv;
-    expect(argv.plugins[0].slug).toEqual('eslint');
-    expect(argv.plugins[1].slug).toEqual('lighthouse');
+    expect(argv.plugins[0]).toEqual(eslintPlugin({ config: '.eslintrc.json' }));
+    expect(argv.plugins[1]).toEqual(
+      lighthousePlugin({ config: '.lighthouserc.json' }),
+    );
   });
 
   it('should load .ts config file', async () => {
     const argv = await cli(['--configPath', configFile('ts'), '--verbose'])
       .argv;
-    expect(argv.plugins[0].slug).toEqual('eslint');
-    expect(argv.plugins[1].slug).toEqual('lighthouse');
+    expect(argv.plugins[0]).toEqual(eslintPlugin({ config: '.eslintrc.json' }));
+    expect(argv.plugins[1]).toEqual(
+      lighthousePlugin({ config: '.lighthouserc.json' }),
+    );
   });
 });
