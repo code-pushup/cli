2a2e36831955051c1333ff26b4cd77c5cca5ebc5
Matěj Chalk <matej.chalk@flowup.cz>
2023-10-05 13:23:30 +0200

    test(plugin-eslint): add test case for Nx monorepo project

--- packages/plugin-eslint/src/lib/eslint-plugin.spec.ts
+++ packages/plugin-eslint/src/lib/eslint-plugin.spec.ts
@@ -3,30 +3,39 @@ import { fileURLToPath } from 'url';
 import type { SpyInstance } from 'vitest';
 import { eslintPlugin } from './eslint-plugin';
 
-const appRootDir = join(
-  fileURLToPath(dirname(import.meta.url)),
-  '..',
-  '..',
-  'test',
-  'fixtures',
-  'todos-app',
-);
-
 describe('eslintPlugin', () => {
+  const fixturesDir = join(
+    fileURLToPath(dirname(import.meta.url)),
+    '..',
+    '..',
+    'test',
+    'fixtures',
+  );
+
   let cwdSpy: SpyInstance;
 
   beforeAll(() => {
-    cwdSpy = vi.spyOn(process, 'cwd').mockReturnValue(appRootDir);
+    cwdSpy = vi.spyOn(process, 'cwd');
   });
 
   afterAll(() => {
     cwdSpy.mockRestore();
   });
 
-  it('should initialize ESLint plugin', async () => {
+  it('should initialize ESLint plugin for React application', async () => {
+    cwdSpy.mockReturnValue(join(fixturesDir, 'todos-app'));
     const plugin = await eslintPlugin({
       eslintrc: '.eslintrc.js',
       patterns: ['src/**/*.js', 'src/**/*.jsx'],
+    });
+    expect(plugin).toMatchSnapshot();
+  });
+
+  it('should initialize ESLint plugin for Nx project', async () => {
+    cwdSpy.mockReturnValue(join(fixturesDir, 'nx-monorepo'));
+    const plugin = await eslintPlugin({
+      eslintrc: './packages/utils/.eslintrc.json',
+      patterns: ['packages/utils/**/*.ts', 'packages/utils/**/*.json'],
     });
     expect(plugin).toMatchSnapshot();
   });
