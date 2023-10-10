9b0a2ca0b7979e9a9b9d1803142f1b4846f41bd4
Matěj Chalk <matej.chalk@flowup.cz>
2023-10-10 14:53:01 +0200

    test(cli-e2e): fix E2E tests - comment out WIP ESLint plugin, fix Lighthouse stub

--- examples/cli-e2e/mocks/code-pushup.config.mjs
+++ examples/cli-e2e/mocks/code-pushup.config.mjs
@@ -1,12 +1,13 @@
-// TODO: import plugins as NPM package names using local registry: https://github.com/flowup/quality-metrics-cli/issues/33
-import eslintPlugin from '../../../dist/packages/plugin-eslint';
+// TODO: import plugins using NPM package names using local registry: https://github.com/flowup/quality-metrics-cli/issues/33
+// import eslintPlugin from '../../../dist/packages/plugin-eslint';
 import lighthousePlugin from '../../../dist/packages/plugin-lighthouse';
 
 export default {
   persist: { outputPath: 'tmp/cli-config-out.json' },
   categories: [],
   plugins: [
-    await eslintPlugin({ eslintrc: '.eslintrc.json', patterns: '**/*.ts' }),
+    // TODO: uncomment once runner is implemented
+    // await eslintPlugin({ eslintrc: '.eslintrc.json', patterns: '**/*.ts' }),
     lighthousePlugin({ config: '.lighthouserc.json' }),
   ],
 };
--- examples/cli-e2e/tests/cli.spec.ts
+++ examples/cli-e2e/tests/cli.spec.ts
@@ -1,31 +1,33 @@
-import { CliArgsObject, objectToCliArgs } from '@code-pushup/utils';
-import { execSync } from 'child_process';
+import {
+  CliArgsObject,
+  executeProcess,
+  objectToCliArgs,
+} from '@code-pushup/utils';
 import { join } from 'path';
 
 const configFile = (ext: 'ts' | 'js' | 'mjs') =>
   join(process.cwd(), `examples/cli-e2e/mocks/code-pushup.config.${ext}`);
 
-const execCli = async (argObj: CliArgsObject) => {
-  await execSync(
-    'node ' +
-      objectToCliArgs({
-        _: './dist/packages/cli/index.js',
-        verbose: true,
-        ...argObj,
-      }).join(' '),
-  );
-};
+const execCli = (argObj: Partial<CliArgsObject>) =>
+  executeProcess({
+    command: 'node',
+    args: objectToCliArgs({
+      _: './dist/packages/cli/index.js',
+      verbose: true,
+      ...argObj,
+    }),
+  });
 
 describe('cli', () => {
   it('should load .js config file', async () => {
-    const argv = await execCli({ configPath: configFile('js') });
+    await execCli({ configPath: configFile('js') });
   });
 
   it('should load .mjs config file', async () => {
-    const argv = await execCli({ configPath: configFile('mjs') });
+    await execCli({ configPath: configFile('mjs') });
   });
 
   it('should load .ts config file', async () => {
-    const argv = await execCli({ configPath: configFile('ts') });
+    await execCli({ configPath: configFile('ts') });
   });
 });
