63c71acae87554996c1528c7a1b5fb3535533205
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2023-10-11 10:13:31 +0200

    Update packages/models/src/lib/persist-config.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/models/src/lib/persist-config.ts
+++ packages/models/src/lib/persist-config.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { generalFilePathSchema } from './implementation/schemas';
 
 export const formatSchema = z.enum(['json', 'stdout', 'md']);
-export type Format = 'json' | 'stdout' | 'md';
+export type Format = z.infer<typeof formatSchema>;
 
 export const persistConfigSchema = z.object({
   outputPath: generalFilePathSchema('Artifacts folder'),
