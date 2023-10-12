27e666c6a8becf828f86429c61309c0f164e3479
Matěj Chalk <matej.chalk@flowup.cz>
2023-10-12 16:03:05 +0200

    chore(react-todos-app): generate example React application with Nx

---
+++ examples/react-todos-app/src/app/app.tsx
@@ -0,0 +1,12 @@
+import './app.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-todos-app" />
+    </div>
+  );
+}
+
+export default App;
