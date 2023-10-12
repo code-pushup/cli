0fef0c3b784454a2ab2d9e0fb65f132e8ee8e196
Matěj Chalk <matej.chalk@flowup.cz>
2023-10-12 17:21:10 +0200

    fix(core): include package.json data

--- packages/core/src/lib/collect-and-persist.ts
+++ packages/core/src/lib/collect-and-persist.ts
@@ -1,11 +1,16 @@
-import { pluginReportSchema } from '@code-pushup/models';
+import { Report, pluginReportSchema } from '@code-pushup/models';
+import { name, version } from '../../package.json';
 import { CollectOptions, collect } from './commands/collect';
 import { logPersistedResults, persistReport } from './implementation/persist';
 
 export async function collectAndPersistReports(
   config: CollectOptions,
 ): Promise<void> {
-  const report = await collect(config);
+  const report: Report = {
+    packageName: name,
+    version,
+    ...(await collect(config)),
+  };
 
   const persistResults = await persistReport(report, config);
   logPersistedResults(persistResults);
