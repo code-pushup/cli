a48aa0224de2296416cb2e79671e018dcf2abacf
Matěj Chalk <matej.chalk@flowup.cz>
2023-10-13 10:59:48 +0200

    test(cli-e2e): add categories to example app in E2E tests

--- e2e/cli-e2e/tests/collect.spec.ts
+++ e2e/cli-e2e/tests/collect.spec.ts
@@ -1,20 +1,24 @@
 import { PluginReport, Report, reportSchema } from '@code-pushup/models';
 import { executeProcess, readJsonFile, readTextFile } from '@code-pushup/utils';
 
-const omitVariableData = ({
-  date,
-  duration,
-  version,
-  ...report
-}: Report | PluginReport) => report;
-
-const omitVariableReportData = (report: Report) =>
-  omitVariableData({
-    ...report,
-    plugins: report.plugins.map(omitVariableData) as PluginReport[],
-  });
-
 describe('CLI collect', () => {
+  const exampleCategoryTitle = 'Code style';
+  const exampleAuditTitle =
+    'Require `const` declarations for variables that are never reassigned after declared';
+
+  const omitVariableData = ({
+    date,
+    duration,
+    version,
+    ...report
+  }: Report | PluginReport) => report;
+
+  const omitVariableReportData = (report: Report) =>
+    omitVariableData({
+      ...report,
+      plugins: report.plugins.map(omitVariableData) as PluginReport[],
+    });
+
   it('should run ESLint plugin and create report.json', async () => {
     const { code, stderr } = await executeProcess({
       command: 'npx',
@@ -44,6 +48,8 @@ describe('CLI collect', () => {
     const md = await readTextFile('tmp/react-todos-app/report.md');
 
     expect(md).toContain('# Code Pushup Report');
+    expect(md).toContain(exampleCategoryTitle);
+    expect(md).toContain(exampleAuditTitle);
   });
 
   it('should print report summary to stdout', async () => {
@@ -59,5 +65,7 @@ describe('CLI collect', () => {
     expect(stdout).toContain('Code Pushup Report');
     expect(stdout).toContain('Generated reports');
     expect(stdout).toContain('report.json');
+    expect(stdout).toContain(exampleCategoryTitle);
+    expect(stdout).toContain(exampleAuditTitle);
   });
 });
