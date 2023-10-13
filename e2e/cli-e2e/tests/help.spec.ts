4262cf8c132a1324c09738c1e77cd31ffa5457aa
Matěj Chalk <matej.chalk@flowup.cz>
2023-10-13 10:24:06 +0200

    test(cli-e2e): check exit code and stderr in E2E tests

--- e2e/cli-e2e/tests/help.spec.ts
+++ e2e/cli-e2e/tests/help.spec.ts
@@ -2,10 +2,12 @@ import { executeProcess } from '@code-pushup/utils';
 
 describe('CLI help', () => {
   it('should print help', async () => {
-    const { stdout } = await executeProcess({
+    const { code, stdout, stderr } = await executeProcess({
       command: 'npx',
       args: ['./dist/packages/cli', '--help'],
     });
+    expect(code).toBe(0);
+    expect(stderr).toBe('');
     expect(stdout).toMatchSnapshot();
   });
 });
