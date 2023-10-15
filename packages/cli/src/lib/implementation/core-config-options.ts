d3908ed8b9fb53a668eb86f57db9ecbde14e2de6
Michael <michael.hladky@push-based.io>
2023-10-16 00:57:01 +0200

    refactor: add config args, adopt e2e

--- packages/cli/src/lib/implementation/core-config-options.ts
+++ packages/cli/src/lib/implementation/core-config-options.ts
@@ -1,18 +1,34 @@
 import { Options } from 'yargs';
-import { ArgsCliObj } from './model';
+import { CoreConfigCliOptions } from './model';
 
-export function yargsCoreConfigOptionsDefinition(): Record<
-  keyof ArgsCliObj,
-  Options
-> {
+type ArgNames = keyof CoreConfigCliOptions;
+export function yargsCoreConfigOptionsDefinition(): Record<ArgNames, Options> {
   return {
-    format: {
+    // persist
+    'persist.outputDir': {
+      describe: 'Directory for the produced reports',
+      type: 'string',
+    },
+    'persist.format': {
       describe: 'Format of the report output. e.g. `md`, `json`, `stdout`',
       type: 'array',
     },
-    apiKey: {
-      describe: 'apiKey for the portal',
+    // update
+    'upload.organization': {
+      describe: 'organization name in your CVS',
       type: 'string',
     },
-  } as unknown as Record<keyof ArgsCliObj, Options>;
+    'upload.project': {
+      describe: 'project name in your CVS',
+      type: 'string',
+    },
+    'upload.server': {
+      describe: 'URL to your portal server',
+      type: 'string',
+    },
+    'upload.apiKey': {
+      describe: 'apiKey for the portal server',
+      type: 'string',
+    } /**/,
+  }; //as unknown as Record<keyof ArgsCliObj, Options>;
 }
