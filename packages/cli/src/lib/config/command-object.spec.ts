4a73f1174dccd261d2615aac39c2af2c343579e9
Michael <michael.hladky@push-based.io>
2023-10-16 14:31:41 +0200

    refactor: config command integration test

--- packages/cli/src/lib/config/command-object.spec.ts
+++ packages/cli/src/lib/config/command-object.spec.ts
@@ -1,6 +1,8 @@
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+import { afterEach, beforeEach, describe, expect } from 'vitest';
 import { objectToCliArgs } from '@code-pushup/utils';
+import { mockConsole, unmockConsole } from '../../../test/console.mock';
 import { middlewares } from '../middlewares';
 import { options } from '../options';
 import { yargsCli } from '../yargs-cli';
@@ -26,7 +28,24 @@ const cli = (args: string[]) =>
     commands: [yargsConfigCommandObject()],
   });
 
+let logs: string[] = [];
+
 describe('config-command-object', () => {
+  beforeEach(async () => {
+    logs = [];
+    mockConsole(msg => {
+      const cleanMsg = msg.replace(
+        /(\\"config\\\\": \\\\".*?\\\\")/m,
+        `\\"config\\": \\"XXX\\"`,
+      );
+      logs.push(cleanMsg);
+    });
+  });
+  afterEach(() => {
+    logs = [];
+    unmockConsole();
+  });
+
   it('should override config with CLI arguments', async () => {
     const args = [
       ...baseArgs,
@@ -37,5 +56,6 @@ describe('config-command-object', () => {
     const parsedArgv = await cli(args).parseAsync();
     expect(parsedArgv.persist.outputDir).toBe('tmp');
     expect(parsedArgv.persist?.format).toEqual(['md']);
+    expect(logs[0]).toMatchSnapshot();
   });
 });
