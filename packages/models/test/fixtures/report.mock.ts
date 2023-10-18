03a883251f28f3835d7fa9488d161337b7ab55e6
Matěj Chalk <matej.chalk@flowup.cz>
2023-10-18 10:19:45 +0200

    test(models): prepare test fixtures with more realistic report data

---
+++ packages/models/test/fixtures/report.mock.ts
@@ -0,0 +1,16 @@
+import { Report } from '../../src';
+import { categoryConfigs } from './categories.mock';
+import { eslintPluginReport } from './eslint-plugin.mock';
+import { lighthousePluginReport } from './lighthouse-plugin.mock';
+
+export function report(): Report {
+  return {
+    packageName: '@code-pushup/core',
+    version: '0.1.0',
+    date: '2023-10-18T07:49:45.506Z',
+    duration:
+      eslintPluginReport().duration + lighthousePluginReport().duration + 50,
+    categories: categoryConfigs(),
+    plugins: [eslintPluginReport(), lighthousePluginReport()],
+  };
+}
