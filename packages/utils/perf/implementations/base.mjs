e6d6e23660e57c20aad3d51111637c6d9a01857c
Michael <michael.hladky@push-based.io>
2023-10-22 02:11:52 +0200

    perf(utils): add benchmark logic and example

---
+++ packages/utils/perf/implementations/base.mjs
@@ -0,0 +1,93 @@
+function groupRefToScore(audits) {
+  return ref => {
+    const score = audits.find(audit => audit.slug === ref.slug)?.score;
+    if (score == null) {
+      throw new Error(
+        `Group has invalid ref - audit with slug ${ref.slug} not found`,
+      );
+    }
+    return score;
+  };
+}
+
+function categoryRefToScore(audits, groups) {
+  return ref => {
+    let audit;
+    let group;
+
+    switch (ref.type) {
+      case 'audit':
+        audit = audits.find(
+          a => a.slug === ref.slug && a.plugin === ref.plugin,
+        );
+        if (!audit) {
+          throw new Error(
+            `Category has invalid ref - audit with slug ${ref.slug} not found in ${ref.plugin} plugin`,
+          );
+        }
+        return audit.score;
+
+      case 'group':
+        group = groups.find(
+          g => g.slug === ref.slug && g.plugin === ref.plugin,
+        );
+        if (!group) {
+          throw new Error(
+            `Category has invalid ref - group with slug ${ref.slug} not found in ${ref.plugin} plugin`,
+          );
+        }
+        return group.score;
+      default:
+        throw new Error(`Type ${ref.type} is unknown`);
+    }
+  };
+}
+
+export function calculateScore(refs, scoreFn) {
+  const numerator = refs.reduce(
+    (sum, ref) => sum + scoreFn(ref) * ref.weight,
+    0,
+  );
+  const denominator = refs.reduce((sum, ref) => sum + ref.weight, 0);
+  return numerator / denominator;
+}
+
+export function scoreReport(report) {
+  const scoredPlugins = report.plugins.map(plugin => {
+    const { groups, audits } = plugin;
+    const preparedAudits = audits.map(audit => ({
+      ...audit,
+      plugin: plugin.slug,
+    }));
+    const preparedGroups =
+      groups?.map(group => ({
+        ...group,
+        score: calculateScore(group.refs, groupRefToScore(preparedAudits)),
+        plugin: plugin.slug,
+      })) || [];
+
+    return {
+      ...plugin,
+      audits: preparedAudits,
+      groups: preparedGroups,
+    };
+  });
+
+  // @TODO intro dict to avoid multiple find calls in the scoreFn
+  const allScoredAudits = scoredPlugins.flatMap(({ audits }) => audits);
+  const allScoredGroups = scoredPlugins.flatMap(({ groups }) => groups);
+
+  const scoredCategories = report.categories.map(category => ({
+    ...category,
+    score: calculateScore(
+      category.refs,
+      categoryRefToScore(allScoredAudits, allScoredGroups),
+    ),
+  }));
+
+  return {
+    ...report,
+    categories: scoredCategories,
+    plugins: scoredPlugins,
+  };
+}
---
+++ packages/utils/perf/index.mjs
@@ -0,0 +1,156 @@
+import Benchmark from 'benchmark';
+import { scoreReport } from './implementations/base.mjs';
+import { scoreReportOptimized1 } from './implementations/optimized1.mjs';
+
+const PROCESS_ARGUMENT_NUM_AUDITS_P1 = parseInt(
+  process.argv
+    .find(arg => arg.startsWith('--numAudits1'))
+    ?.split('=')
+    .pop() || '0',
+);
+const PROCESS_ARGUMENT_NUM_AUDITS_P2 = parseInt(
+  process.argv
+    .find(arg => arg.startsWith('--numAudits2'))
+    ?.split('=')
+    .pop() || '0',
+);
+const PROCESS_ARGUMENT_NUM_GROUPS_P2 = parseInt(
+  process.argv
+    .find(arg => arg.startsWith('--numGroupRefs2'))
+    ?.split('=')
+    .pop() || '0',
+);
+
+const suite = new Benchmark.Suite();
+
+const AUDIT_PREFIX = 'a-';
+const GROUP_PREFIX = 'g:';
+const PLUGIN_PREFIX = 'p.';
+const SLUG_PLUGIN_P1 = PLUGIN_PREFIX + 1;
+const AUDIT_P1_PREFIX = AUDIT_PREFIX + SLUG_PLUGIN_P1;
+const SLUG_PLUGIN_P2 = PLUGIN_PREFIX + 2;
+const AUDIT_P2_PREFIX = AUDIT_PREFIX + SLUG_PLUGIN_P2;
+const GROUP_P2_PREFIX = GROUP_PREFIX + SLUG_PLUGIN_P2;
+const NUM_AUDITS_P1 = PROCESS_ARGUMENT_NUM_AUDITS_P1 || 27;
+const NUM_AUDITS_P2 = PROCESS_ARGUMENT_NUM_AUDITS_P2 || 18;
+const NUM_GROUPS_P2 = PROCESS_ARGUMENT_NUM_GROUPS_P2 || NUM_AUDITS_P2 / 2;
+
+// ==================
+
+// Add listener
+const listeners = {
+  cycle: function (event) {
+    console.log(String(event.target));
+  },
+  complete: function () {
+    if (typeof this?.filter === 'function') {
+      console.log('Fastest is ' + this?.filter('fastest').map('name'));
+    }
+  },
+};
+
+// ==================
+
+// Add tests
+suite.add('scoreReport', _scoreReport);
+suite.add('scoreReportOptimized', _scoreReportOptimized);
+
+// ==================
+
+// Add Listener
+Object.entries(listeners).forEach(([name, fn]) => {
+  suite.on(name, fn);
+});
+
+// ==================
+
+console.info('You can adjust the number of runs with the following arguments:');
+console.info(
+  `numAudits1 - Number of audits in plugin 1. --numAudits1=${NUM_AUDITS_P1}`,
+);
+console.info(
+  `numAudits2 - Number of audits in plugin 2.  --numAudits2=${NUM_AUDITS_P2}`,
+);
+console.info(
+  `numGroupRefs2 - Number of groups refs in plugin 2.  --numGroupRefs2=${NUM_GROUPS_P2}`,
+);
+console.log(' ');
+console.log('Start benchmark...');
+console.log(' ');
+
+let start = performance.now();
+suite.run();
+
+console.log('');
+console.log(
+  'Total Duration: ',
+  ((performance.now() - start) / 1000).toFixed(2) + ' sec',
+);
+
+// ==============================================================
+
+function _scoreReport() {
+  scoreReport(minimalReport());
+}
+
+function _scoreReportOptimized() {
+  scoreReportOptimized1(minimalReport());
+}
+
+// ==============================================================
+
+function minimalReport(opt) {
+  const numAuditsP1 = opt?.numAuditsP1 || NUM_AUDITS_P1;
+  const numAuditsP2 = opt?.numAuditsP2 || NUM_AUDITS_P2;
+  const numGroupRefs2 = opt?.numGroupRefs2 || NUM_GROUPS_P2;
+
+  return {
+    categories: [
+      {
+        slug: 'c1_',
+        refs: new Array(numAuditsP1).map((_, idx) => ({
+          type: 'audit',
+          plugin: SLUG_PLUGIN_P1,
+          slug: `${AUDIT_P1_PREFIX}${idx}`,
+          weight: 1,
+        })),
+        isBinary: false,
+      },
+      {
+        slug: 'c2_',
+        refs: new Array(numAuditsP2).map((_, idx) => ({
+          type: 'audit',
+          plugin: SLUG_PLUGIN_P2,
+          slug: `${AUDIT_P2_PREFIX}${idx}`,
+          weight: 1,
+        })),
+        isBinary: false,
+      },
+    ],
+    plugins: [
+      {
+        slug: SLUG_PLUGIN_P1,
+        audits: new Array(numAuditsP1).map((_, idx) => ({
+          slug: `${AUDIT_P1_PREFIX}${idx}`,
+          score: 0.1,
+        })),
+      },
+      {
+        slug: SLUG_PLUGIN_P2,
+        audits: new Array(numAuditsP2).map((_, idx) => ({
+          slug: `${AUDIT_P2_PREFIX}${idx}`,
+          score: 0.1,
+        })),
+        groups: [
+          {
+            slug: GROUP_P2_PREFIX + 1,
+            refs: new Array(numGroupRefs2).map((_, idx) => ({
+              slug: `${AUDIT_P2_PREFIX}${idx}`,
+              weight: 1,
+            })),
+          },
+        ],
+      },
+    ],
+  };
+}
