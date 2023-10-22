efcdc2181ba64ea7e4067ffcef7e133a1f6897a4
Michael <michael.hladky@push-based.io>
2023-10-22 04:39:48 +0200

    refactor: add exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,13 @@
 export {
   logPersistedResults,
   persistReport,
+  PersistError,
+  PersistDirError,
 } from './lib/implementation/persist';
-export { executePlugins } from './lib/implementation/execute-plugin';
+export {
+  executePlugins,
+  PluginOutputError,
+} from './lib/implementation/execute-plugin';
 export { collect, CollectOptions } from './lib/implementation/collect';
 export { upload, UploadOptions } from './lib/upload';
 export {
