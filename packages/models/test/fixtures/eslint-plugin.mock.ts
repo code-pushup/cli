e7ed3ae56b15fd877f3db57f2693b0162e81043f
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2023-10-24 22:55:19 +0200

    Update packages/models/test/fixtures/eslint-plugin.mock.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/models/test/fixtures/eslint-plugin.mock.ts
+++ packages/models/test/fixtures/eslint-plugin.mock.ts
@@ -7,14 +7,14 @@ import type {
 import { ESLINT_AUDITS_MAP } from './eslint-audits.mock';
 import { runnerConfig } from './runner.mock';
 
-const eslintMeta: Omit<PluginConfig, 'audits' | 'runner'> = {
+const eslintMeta = {
   slug: 'eslint',
   title: 'ESLint',
   icon: 'eslint',
   description: 'Official Code PushUp ESLint plugin',
   packageName: '@code-pushup/eslint-plugin',
   version: '0.1.0',
-};
+} satisfies Partial<PluginConfig>;
 
 export function eslintPluginConfig(outputDir = 'tmp'): PluginConfig {
   const audits = Object.values(ESLINT_AUDITS_MAP).map(
