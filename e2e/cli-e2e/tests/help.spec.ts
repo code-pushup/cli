468ab87755bdb7f8d9841aca457489dc281730d0
Michael <michael.hladky@push-based.io>
2023-10-26 13:44:48 +0200

    refactor: add test docs

--- e2e/cli-e2e/tests/help.spec.ts
+++ e2e/cli-e2e/tests/help.spec.ts
@@ -1,13 +1,15 @@
 import { executeProcess } from '@code-pushup/utils';
 
 describe('CLI help', () => {
-  it('should print help', async () => {
+  it('should print help whit argument ---help', async () => {
     const { code, stdout, stderr } = await executeProcess({
       command: 'npx',
       args: ['./dist/packages/cli', '--help'],
     });
     expect(code).toBe(0);
     expect(stderr).toBe('');
-    expect(stdout).toMatchSnapshot();
+    expect(stdout).toMatchSnapshot('help');
   });
+
+  // @TODO 'should print help with help command'
 });
