cd57936b8666e876be2999a17dd13c897ab39e16
Michael <michael.hladky@push-based.io>
2023-10-26 05:35:59 +0200

    refactor(utils): test progress bar formatting

--- packages/utils/src/lib/progress.spec.ts
+++ packages/utils/src/lib/progress.spec.ts
@@ -1,53 +1,57 @@
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-import { describe, expect, it } from 'vitest';
-import { executeProcess } from './execute-process';
-import { getProgressBar, getSingletonProgressBars } from './progress';
-
-const progressPath = join(
-  fileURLToPath(dirname(import.meta.url)),
-  '..',
-  '..',
-  'test',
-  'fixtures',
-  'execute-progress.mock.mjs',
-);
+import {describe, expect, it} from 'vitest';
+import {barStyles, getProgressBar, getSingletonProgressBars} from './progress';
 
 describe('getSingletonMultiProgressBars', () => {
   it('should be singleton', async () => {
     const a = getSingletonProgressBars();
     const b = getSingletonProgressBars();
     expect(a).toBe(b);
+    a.close();
+    b.close();
   });
 });
 
+const taskAName = 'a';
+const bars = getSingletonProgressBars({progressWidth:1}) as any;
+const progressBuffer = bars.logger.progressBuffer;
+
 describe('getProgressBar', () => {
-  it('should create bar', async () => {
-    const tasks = getSingletonProgressBars();
-    expect(tasks.getIndex('a')).toBe(undefined);
-    getProgressBar('a');
-    expect(tasks.getIndex('a')).toBe(0);
-    tasks.close();
+
+  it('should init task', async () => {
+    expect(bars.getIndex(taskAName)).toBe(undefined);
+    getProgressBar(taskAName);
+    expect(bars.getIndex(taskAName)).toBe(0);
+    expect(progressBuffer[0].trim()).toBe(`${taskAName}: ${barStyles.idle(' ')}   0% |`);
+
+    // safety checks int the first test only
+    bars.removeTask(taskAName);
+    expect(bars.getIndex(taskAName)).toBe(undefined);
+    await bars.promise; // check if all tasks are done
+  });
+
+  it('should update task message', async () => {
+    const taskA = getProgressBar(taskAName);
+    expect(bars.tasks[taskAName].message).toBe('');
+    taskA.updateTitle('0');
+    expect(bars.tasks[taskAName].message).toBe('0');
+    expect(progressBuffer[0].trim()).toBe(`${taskAName}: ${barStyles.active(' ')}   0% | 0`);
+    bars.removeTask(taskAName);
+  });
+
+  it('should update task progress', async () => {
+    const taskA = getProgressBar(taskAName);
+    taskA.incrementInSteps(2);
+    expect(bars.tasks[taskAName].message).toBe('');
+    expect(progressBuffer[0].trim()).toBe(`${taskAName}: ${barStyles.active('▌')}  50% |`);
+    bars.removeTask(taskAName);
   });
 
   it('should end task when endProgress is called', async () => {
-    const tasks = getSingletonProgressBars();
-    const taskAName = 'a';
     const taskA = getProgressBar(taskAName);
-    expect(tasks.isDone(taskAName)).toBe(false);
+    expect(bars.isDone(taskAName)).toBe(false);
     taskA.endProgress();
-    expect(tasks.isDone(taskAName)).toBe(true);
-    await tasks.promise; // check if all tasks are done
+    expect(bars.isDone(taskAName)).toBe(true);
+    expect(progressBuffer[0].trim()).toBe(`${taskAName}: ${barStyles.done('█')} 100% |`);
   });
 
-  it('should log progress bar', async () => {
-    const { stdout } = await executeProcess({
-      command: 'npx',
-      args: ['node', progressPath, '--verbose=1'],
-    });
-    // log from the process itself (--verbose=1)
-    expect(stdout).toContain('progress:start with duration: 300, steps: 10');
-    // log from progress bar
-    // expect(stdout).toContain('mock-progress'); // @TODO fix testing for progress bar log! ATM is is not visible in stdout
-  });
 });
--- packages/utils/src/lib/progress.ts
+++ packages/utils/src/lib/progress.ts
@@ -1,13 +1,21 @@
 import chalk from 'chalk';
-import { MultiProgressBars } from 'multi-progress-bars';
+import {CtorOptions, MultiProgressBars} from 'multi-progress-bars';
 
-const barStyles = {
+/**
+ * ANSI escape codes in terminal stdout:
+ * - `\u001b[30m` black
+ * - `\u001b[90m` gray
+ * - `\u001b[32m` green
+ * - `\u001b[39m` reset or default color
+ */
+
+export const barStyles = {
   active: (s: string) => chalk.green(s),
   done: (s: string) => chalk.gray(s),
   idle: (s: string) => chalk.gray(s),
 };
 
-const messageStyles = {
+export const messageStyles = {
   active: (s: string) => chalk.black(s),
   done: (s: string) => chalk.green(chalk.bold(s)),
   idle: (s: string) => chalk.gray(s),
@@ -22,11 +30,12 @@ export type ProgressBar = {
 
 let mpb: MultiProgressBars;
 
-export function getSingletonProgressBars(): MultiProgressBars {
+export function getSingletonProgressBars(options?: Partial<CtorOptions>): MultiProgressBars {
   if (!mpb) {
     mpb = new MultiProgressBars({
       initMessage: '',
       border: true,
+      ...options,
     });
   }
   return mpb;
@@ -36,17 +45,18 @@ export function getProgressBar(taskName: string): ProgressBar {
   const tasks = getSingletonProgressBars();
 
   // Initialize progress bar if not set
-  if (tasks.getIndex(taskName) === undefined) {
-    tasks.addTask(taskName, {
-      type: 'percentage',
-      percentage: 0,
-    });
-  }
+  tasks.addTask(taskName, {
+    type: 'percentage',
+    percentage: 0,
+    message: '',
+    barTransformFn: barStyles.idle,
+  });
 
   return {
     incrementInSteps: (numPlugins: number) => {
       tasks.incrementTask(taskName, {
         percentage: 1 / numPlugins,
+        barTransformFn: barStyles.active,
       });
     },
     updateTitle: (title: string) => {
