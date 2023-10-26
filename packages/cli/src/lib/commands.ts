6b46fc515f17b0f871c1b5b89b39cc42628b2af4
Michael <michael.hladky@push-based.io>
2023-10-26 14:26:09 +0200

    refactor: sort imports

--- packages/cli/src/lib/commands.ts
+++ packages/cli/src/lib/commands.ts
@@ -1,16 +1,16 @@
 import { CommandModule } from 'yargs';
 import { yargsAutorunCommandObject } from './autorun/command-object';
 import { yargsCollectCommandObject } from './collect/command-object';
-import { yargsConfigCommandObject } from './print-config/command-object';
 import { yargsUploadCommandObject } from './upload/command-object';
+import { yargsConfigCommandObject } from './print-config/command-object';
 
 export const commands: CommandModule[] = [
   {
     ...yargsAutorunCommandObject(),
     command: '*',
   },
-  yargsConfigCommandObject(),
+  yargsAutorunCommandObject(),
   yargsCollectCommandObject(),
   yargsUploadCommandObject(),
-  yargsAutorunCommandObject(),
+  yargsConfigCommandObject(),
 ];
