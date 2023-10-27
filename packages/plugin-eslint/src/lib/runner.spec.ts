a3e6514b7a36ffb73ed2e4d318e3894fa29259db
Katerina Pilatova <katerina.pilatova@flowup.cz>
2023-10-27 12:39:58 +0200

    fix(testing): fix quote formatting for Windows

--- packages/plugin-eslint/src/lib/runner.spec.ts
+++ packages/plugin-eslint/src/lib/runner.spec.ts
@@ -1,4 +1,5 @@
 import { ESLint } from 'eslint';
+import os from 'os';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import type { SpyInstance } from 'vitest';
@@ -12,6 +13,7 @@ import {
 
 describe('executeRunner', () => {
   let cwdSpy: SpyInstance;
+  let platformSpy: SpyInstance;
   let argv: string[];
 
   beforeAll(async () => {
@@ -24,6 +26,8 @@ describe('executeRunner', () => {
       'todos-app',
     );
     cwdSpy = vi.spyOn(process, 'cwd').mockReturnValue(appDir);
+    // Windows does not require additional quotation marks for globs
+    platformSpy = vi.spyOn(os, 'platform').mockReturnValue('win32');
 
     const eslintrc = '.eslintrc.js';
     const patterns = ['src/**/*.js', 'src/**/*.jsx'];
@@ -45,6 +49,7 @@ describe('executeRunner', () => {
 
   afterAll(() => {
     cwdSpy.mockRestore();
+    platformSpy.mockRestore();
   });
 
   it('should execute ESLint and create audit results for React application', async () => {
