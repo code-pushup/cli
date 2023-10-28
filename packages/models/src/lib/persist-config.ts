a30f9823539b2d189a0af41603f852d957d3b6c3
Michael <michael.hladky@push-based.io>
2023-10-28 23:16:08 +0200

    feat(models): add filename to persist model

--- packages/models/src/lib/persist-config.ts
+++ packages/models/src/lib/persist-config.ts
@@ -1,11 +1,16 @@
 import { z } from 'zod';
-import { filePathSchema } from './implementation/schemas';
+import { fileNameSchema, filePathSchema } from './implementation/schemas';
 
 export const formatSchema = z.enum(['json', 'stdout', 'md']);
 export type Format = z.infer<typeof formatSchema>;
 
 export const persistConfigSchema = z.object({
   outputDir: filePathSchema('Artifacts folder'),
+  /* filename: fileNameSchema(
+    'Artifacts file name (without extension)',
+  )
+    .default('report')
+    .optional(),*/
   format: z.array(formatSchema).default(['stdout']).optional(),
 });
 
