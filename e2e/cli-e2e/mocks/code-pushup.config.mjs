fb87223764567ed78eb91b9fc3a6abf50e5fb78e
Michael <michael.hladky@push-based.io>
2023-10-29 20:54:44 +0100

    refactor: fix path 2

--- e2e/cli-e2e/mocks/code-pushup.config.mjs
+++ e2e/cli-e2e/mocks/code-pushup.config.mjs
@@ -1,3 +1,4 @@
+import { join } from 'path';
 // TODO: import plugins using NPM package names using local registry: https://github.com/flowup/quality-metrics-cli/issues/33
 // import eslintPlugin from '../../../dist/packages/plugin-eslint';
 import lighthousePlugin from '../../../dist/packages/plugin-lighthouse';
