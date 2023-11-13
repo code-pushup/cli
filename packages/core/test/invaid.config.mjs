a1eaa39b2d3eeb0449f69b122978a8693dbf2810
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2023-11-13 22:43:11 +0100

    refactor(core): improve report loading (#238)

---
+++ packages/core/test/invaid.config.mjs
@@ -0,0 +1,9 @@
+/**
+ * This config provides the minimal configurations and is here for general purpose.
+ * Multiple tests can rely on it.
+ *
+ * Usage:
+ * npx ./dist/packages/cli collect --config=./packages/cli/test/minimal.config.ts
+ */
+
+export default {};
