39ad8d92ab35263185466e698c38c692011b9060
Michael <michael.hladky@push-based.io>
2023-11-20 02:13:50 +0100

    docs: add example plugin to root config

---
+++ examples/plugins/lighthouse.plugin.ts
@@ -0,0 +1,91 @@
+import {Result} from 'lighthouse';
+
+import {
+  AuditOutputs,
+  PluginConfig,
+} from '../../dist/packages/models';
+import {objectToCliArgs} from "../../packages/utils/src";
+import {CategoryRef, RunnerConfig} from "../../packages/models/src";
+
+export type LighthouseOptions = {
+  url: string;
+  outputFile: string;
+};
+
+export const pluginSlug = 'lighthouse-plugin';
+export const lighthousePluginRecommended: CategoryRef[] = [
+
+];
+
+/**
+ * @example
+ * // code-pushup.config.ts
+ * import {
+ * create as lighthousePlugin,
+ * pluginSlug as lighthousePluginSlug,
+ * } from 'lighthouse.plugin.ts';
+ * export default {
+ *   persist: {
+ *     outputDir: '.code-pushup',
+ *     format: ['json', 'md', 'stdout'],
+ *   },
+ *   plugins: [
+ *     await lighthousePlugin({
+ *       url: "angular.dev"
+ *     })
+ *   ],
+ *   categories: [
+ *     {
+ *       slug: 'performance',
+ *       title: 'Performance',
+ *       refs: [
+ *         ...lighthousePluginRecommended
+ *       ]
+ *     }
+ *   ]
+ * }
+ *
+ */
+export async function create(options: LighthouseOptions): Promise<PluginConfig> {
+  return {
+    slug: pluginSlug,
+    title: 'Lighthouse Plugin',
+    icon: 'javascript',
+    description:
+      'A plugin to measure and assert filesize of files in a directory.',
+    runner: runnerConfig(options),
+    audits: [],
+  };
+}
+
+function runnerConfig(options: LighthouseOptions): PluginConfig['runner'] {
+  const {outputFile, url} = options;
+  return {
+    command: 'npx',
+    args: objectToCliArgs({
+      _: url,
+      output: 'json',
+      'output-path': outputFile,
+      'chrome-flags:"--headless"': true
+    }),
+    outputFile,
+    outputTransform: async (output: string): Promise<AuditOutputs> => {
+      const lighthouseOutput: Result = JSON.parse((output));
+      console.info(lighthouseOutput);
+      return lhrToAuditOutputs(lighthouseOutput) as AuditOutputs;
+    }
+  } satisfies RunnerConfig;
+}
+
+function lhrToAuditOutputs(lhr: Result): AuditOutputs {
+  return Object.entries(lhr.audits).map(([slug, result]) => {
+    return {
+      slug,
+      score: result.score,
+      value: result.numericValue,
+      displayValue: result.displayValue,
+      description: result.description,
+      // details: result.details as Issue,
+    };
+  });
+}
