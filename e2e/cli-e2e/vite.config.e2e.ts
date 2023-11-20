d3c6fed0ac1bda450676d99b351df2a4e340cfa2
Igor Katsuba <igor@katsuba.dev>
2023-11-20 21:34:40 +0200

    chore(cli-e2e): fix some problems after conflicts

--- e2e/cli-e2e/vite.config.e2e.ts
+++ e2e/cli-e2e/vite.config.e2e.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     },
     environment: 'node',
     include: ['tests/**/*.e2e.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    globalSetup: ['global-setup.ts'],
+    globalSetup: ['global-setup.e2e.ts'],
   },
 });
