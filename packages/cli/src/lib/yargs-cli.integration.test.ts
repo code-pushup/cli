f54aeabe2fd26ba39c4f7d1d88e786569c1262f4
Katerina Pilatova <katerina.pilatova@flowup.cz>
2023-11-21 14:50:33 +0100

    test(cli): expand yargs-cli tests, improve typing

--- packages/cli/src/lib/yargs-cli.integration.test.ts
+++ packages/cli/src/lib/yargs-cli.integration.test.ts
@@ -1,69 +1,104 @@
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
 import { describe, expect, it } from 'vitest';
-import { objectToCliArgs } from '@code-pushup/utils';
-import { middlewares } from './middlewares';
+import { CoreConfig } from '@code-pushup/models';
+import {
+  CoreConfigCliOptions,
+  GeneralCliOptions,
+} from './implementation/model';
 import { options } from './options';
 import { yargsCli } from './yargs-cli';
 
 describe('yargsCli', () => {
-  it('global options should provide correct defaults', async () => {
-    const args: string[] = [];
-    const parsedArgv = await yargsCli(args, {
+  it('should provide correct default values for global options', async () => {
+    const parsedArgv = await yargsCli<GeneralCliOptions>([], {
       options,
     }).parseAsync();
     expect(parsedArgv.verbose).toBe(false);
     expect(parsedArgv.progress).toBe(true);
   });
 
-  it('multiple config args should be parsed to last item from array', async () => {
-    const args: string[] = ['--config=./config.a.ts', '--config=./config.b.ts'];
-    const parsedArgv = await yargsCli(args, {
+  it('should parse a single boolean negated argument', async () => {
+    const parsedArgv = await yargsCli<GeneralCliOptions>(['--no-progress'], {
       options,
     }).parseAsync();
-    expect(parsedArgv.config).toBe('./config.b.ts');
+    expect(parsedArgv.progress).toBe(false);
   });
 
-  it('single config arg should be parsed as a single string', async () => {
-    const args: string[] = ['--config=./config.a.ts'];
-    const parsedArgv = await yargsCli(args, {
-      options,
-    }).parseAsync();
+  it('should parse a single config argument as a string', async () => {
+    const parsedArgv = await yargsCli<GeneralCliOptions>(
+      ['--config=./config.a.ts'],
+      {
+        options,
+      },
+    ).parseAsync();
     expect(parsedArgv.config).toBe('./config.a.ts');
   });
 
-  it('global options should parse correctly', async () => {
-    const args: string[] = objectToCliArgs({
-      verbose: true,
-      progress: false,
-    });
+  it('should parse an array argument', async () => {
+    const parsedArgv = await yargsCli<CoreConfig>(
+      ['--persist.format=md', '--persist.format=json'],
+      {
+        options,
+      },
+    ).parseAsync();
+    expect(parsedArgv.persist.format).toEqual(['md', 'json']);
+  });
 
-    const parsedArgv = await yargsCli(args, {
-      options,
-    }).parseAsync();
+  it('should parse global options correctly', async () => {
+    const parsedArgv = await yargsCli<GeneralCliOptions>(
+      ['--verbose', '--no-progress'],
+      {
+        options,
+      },
+    ).parseAsync();
     expect(parsedArgv.verbose).toBe(true);
     expect(parsedArgv.progress).toBe(false);
   });
 
-  it('global options and middleware handle argument overrides correctly', async () => {
-    const args: string[] = objectToCliArgs({
-      config: join(
-        fileURLToPath(dirname(import.meta.url)),
-        '..',
-        '..',
-        'test',
-        'cli-parsing.config.mock.ts',
-      ),
-      verbose: true,
-      progress: false,
-      format: ['md'],
-    });
-    const parsedArgv = await yargsCli(args, {
-      options,
-      middlewares,
-    }).parseAsync();
-    expect(parsedArgv.config).toContain('');
-    expect(parsedArgv.verbose).toBe(true);
-    expect(parsedArgv.progress).toBe(false);
+  it('should use the last occurrence of an argument if config is passed multiple times', async () => {
+    const parsedArgv = await yargsCli<GeneralCliOptions>(
+      ['--config=./config.a.ts', '--config=./config.b.ts'],
+      {
+        options,
+      },
+    ).parseAsync();
+    expect(parsedArgv.config).toBe('./config.b.ts');
+  });
+
+  it('should handle global options and middleware argument overrides correctly', async () => {
+    const parsedArgv = await yargsCli<GeneralCliOptions & CoreConfigCliOptions>(
+      [
+        '--verbose',
+        '--persist.format=md',
+        '--persist.outputDir=code-pushdown/output/dir',
+        '--persist.filename=code-pushdown-report',
+        '--upload.organization=pushdown',
+        '--upload.project=code-push-down',
+        '--upload.server=https://code-pushdown.com/api',
+        '--upload.apiKey=some-api-key',
+      ],
+      {
+        options,
+      },
+    ).parseAsync();
+    expect(parsedArgv).toEqual(
+      expect.objectContaining({
+        // default values
+        progress: true,
+        config: expect.stringContaining('code-pushup.config'),
+        // overridden arguments
+        verbose: true,
+        persist: expect.objectContaining({
+          outputDir: 'code-pushdown/output/dir',
+          filename: 'code-pushdown-report',
+          format: ['md'],
+        }),
+        upload: expect.objectContaining({
+          organization: 'pushdown',
+          project: 'code-push-down',
+          server: 'https://code-pushdown.com/api',
+          apiKey: 'some-api-key',
+        }),
+      }),
+    );
   });
 });
