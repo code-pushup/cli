8f6561640c22a798613f802f2ff3d2aeef4d72d9
Katerina Pilatova <katerina.pilatova@flowup.cz>
2023-11-22 15:36:27 +0100

    test(cli): include collect unit test

---
+++ packages/cli/src/lib/collect/collect-command.unit.test.ts
@@ -0,0 +1,83 @@
+import { bundleRequire } from 'bundle-require';
+import { vol } from 'memfs';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { collectAndPersistReports } from '@code-pushup/core';
+import { report } from '@code-pushup/models/testing';
+import { DEFAULT_CLI_CONFIGURATION } from '../../../test/constants';
+import { yargsCli } from '../yargs-cli';
+import { yargsCollectCommandObject } from './collect-command';
+
+// Mock file system API's
+vi.mock('fs', async () => {
+  const memfs: typeof import('memfs') = await vi.importActual('memfs');
+  return memfs.fs;
+});
+vi.mock('fs/promises', async () => {
+  const memfs: typeof import('memfs') = await vi.importActual('memfs');
+  return memfs.fs.promises;
+});
+
+vi.mock('@code-pushup/core', async () => {
+  const core = await vi.importActual('@code-pushup/core');
+  return {
+    ...(core as object),
+    collectAndPersistReports: vi.fn().mockResolvedValue({}),
+  };
+});
+
+// Mock bundleRequire inside importEsmModule used for fetching config
+vi.mock('bundle-require', async () => {
+  const { minimalConfig }: typeof import('@code-pushup/models/testing') =
+    await vi.importActual('@code-pushup/models/testing');
+  return {
+    bundleRequire: vi
+      .fn()
+      .mockResolvedValue({ mod: { default: minimalConfig() } }),
+  };
+});
+
+describe('collect-command', () => {
+  beforeEach(async () => {
+    vi.clearAllMocks();
+    vol.reset();
+    vol.fromJSON(
+      {
+        'my-report.json': JSON.stringify(report()),
+        'code-pushup.config.ts': '', // only needs to exist for stat inside bundleRequire
+      },
+      '/test',
+    );
+  });
+
+  it('should call collect with correct parameters', async () => {
+    await yargsCli(
+      [
+        'collect',
+        '--verbose',
+        '--config=/test/code-pushup.config.ts',
+        '--persist.filename=my-report',
+        '--persist.outputDir=/test',
+      ],
+      {
+        ...DEFAULT_CLI_CONFIGURATION,
+        commands: [yargsCollectCommandObject()],
+      },
+    ).parseAsync();
+
+    expect(bundleRequire).toHaveBeenCalledWith({
+      format: 'esm',
+      filepath: '/test/code-pushup.config.ts',
+    });
+
+    expect(collectAndPersistReports).toHaveBeenCalledWith(
+      expect.objectContaining({
+        verbose: true,
+        config: '/test/code-pushup.config.ts',
+        persist: expect.objectContaining({
+          filename: 'my-report',
+          outputDir: '/test',
+        }),
+      }),
+    );
+  });
+});
