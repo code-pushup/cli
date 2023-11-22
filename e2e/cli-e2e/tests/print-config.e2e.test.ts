5121510c534319f7d808a3b7c14eb87079cc63ff
Katerina Pilatova <katerina.pilatova@flowup.cz>
2023-11-22 16:03:50 +0100

    test(cli-e2e): reduce print-config tests to critical path

--- e2e/cli-e2e/tests/print-config.e2e.test.ts
+++ e2e/cli-e2e/tests/print-config.e2e.test.ts
@@ -1,82 +1,53 @@
-import { join } from 'path';
 import { expect } from 'vitest';
-import { CliArgsObject } from '@code-pushup/utils';
-import { configFile, execCli, extensions } from '../mocks/utils';
-
-const execCliPrintConfig = (argObj: Partial<CliArgsObject>) =>
-  execCli('print-config', argObj);
+import { executeProcess } from '@code-pushup/utils';
+import { configFile, extensions } from '../mocks/utils';
 
 describe('print-config', () => {
   it.each(extensions)(
-    'should load .%s config file',
+    'should load .%s config file with correct arguments',
     async ext => {
-      const { code, stderr, stdout } = await execCliPrintConfig({
-        config: configFile(ext),
+      const { code, stderr, stdout } = await executeProcess({
+        command: 'code-pushup',
+        args: [
+          'print-config',
+          '--verbose',
+          '--no-progress',
+          `--config=${configFile(ext)}`,
+          '--persist.outputDir=my-dir',
+          '--persist.format=md',
+          '--persist.filename=my-report',
+        ],
       });
+
       expect(code).toBe(0);
       expect(stderr).toBe('');
-      const args = JSON.parse(stdout);
-      expect(args).toEqual({
-        progress: false,
-        verbose: true,
-        config: expect.stringContaining(`code-pushup.config.${ext}`),
-        upload: {
-          organization: 'code-pushup',
-          project: `cli-${ext}`,
-          apiKey: 'e2e-api-key',
-          server: 'https://e2e.com/api',
-        },
-        persist: {
-          outputDir: join('tmp', ext),
-          filename: 'report',
-        },
-        plugins: expect.arrayContaining([
-          expect.objectContaining({ slug: 'eslint', title: 'ESLint' }),
-          expect.objectContaining({
-            slug: 'lighthouse',
-            title: 'ChromeDevTools Lighthouse',
-          }),
-        ]),
-        // @TODO add test data to config file
-        categories: expect.any(Array),
-        onlyPlugins: [],
-      });
+      expect(JSON.parse(stdout)).toEqual(
+        expect.objectContaining({
+          config: expect.stringContaining(`code-pushup.config.${ext}`),
+          upload: {
+            organization: 'code-pushup',
+            project: `cli-${ext}`,
+            apiKey: 'e2e-api-key',
+            server: 'https://e2e.com/api',
+          },
+          persist: {
+            outputDir: 'my-dir',
+            format: ['md'],
+            filename: 'my-report',
+          },
+          plugins: expect.arrayContaining([
+            expect.objectContaining({ slug: 'eslint', title: 'ESLint' }),
+            expect.objectContaining({
+              slug: 'lighthouse',
+              title: 'ChromeDevTools Lighthouse',
+            }),
+          ]),
+          // @TODO add test data to config file
+          categories: expect.any(Array),
+          onlyPlugins: [],
+        }),
+      );
     },
     120000,
   );
-
-  it('should load .ts config file and merge cli arguments', async () => {
-    const { code, stderr, stdout } = await execCliPrintConfig({
-      'persist.outputDir': 'my-dir',
-      'persist.format': 'md',
-      'persist.filename': 'my-report',
-    });
-    expect(code).toBe(0);
-    expect(stderr).toBe('');
-    const args = JSON.parse(stdout);
-    expect(args).toEqual({
-      progress: false,
-      verbose: true,
-      config: expect.stringContaining(`code-pushup.config.ts`),
-      upload: expect.any(Object),
-      persist: expect.objectContaining({
-        outputDir: 'my-dir',
-        format: ['md'],
-        filename: 'my-report',
-      }),
-      plugins: expect.any(Array),
-      categories: expect.any(Array),
-      onlyPlugins: [],
-    });
-  }, 120000);
-
-  it('should parse persist.format from arguments', async () => {
-    const { code, stderr, stdout } = await execCliPrintConfig({
-      'persist.format': ['md', 'json', 'stdout'],
-    });
-    expect(code).toBe(0);
-    expect(stderr).toBe('');
-    const args = JSON.parse(stdout);
-    expect(args.persist.format).toEqual(['md', 'json', 'stdout']);
-  }, 120000);
 });
