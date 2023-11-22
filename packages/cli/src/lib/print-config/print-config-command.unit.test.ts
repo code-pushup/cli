44e94df9682da29da785d271e319e072aeb4d755
Katerina Pilatova <katerina.pilatova@flowup.cz>
2023-11-22 14:12:55 +0100

    test(cli): improve print-config unit test, remove duplicates

--- packages/cli/src/lib/print-config/print-config-command.unit.test.ts
+++ packages/cli/src/lib/print-config/print-config-command.unit.test.ts
@@ -1,69 +1,47 @@
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import { describe, expect } from 'vitest';
-import { objectToCliArgs } from '@code-pushup/utils';
+import { SpyInstance, describe, expect } from 'vitest';
 import { DEFAULT_CLI_CONFIGURATION } from '../../../test/constants';
 import { yargsCli } from '../yargs-cli';
 import { yargsConfigCommandObject } from './print-config-command';
 
-const baseArgs = [
-  ...objectToCliArgs({
-    verbose: true,
-    config: join(
+describe('print-config-command', () => {
+  let logSpy: SpyInstance;
+
+  beforeEach(async () => {
+    logSpy = vi.spyOn(console, 'log');
+  });
+
+  afterEach(() => {
+    logSpy.mockRestore();
+  });
+
+  it('should filter out meta arguments and kebab duplicates', async () => {
+    const configPath = join(
       fileURLToPath(dirname(import.meta.url)),
       '..',
       '..',
       '..',
       'test',
       'all-values.config.ts',
-    ),
-  }),
-];
-const cli = (args: string[]) =>
-  yargsCli(['config', ...args], {
-    ...DEFAULT_CLI_CONFIGURATION,
-    commands: [yargsConfigCommandObject()],
-  });
-
-describe('print-config-command-object', () => {
-  it('should print existing config', async () => {
-    const parsedArgv = await cli(baseArgs).parseAsync();
-    expect(parsedArgv.persist.outputDir).toBe('tmp');
-    expect(parsedArgv.persist.filename).toBe('report');
-    expect(parsedArgv.persist.format).toBeUndefined();
-    expect(parsedArgv.upload?.organization).toBe('code-pushup');
-    expect(parsedArgv.upload?.project).toBe('cli');
-    expect(parsedArgv.upload?.apiKey).toBe('dummy-api-key');
-    expect(parsedArgv.upload?.server).toBe('https://example.com/api');
-    expect(parsedArgv.plugins).toEqual(expect.any(Array));
-    expect(parsedArgv.plugins[0]?.slug).toBe('plugin-1');
-    expect(parsedArgv.categories).toEqual(expect.any(Array));
-  });
-
-  it('should override config with CLI arguments', async () => {
-    const overrides = {
-      'persist.outputDir': 'custom/output/dir',
-      'persist.format': ['md'],
-      'persist.filename': 'custom-report-filename',
-      'upload.organization': 'custom-org',
-      'upload.project': 'custom-project',
-      'upload.apiKey': 'custom-api-key',
-      'upload.server': 'https//custom-server.com/api',
-    };
-    const args = [...baseArgs, ...objectToCliArgs(overrides)];
-
-    const parsedArgv = await cli(args).parseAsync();
-    expect(parsedArgv.persist.outputDir).toBe(overrides['persist.outputDir']);
-    expect(parsedArgv.persist.filename).toBe('custom-report-filename');
-    expect(parsedArgv.persist.format).toEqual(overrides['persist.format']);
-    expect(parsedArgv.upload?.organization).toEqual(
-      overrides['upload.organization'],
     );
-    expect(parsedArgv.upload?.project).toEqual(overrides['upload.project']);
-    expect(parsedArgv.upload?.apiKey).toEqual(overrides['upload.apiKey']);
-    expect(parsedArgv.upload?.server).toEqual(overrides['upload.server']);
-    expect(parsedArgv.plugins).toEqual(expect.any(Array));
-    expect(parsedArgv.plugins[0]?.slug).toBe('plugin-1');
-    expect(parsedArgv.categories).toEqual(expect.any(Array));
+    await yargsCli(
+      [
+        'print-config',
+        '--verbose',
+        `--config=${configPath}`,
+        '--persist.outputDir=destinationDir',
+      ],
+      { ...DEFAULT_CLI_CONFIGURATION, commands: [yargsConfigCommandObject()] },
+    ).parseAsync();
+
+    expect(logSpy).not.toHaveBeenCalledWith(expect.stringContaining('"$0":'));
+    expect(logSpy).not.toHaveBeenCalledWith(expect.stringContaining('"_":'));
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining('"outputDir": "destinationDir"'),
+    );
+    expect(logSpy).not.toHaveBeenCalledWith(
+      expect.stringContaining('"output-dir":'),
+    );
   });
 });
