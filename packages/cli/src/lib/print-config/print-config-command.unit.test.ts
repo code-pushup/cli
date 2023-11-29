3a463bb66e09dd809109da829e07cff1da070691
Katerina Pilatova <katerina.pilatova@flowup.cz>
2023-11-29 13:05:52 +0100

    test(cli): extract and configure mock setup, remove integration setup

--- packages/cli/src/lib/print-config/print-config-command.unit.test.ts
+++ packages/cli/src/lib/print-config/print-config-command.unit.test.ts
@@ -1,47 +1,17 @@
 import { vol } from 'memfs';
-import { SpyInstance, describe, expect } from 'vitest';
+import { beforeEach, describe, expect } from 'vitest';
 import { DEFAULT_CLI_CONFIGURATION } from '../../../mocks/constants';
 import { yargsCli } from '../yargs-cli';
 import { yargsConfigCommandObject } from './print-config-command';
 
-// Mock file system API's
-vi.mock('fs', async () => {
-  const memfs: typeof import('memfs') = await vi.importActual('memfs');
-  return memfs.fs;
-});
-vi.mock('fs/promises', async () => {
-  const memfs: typeof import('memfs') = await vi.importActual('memfs');
-  return memfs.fs.promises;
-});
-
-// Mock bundleRequire inside importEsmModule used for fetching config
-vi.mock('bundle-require', async () => {
-  const { CORE_CONFIG_MOCK }: typeof import('@code-pushup/testing-utils') =
-    await vi.importActual('@code-pushup/testing-utils');
-  return {
-    bundleRequire: vi
-      .fn()
-      .mockResolvedValue({ mod: { default: CORE_CONFIG_MOCK } }),
-  };
-});
-
 describe('print-config-command', () => {
-  let logSpy: SpyInstance;
-
   beforeEach(() => {
-    vi.clearAllMocks();
-    logSpy = vi.spyOn(console, 'log');
-    vol.reset();
     vol.fromJSON(
       {
         'code-pushup.config.ts': '', // only needs to exist for stat inside readCodePushupConfig
       },
       '/test',
     );
-  });
-
-  afterEach(() => {
-    logSpy.mockRestore();
   });
 
   it('should filter out meta arguments and kebab duplicates', async () => {
@@ -55,12 +25,16 @@ describe('print-config-command', () => {
       { ...DEFAULT_CLI_CONFIGURATION, commands: [yargsConfigCommandObject()] },
     ).parseAsync();
 
-    expect(logSpy).not.toHaveBeenCalledWith(expect.stringContaining('"$0":'));
-    expect(logSpy).not.toHaveBeenCalledWith(expect.stringContaining('"_":'));
-    expect(logSpy).toHaveBeenCalledWith(
+    expect(console.log).not.toHaveBeenCalledWith(
+      expect.stringContaining('"$0":'),
+    );
+    expect(console.log).not.toHaveBeenCalledWith(
+      expect.stringContaining('"_":'),
+    );
+    expect(console.log).toHaveBeenCalledWith(
       expect.stringContaining('"outputDir": "destinationDir"'),
     );
-    expect(logSpy).not.toHaveBeenCalledWith(
+    expect(console.log).not.toHaveBeenCalledWith(
       expect.stringContaining('"output-dir":'),
     );
   });
---
+++ testing-utils/src/lib/setup/portal-client.mock.ts
@@ -0,0 +1,14 @@
+import { vi } from 'vitest';
+import { ReportFragment } from '@code-pushup/portal-client';
+
+vi.mock('@code-pushup/portal-client', async () => {
+  const module: typeof import('@code-pushup/portal-client') =
+    await vi.importActual('@code-pushup/portal-client');
+
+  return {
+    ...module,
+    uploadToPortal: vi.fn(
+      async () => ({ packageName: '@code-pushup/cli' } as ReportFragment),
+    ),
+  };
+});
