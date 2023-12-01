38b29402f112dc5bc93e38dcf17d4c5e5b6ccd10
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2023-12-02 00:05:21 +0100

    Update testing-utils/src/lib/setup/fs.mock.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- testing-utils/src/lib/setup/fs.mock.ts
+++ testing-utils/src/lib/setup/fs.mock.ts
@@ -1,6 +1,5 @@
 import { vi } from 'vitest';
 
-console.log('setup fs mock with memfs');
 vi.mock('fs', async () => {
   const memfs: typeof import('memfs') = await vi.importActual('memfs');
   return memfs.fs;
