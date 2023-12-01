0d7a868a43a46dc3ed3275e7e8e8c7adfdf08542
Matěj Chalk <matej.chalk@flowup.cz>
2023-12-01 19:25:56 +0100

    test(plugin-eslint): unit test utilities for Nx helpers

---
+++ packages/plugin-eslint/src/lib/nx/utils.unit.test.ts
@@ -0,0 +1,71 @@
+import { vol } from 'memfs';
+import type { SpyInstance } from 'vitest';
+import { findCodePushupEslintrc } from './utils';
+
+describe('find code-pushup.eslintrc.* file', () => {
+  const MEMFS_VOLUME = '/test';
+  let cwdSpy: SpyInstance;
+
+  beforeAll(() => {
+    cwdSpy = vi.spyOn(process, 'cwd').mockReturnValue(MEMFS_VOLUME);
+  });
+
+  afterAll(() => {
+    cwdSpy.mockRestore();
+  });
+
+  it('should find code-pushup.eslinrc.json', async () => {
+    vol.fromJSON(
+      {
+        'packages/cli/code-pushup.eslintrc.json':
+          '{ "extends": "@code-pushup" }',
+        'packages/core/code-pushup.eslintrc.json':
+          '{ "extends": "@code-pushup" }',
+      },
+      MEMFS_VOLUME,
+    );
+
+    await expect(
+      findCodePushupEslintrc({ root: 'packages/cli' }),
+    ).resolves.toBe('./packages/cli/code-pushup.eslintrc.json');
+  });
+
+  it('should find code-pushup.eslinrc.js', async () => {
+    vol.fromJSON(
+      {
+        'packages/cli/code-pushup.eslintrc.json':
+          '{ "extends": "@code-pushup" }',
+        'packages/core/code-pushup.eslintrc.js':
+          "module.exports = { extends: '@code-pushup' };",
+      },
+      MEMFS_VOLUME,
+    );
+
+    await expect(
+      findCodePushupEslintrc({ root: 'packages/core' }),
+    ).resolves.toBe('./packages/core/code-pushup.eslintrc.js');
+  });
+
+  it('should look for JavaScript extensions before YAML', async () => {
+    vol.fromJSON(
+      {
+        'libs/utils/code-pushup.eslintrc.cjs':
+          "module.exports = { extends: '@code-pushup' };",
+        'libs/utils/code-pushup.eslintrc.yml': '- extends: "@code-pushup"\n',
+      },
+      MEMFS_VOLUME,
+    );
+
+    await expect(findCodePushupEslintrc({ root: 'libs/utils' })).resolves.toBe(
+      './libs/utils/code-pushup.eslintrc.cjs',
+    );
+  });
+
+  it('should return null if not found', async () => {
+    vol.fromJSON({}, MEMFS_VOLUME);
+
+    await expect(
+      findCodePushupEslintrc({ root: 'libs/utils' }),
+    ).resolves.toBeNull();
+  });
+});
