eda2118fbdf9153284a3ddce1d1a3b679163e963
Katerina Pilatova <katerina.pilatova@flowup.cz>
2023-12-05 19:40:28 +0100

    test(testing-utils): point cwd to in-memory fs root in mock setup

--- testing-utils/src/lib/setup/fs.mock.ts
+++ testing-utils/src/lib/setup/fs.mock.ts
@@ -1,4 +1,5 @@
-import { vi } from 'vitest';
+import { SpyInstance, afterEach, beforeEach, vi } from 'vitest';
+import { MEMFS_VOLUME } from '../memfs';
 
 vi.mock('fs', async () => {
   const memfs: typeof import('memfs') = await vi.importActual('memfs');
@@ -7,4 +8,14 @@ vi.mock('fs', async () => {
 vi.mock('fs/promises', async () => {
   const memfs: typeof import('memfs') = await vi.importActual('memfs');
   return memfs.fs.promises;
+});
+
+let cwdSpy: SpyInstance;
+
+beforeEach(() => {
+  cwdSpy = vi.spyOn(process, 'cwd').mockReturnValue(MEMFS_VOLUME);
+});
+
+afterEach(() => {
+  cwdSpy.mockRestore();
 });
