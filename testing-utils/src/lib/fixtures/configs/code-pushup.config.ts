3942ff11568a061eb0082aff0ccc2fe7ce3fbd0d
Michael <michael.hladky@push-based.io>
2023-12-12 13:29:20 +0100

    refactor(models): ad comments

--- testing-utils/src/lib/fixtures/configs/code-pushup.config.ts
+++ testing-utils/src/lib/fixtures/configs/code-pushup.config.ts
@@ -1,6 +1,7 @@
 import { type RcConfig } from '@code-pushup/models';
 
 export default {
+  // @TODO remove ad we have defaults
   persist: { filename: 'output.json' },
   upload: {
     organization: 'code-pushup',
