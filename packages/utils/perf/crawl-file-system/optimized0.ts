e96670ef2fffe9a003948948fc73863acc1dc34b
Michael <michael.hladky@push-based.io>
2023-12-12 13:48:34 +0100

    refactor(utils): remove lib

--- packages/utils/perf/crawl-file-system/optimized0.ts
+++ packages/utils/perf/crawl-file-system/optimized0.ts
@@ -1,9 +1,8 @@
-import { fastFindInFiles } from 'fast-find-in-files';
 import { CrawlFileSystemOptions } from '../../src';
 
 export function crawlFileSystemOptimized0<T = string>(
   options: CrawlFileSystemOptions<T>,
 ): Promise<T[]> {
   const { directory, pattern: needle = '@TODO' } = options;
-  return fastFindInFiles({ directory, needle }) as unknown as Promise<T[]>;
+  return Promise.resolve([directory, needle] as T[]);
 }
