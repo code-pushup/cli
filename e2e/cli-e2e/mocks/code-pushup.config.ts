278cc4b5b6038d5e1ca9c6ebc8893b5c4695e053
Michael <michael.hladky@push-based.io>
2023-12-14 15:48:43 +0100

    test(cli-e2e): adjust config

--- e2e/cli-e2e/mocks/code-pushup.config.ts
+++ e2e/cli-e2e/mocks/code-pushup.config.ts
@@ -1,11 +1,10 @@
 // TODO: import plugins using NPM package names using local registry: https://github.com/flowup/quality-metrics-cli/issues/33
-import { join } from 'path';
 import eslintPlugin from '../../../dist/packages/plugin-eslint';
 import lighthousePlugin from '../../../dist/packages/plugin-lighthouse';
 import { CoreConfig } from '../../../packages/models/src';
 
 export default {
-  persist: { outputDir: join('tmp', 'ts') },
+  persist: { outputDir: 'tmp', filename: 'report', },
   upload: {
     organization: 'code-pushup',
     project: 'cli-ts',
