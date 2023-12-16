1ba2b4ad6e9c2c4429ee829754894fa797fd4080
Michael <michael.hladky@push-based.io>
2023-12-16 15:01:29 +0100

    wip

--- examples/plugins/src/package-json/src/integration/dependencies.audit.ts
+++ examples/plugins/src/package-json/src/integration/dependencies.audit.ts
@@ -1,5 +1,8 @@
 import { Audit, AuditOutput, Issue } from '@code-pushup/models';
-import { factorOf, findLineNumberInText } from '@code-pushup/utils';
+import {
+  factorOf,
+  findLineNumberInText,
+} from '../../../../../../dist/packages/utils';
 import {
   DependencyMap,
   DependencyType,
