dc00bc5b664cc51a11a4237c11e2a8ab435ca8a7
Michael <michael.hladky@push-based.io>
2023-12-18 00:32:12 +0100

    wip

--- packages/cli/src/lib/history/prompts.ts
+++ packages/cli/src/lib/history/prompts.ts
@@ -3,14 +3,14 @@ import inquirer from 'inquirer';
 export async function multiselect<T extends string>(options: {
   name: string;
   message: string;
-  choices: T[];
+  choices: T[] ;
 }): Promise<T[]> {
   const { name, message = 'Pick a choice:', choices } = options;
   const answer = (await inquirer.prompt({
     name,
     type: 'list',
     message,
-    choices,
+    choices: ['1','2','3'],
   })) as T[];
   return answer || [];
 }
