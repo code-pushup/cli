94c4f8045da9b41a7be186570a3e1a8df9c2b639
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2023-12-17 22:44:51 +0100

    Update packages/models/src/lib/implementation/constants.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/models/src/lib/implementation/constants.ts
+++ packages/models/src/lib/implementation/constants.ts
@@ -1,5 +1,5 @@
 import { Format } from '../persist-config';
 
 export const PERSIST_OUTPUT_DIR = '.code-pushup';
-export const PERSIST_FORMAT = ['json' as Format];
+export const PERSIST_FORMAT = ['json'] satisfies Format[];
 export const PERSIST_FILENAME = 'report';
