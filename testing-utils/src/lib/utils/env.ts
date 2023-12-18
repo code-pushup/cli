7578a30b63d650ef28d51e8cc0fc380464c2c966
Michael <michael.hladky@push-based.io>
2023-12-18 16:26:24 +0100

    wip

--- testing-utils/src/lib/utils/env.ts
+++ testing-utils/src/lib/utils/env.ts
@@ -10,7 +10,10 @@ export const envSchema = z.object({
 export type Env = z.infer<typeof envSchema>;
 
 export function uploadConfigFromEnv(): UploadConfig {
+  throw new Error(JSON.stringify(process.env));
   const env = envSchema.parse(process.env as Env);
+  throw new Error(JSON.stringify(env));
+
   return {
     server: env.CP_SERVER,
     apiKey: env.CP_API_KEY,
