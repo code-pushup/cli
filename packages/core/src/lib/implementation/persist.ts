885d25b45739288732a5b7fac3b77b81513aaf04
Michael <michael.hladky@push-based.io>
2023-12-18 02:29:38 +0100

    wip

--- packages/core/src/lib/implementation/persist.ts
+++ packages/core/src/lib/implementation/persist.ts
@@ -28,6 +28,7 @@ export async function persistReport(
   config: CoreConfig,
 ): Promise<MultipleFileResults> {
   const { persist } = config;
+  console.log('persist: ', persist);
   const outputDir = persist.outputDir;
   const filename = persist.filename;
   const format = persist.format ?? [];
