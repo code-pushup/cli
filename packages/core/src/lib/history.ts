085fa462f5bcc9b573f4bc2eac7cd8bca0d5df75
Michael <michael.hladky@push-based.io>
2023-12-18 01:13:42 +0100

    wip

--- packages/core/src/lib/history.ts
+++ packages/core/src/lib/history.ts
@@ -3,6 +3,7 @@ import { CoreConfig } from '@code-pushup/models';
 import { getProgressBar, getStartDuration, git } from '@code-pushup/utils';
 import { collectAndPersistReports } from './collect-and-persist';
 import { GlobalOptions } from './types';
+import {UploadOptions} from "./upload";
 
 export type HistoryOptions = Required<CoreConfig> & GlobalOptions;
 
@@ -33,16 +34,16 @@ export async function history(
         filename: `${commit}-report`,
       },
     });
-    /*
+
     const { upload } = config as unknown as UploadOptions;
     if (upload) {
       console.warn('Upload skipped because configuration is not set.'); // @TODO log verbose
     } else {
-      progress.updateTitle(`Upload ${commit}`);
+      progressBar?.updateTitle(`Upload ${commit}`);
       // await uploadToServer(config as unknown as UploadOptions);
       result['upload'] = new Date().toISOString();
     }
-*/
+    /**/
     reports.push({
       [join(config.persist.filename)]: result,
     });
