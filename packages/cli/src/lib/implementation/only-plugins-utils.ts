7048e63e7975ad10ca22b9808e37deeb51cd031b
Michael <michael.hladky@push-based.io>
2023-12-19 17:02:43 +0100

    refactor(cli): adjust only plugin utils login

--- packages/cli/src/lib/implementation/only-plugins-utils.ts
+++ packages/cli/src/lib/implementation/only-plugins-utils.ts
@@ -28,15 +28,14 @@ export function filterCategoryByOnlyPluginsOption(
     category.refs.every(ref => {
       const isNotSkipped = onlyPlugins.includes(ref.slug);
 
-      if (!isNotSkipped) {
-        verbose &&
-          console.info(
-            `${chalk.yellow('⚠')} Category "${
-              category.title
-            }" is ignored because it references audits from skipped plugin "${
-              ref.slug
-            }"`,
-          );
+      if (!isNotSkipped && verbose) {
+        console.info(
+          `${chalk.yellow('⚠')} Category "${
+            category.title
+          }" is ignored because it references audits from skipped plugin "${
+            ref.slug
+          }"`,
+        );
       }
 
       return isNotSkipped;
@@ -55,16 +54,15 @@ export function validateOnlyPluginsOption(
     ? onlyPlugins.filter(plugin => !plugins.some(({ slug }) => slug === plugin))
     : [];
 
-  if (missingPlugins.length > 0) {
-    verbose &&
-      console.warn(
-        `${chalk.yellow(
-          '⚠',
-        )} The --onlyPlugin argument references plugins with "${missingPlugins.join(
-          '", "',
-        )}" slugs, but no such plugins are present in the configuration. Expected one of the following plugin slugs: "${plugins
-          .map(({ slug }) => slug)
-          .join('", "')}".`,
-      );
+  if (missingPlugins.length > 0 && verbose) {
+    console.warn(
+      `${chalk.yellow(
+        '⚠',
+      )} The --onlyPlugin argument references plugins with "${missingPlugins.join(
+        '", "',
+      )}" slugs, but no such plugins are present in the configuration. Expected one of the following plugin slugs: "${plugins
+        .map(({ slug }) => slug)
+        .join('", "')}".`,
+    );
   }
 }
