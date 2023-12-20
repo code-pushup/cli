fc024c0375176d5634ed9ab4c6962b98cd65a0d9
Michael Seredenko <misha.seredenko@push-based.io>
2023-12-20 17:38:06 +0100

    test(cli): add mock env variables for core config autoload integration test

--- packages/cli/src/lib/implementation/core-config.middleware.autoload.integration.test.ts
+++ packages/cli/src/lib/implementation/core-config.middleware.autoload.integration.test.ts
@@ -2,6 +2,21 @@ import { describe, expect } from 'vitest';
 import { coreConfigMiddleware } from './core-config.middleware';
 
 describe('configMiddleware-autoload', () => {
+  let originalEnv: NodeJS.ProcessEnv;
+
+  beforeEach(() => {
+    originalEnv = { ...process.env };
+
+    process.env['CP_SERVER'] = 'https://api.codepushup.com';
+    process.env['CP_API_KEY'] = 'apiKey';
+    process.env['CP_ORGANIZATION'] = 'org';
+    process.env['CP_PROJECT'] = 'cli';
+  });
+
+  afterEach(() => {
+    process.env = originalEnv;
+  });
+
   it('should load code-pushup.config.(ts|mjs|js) by default', async () => {
     const config = await coreConfigMiddleware({});
     expect(config?.upload?.project).toBe('cli');
