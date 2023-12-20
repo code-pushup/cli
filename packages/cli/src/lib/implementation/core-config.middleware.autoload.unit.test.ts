ac74ce334b92210220a61d45dcbe8d5ef7834877
Michael <michael.hladky@push-based.io>
2023-12-20 18:07:28 +0100

    test(cli): fix test

--- packages/cli/src/lib/implementation/core-config.middleware.autoload.unit.test.ts
+++ packages/cli/src/lib/implementation/core-config.middleware.autoload.unit.test.ts
@@ -4,21 +4,6 @@ import { MINIMAL_CONFIG_MOCK } from '@code-pushup/testing-utils';
 import { coreConfigMiddleware } from './core-config.middleware';
 
 describe('configMiddleware-autoload', () => {
-  let originalEnv: NodeJS.ProcessEnv;
-
-  beforeEach(() => {
-    originalEnv = { ...process.env };
-
-    process.env['CP_SERVER'] = 'https://api.codepushup.com';
-    process.env['CP_API_KEY'] = 'apiKey';
-    process.env['CP_ORGANIZATION'] = 'org';
-    process.env['CP_PROJECT'] = 'cli';
-  });
-
-  afterEach(() => {
-    process.env = originalEnv;
-  });
-
   it('should load code-pushup.config.(ts|mjs|js) by default', async () => {
     vol.fromJSON({
       // this is only needed to pass the file API's, the config is mocked in bundleRequire
