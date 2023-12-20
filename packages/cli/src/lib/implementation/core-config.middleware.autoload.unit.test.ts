efab79a281a2af01ad3b7ce09eff25e37b8909fd
Michael <michael.hladky@push-based.io>
2023-12-20 18:06:57 +0100

    test(cli): fix test

--- packages/cli/src/lib/implementation/core-config.middleware.autoload.unit.test.ts
+++ packages/cli/src/lib/implementation/core-config.middleware.autoload.unit.test.ts
@@ -1,4 +1,6 @@
+import { vol } from 'memfs';
 import { describe, expect } from 'vitest';
+import { MINIMAL_CONFIG_MOCK } from '@code-pushup/testing-utils';
 import { coreConfigMiddleware } from './core-config.middleware';
 
 describe('configMiddleware-autoload', () => {
@@ -18,6 +20,10 @@ describe('configMiddleware-autoload', () => {
   });
 
   it('should load code-pushup.config.(ts|mjs|js) by default', async () => {
+    vol.fromJSON({
+      // this is only needed to pass the file API's, the config is mocked in bundleRequire
+      ['code-pushup.config.ts']: JSON.stringify(MINIMAL_CONFIG_MOCK),
+    });
     const config = await coreConfigMiddleware({});
     expect(config?.upload?.project).toBe('cli');
   });
