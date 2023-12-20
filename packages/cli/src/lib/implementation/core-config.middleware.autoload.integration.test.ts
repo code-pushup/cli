2ecc60849822217adae5d5ac3756a082a780b7f3
Michael <michael.hladky@push-based.io>
2023-12-20 15:49:29 +0100

    test(cli): fix test

--- packages/cli/src/lib/implementation/core-config.middleware.autoload.integration.test.ts
+++ packages/cli/src/lib/implementation/core-config.middleware.autoload.integration.test.ts
@@ -1,31 +1,7 @@
-import { vol } from 'memfs';
-import { afterEach, beforeEach, describe, expect, vi } from 'vitest';
-import { MEMFS_VOLUME } from '@code-pushup/testing-utils';
+import { describe, expect } from 'vitest';
 import { coreConfigMiddleware } from './core-config.middleware';
 
-vi.mock('fs', async () => {
-  const memfs: typeof import('memfs') = await vi.importActual('memfs');
-  return memfs.fs;
-});
-vi.mock('fs/promises', async () => {
-  const memfs: typeof import('memfs') = await vi.importActual('memfs');
-  return memfs.fs.promises;
-});
-
 describe('configMiddleware-autoload', () => {
-  beforeEach(() => {
-    vol.fromJSON(
-      {
-        ['code-pushup.config.ts']: '{}',
-      },
-      MEMFS_VOLUME,
-    );
-  });
-
-  afterEach(() => {
-    vol.reset();
-  });
-
   it('should load code-pushup.config.(ts|mjs|js) by default', async () => {
     const config = await coreConfigMiddleware({});
     expect(config?.upload?.project).toBe('cli');
