039d26647bc30c3749d5c94cd39a35e71f981164
Michael <michael.hladky@push-based.io>
2024-01-04 13:24:49 -0600

    test(cli): format

--- packages/cli/src/lib/implementation/core-config.middleware.unit.test.ts
+++ packages/cli/src/lib/implementation/core-config.middleware.unit.test.ts
@@ -16,15 +16,13 @@ vi.mock('bundle-require', async () => {
 describe('configMiddleware', () => {
   it('should load code-pushup.config.(ts|mjs|js) by default', async () => {
     vol.fromJSON({
-      // this is only needed to pass the file API's, the config is mocked in bundleRequire
+      // this is only needed to pass the file API's, the config file content is mocked in bundleRequire
       ['code-pushup.config.ts']: '',
     });
     const config = await coreConfigMiddleware({});
     expect(config?.upload?.project).toBe('cli');
   });
   it('should throw if no code-pushup.config.(ts|mjs|js) is present', async () => {
-    // It is indirectly testing if code-pushup.config.(ts|mjs|ts) is used.
-    // The reason for this is a hard to setup test
     await expect(coreConfigMiddleware({})).rejects.toThrow(
       'No file code-pushup.config',
     );
