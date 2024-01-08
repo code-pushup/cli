facaf08e152d70996f4cc921ccfdd24d0dfc6a8c
Michael Seredenko <misha.seredenko@push-based.io>
2024-01-08 10:17:26 +0100

    test(utils): update snapshots for report tests

--- packages/utils/src/lib/sort-report.integration.test.ts
+++ packages/utils/src/lib/sort-report.integration.test.ts
@@ -1,10 +1,10 @@
-import { report } from '@code-pushup/models/testing';
+import { minimalReport, reportMock } from '@code-pushup/models/testing';
 import { scoreReport } from './scoring';
 import { sortReport } from './sort-report';
 
 describe('sortReport', () => {
-  it('should sort the report', () => {
-    const sortedReport = sortReport(scoreReport(report()));
+  it('should sort the audits and groups of audits in categories, all audits and nested issues', () => {
+    const sortedReport = sortReport(scoreReport(reportMock()));
     expect(sortedReport).toMatchSnapshot();
   });
 });
