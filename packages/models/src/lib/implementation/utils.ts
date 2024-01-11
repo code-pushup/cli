94f22bf0ea12fdf89f36aebd75989cdd2c1e5f21
Katerina Pilatova <katerina.pilatova@flowup.cz>
2024-01-11 14:39:03 +0100

    refactor(models): require non-empty plugins, package,version is required for report, clean up tests

--- packages/models/src/lib/implementation/utils.ts
+++ packages/models/src/lib/implementation/utils.ts
@@ -1,3 +1,7 @@
+import type { CategoryConfig } from '../category-config';
+import type { PluginConfig } from '../plugin-config';
+import type { PluginReport } from '../report';
+
 /**
  * Regular expression to validate a slug for categories, plugins and audits.
  * - audit (e.g. 'max-lines')
@@ -54,4 +58,65 @@ export function errorItems(
 
 export function exists<T>(value: T): value is NonNullable<T> {
   return value != null;
+}
+
+/**
+ * Get category references that do not point to any audit or group
+ * @param categories
+ * @param plugins
+ * @returns Array of missing references.
+ */
+export function getMissingRefsForCategories(
+  categories: CategoryConfig[],
+  plugins: PluginConfig[] | PluginReport[],
+) {
+  const missingRefs: string[] = [];
+  const auditRefsFromCategory = categories.flatMap(({ refs }) =>
+    refs
+      .filter(({ type }) => type === 'audit')
+      .map(({ plugin, slug }) => `${plugin}/${slug}`),
+  );
+  const auditRefsFromPlugins = plugins.flatMap(
+    ({ audits, slug: pluginSlug }) => {
+      return audits.map(({ slug }) => `${pluginSlug}/${slug}`);
+    },
+  );
+  const missingAuditRefs = hasMissingStrings(
+    auditRefsFromCategory,
+    auditRefsFromPlugins,
+  );
+
+  if (Array.isArray(missingAuditRefs) && missingAuditRefs.length > 0) {
+    missingRefs.push(...missingAuditRefs);
+  }
+  const groupRefsFromCategory = categories.flatMap(({ refs }) =>
+    refs
+      .filter(({ type }) => type === 'group')
+      .map(({ plugin, slug }) => `${plugin}#${slug} (group)`),
+  );
+  const groupRefsFromPlugins = plugins.flatMap(
+    ({ groups, slug: pluginSlug }) => {
+      return Array.isArray(groups)
+        ? groups.map(({ slug }) => `${pluginSlug}#${slug} (group)`)
+        : [];
+    },
+  );
+  const missingGroupRefs = hasMissingStrings(
+    groupRefsFromCategory,
+    groupRefsFromPlugins,
+  );
+  if (Array.isArray(missingGroupRefs) && missingGroupRefs.length > 0) {
+    missingRefs.push(...missingGroupRefs);
+  }
+
+  return missingRefs.length ? missingRefs : false;
+}
+export function missingRefsForCategoriesErrorMsg(
+  categories: CategoryConfig[],
+  plugins: PluginConfig[] | PluginReport[],
+) {
+  const missingRefs = getMissingRefsForCategories(categories, plugins);
+  return `The following category references need to point to an audit or group: ${errorItems(
+    missingRefs,
+  )}`;
 }
--- packages/models/src/lib/report.ts
+++ packages/models/src/lib/report.ts
@@ -2,11 +2,17 @@ import { z } from 'zod';
 import { auditSchema } from './audit';
 import { auditOutputSchema } from './audit-output';
 import { categoryConfigSchema } from './category-config';
-import { groupSchema } from './group';
+import { Group, groupSchema } from './group';
 import {
   executionMetaSchema,
   packageVersionSchema,
 } from './implementation/schemas';
+import {
+  errorItems,
+  getMissingRefsForCategories,
+  hasMissingStrings,
+  missingRefsForCategoriesErrorMsg,
+} from './implementation/utils';
 import { pluginMetaSchema } from './plugin-config';
 
 export const auditReportSchema = auditSchema.merge(auditOutputSchema);
@@ -24,11 +30,40 @@ export const pluginReportSchema = pluginMetaSchema
       audits: z.array(auditReportSchema),
       groups: z.array(groupSchema).optional(),
     }),
+  )
+  .refine(
+    pluginReport =>
+      !getMissingRefsFromGroups(pluginReport.audits, pluginReport.groups ?? []),
+    pluginReport => ({
+      message: missingRefsFromGroupsErrorMsg(
+        pluginReport.audits,
+        pluginReport.groups ?? [],
+      ),
+    }),
   );
+
 export type PluginReport = z.infer<typeof pluginReportSchema>;
+
+// every listed group ref points to an audit within the plugin report
+function missingRefsFromGroupsErrorMsg(audits: AuditReport[], groups: Group[]) {
+  const missingRefs = getMissingRefsFromGroups(audits, groups);
+  return `group references need to point to an existing audit in this plugin report: ${errorItems(
+    missingRefs,
+  )}`;
+}
+
+function getMissingRefsFromGroups(audits: AuditReport[], groups: Group[]) {
+  return hasMissingStrings(
+    groups.flatMap(({ refs: auditRefs }) =>
+      auditRefs.map(({ slug: ref }) => ref),
+    ),
+    audits.map(({ slug }) => slug),
+  );
+}
 
 export const reportSchema = packageVersionSchema({
   versionDescription: 'NPM version of the CLI',
+  required: true,
 })
   .merge(
     executionMetaSchema({
@@ -40,10 +75,19 @@ export const reportSchema = packageVersionSchema({
     z.object(
       {
         categories: z.array(categoryConfigSchema),
-        plugins: z.array(pluginReportSchema),
+        plugins: z.array(pluginReportSchema).min(1),
       },
       { description: 'Collect output data' },
     ),
+  )
+  .refine(
+    report => !getMissingRefsForCategories(report.categories, report.plugins),
+    report => ({
+      message: missingRefsForCategoriesErrorMsg(
+        report.categories,
+        report.plugins,
+      ),
+    }),
   );
 
 export type Report = z.infer<typeof reportSchema>;
