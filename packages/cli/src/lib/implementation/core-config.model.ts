c6621921d2737e56ee578a8672c1bb19f8365397
Michael <michael.hladky@push-based.io>
2024-01-15 18:22:06 +0100

    fix lint

--- packages/cli/src/lib/implementation/core-config.model.ts
+++ packages/cli/src/lib/implementation/core-config.model.ts
@@ -1,18 +1,19 @@
 import { Format } from '@code-pushup/models';
 
+/* eslint-disable @typescript-eslint/naming-convention */
 export type PersistConfigCliOptions = {
   'persist.outputDir': string;
   'persist.filename': string;
   'persist.format': Format;
 };
 
+/* eslint-disable @typescript-eslint/naming-convention */
 export type UploadConfigCliOptions = {
   'upload.organization': string;
   'upload.project': string;
   'upload.apiKey': string;
   'upload.server': string;
 };
-/* eslint-enable @typescript-eslint/naming-convention */
 
 export type CoreConfigCliOptions = PersistConfigCliOptions &
   UploadConfigCliOptions;
