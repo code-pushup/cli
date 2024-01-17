d180f6f0b884f03dd455757b76262031b1c7b701
Michael <michael.hladky@push-based.io>
2024-01-17 22:53:14 +0100

    test

--- e2e/cli-e2e/tests/collect.e2e.test.ts
+++ e2e/cli-e2e/tests/collect.e2e.test.ts
@@ -3,7 +3,7 @@ import { executeProcess, readJsonFile, readTextFile } from '@code-pushup/utils';
 
 describe('CLI collect', () => {
   const exampleCategoryTitle = 'Code style';
-  const exampleAuditTitle = 'Require `const` declarations for variables';
+  const exampleAuditTitle = 'PushUp';
 
   /* eslint-disable @typescript-eslint/no-unused-vars */
   const omitVariableData = ({
