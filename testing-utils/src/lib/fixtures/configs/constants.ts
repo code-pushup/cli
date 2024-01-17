4c698615f37787850f504ceba8be7adabf294269
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-01-17 18:56:19 +0100

    Update testing-utils/src/lib/fixtures/configs/constants.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- testing-utils/src/lib/fixtures/configs/constants.ts
+++ testing-utils/src/lib/fixtures/configs/constants.ts
@@ -3,4 +3,4 @@ export const CORE_CONFIG_NAMES = [
   'persist' as const,
   'persist-only-filename' as const,
 ];
-export type CoreConfigNames = (typeof CORE_CONFIG_NAMES)[number];
+export type CoreConfigName = (typeof CORE_CONFIG_NAMES)[number];
