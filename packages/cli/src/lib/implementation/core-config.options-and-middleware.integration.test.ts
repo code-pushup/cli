c5a1fde7ee5e00040af5b3777adb937348e3d830
Michael <michael.hladky@push-based.io>
2024-01-24 22:58:42 +0100

    wip

--- packages/cli/src/lib/implementation/core-config.options-and-middleware.integration.test.ts
+++ packages/cli/src/lib/implementation/core-config.options-and-middleware.integration.test.ts
@@ -1,13 +1,51 @@
-import {describe, expect} from 'vitest';
-import {CoreConfig, PERSIST_FILENAME, PERSIST_FORMAT, PERSIST_OUTPUT_DIR,} from '@code-pushup/models';
-import {yargsCli} from '../yargs-cli';
-import {coreConfigMiddleware} from './core-config.middleware';
-import {yargsCoreConfigOptionsDefinition} from './core-config.options';
+import { vol } from 'memfs';
+import {
+  SpyInstance,
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  vi,
+} from 'vitest';
+import {
+  CoreConfig,
+  PERSIST_FILENAME,
+  PERSIST_FORMAT,
+  PERSIST_OUTPUT_DIR,
+} from '@code-pushup/models';
+import { MEMFS_VOLUME } from '@code-pushup/testing-utils';
+import { yargsCli } from '../yargs-cli';
+import { coreConfigMiddleware } from './core-config.middleware';
+import { yargsCoreConfigOptionsDefinition } from './core-config.options';
+
+vi.mock('fs', async () => {
+  const memfs: typeof import('memfs') = await vi.importActual('memfs');
+  return memfs.fs;
+});
+vi.mock('fs/promises', async () => {
+  const memfs: typeof import('memfs') = await vi.importActual('memfs');
+  return memfs.fs.promises;
+});
+
+let cwdSpy: SpyInstance;
 
 describe('cliWithConfigOptionsAndMiddleware', () => {
+  beforeEach(() => {
+    cwdSpy = vi.spyOn(process, 'cwd').mockReturnValue(MEMFS_VOLUME);
+    vol.fromJSON(
+      {
+        'code-pushup.persist-only-filename.config.ts': '',
+      },
+      MEMFS_VOLUME,
+    );
+  });
+
+  afterEach(() => {
+    cwdSpy.mockRestore();
+  });
 
   it('should take default values for persist when no argument is given in rc or over the cli', async () => {
-    const {persist} = await yargsCli<CoreConfig>(
+    const { persist } = await yargsCli<CoreConfig>(
       [
         '--persist.outputDir=tmp-cli',
         '--persist.format=md',
@@ -15,20 +53,22 @@ describe('cliWithConfigOptionsAndMiddleware', () => {
         '--config=./minimal.config.ts', // cwd is also mocked
       ],
       {
-        options: {...yargsCoreConfigOptionsDefinition()},
-        middlewares: [{middlewareFunction: coreConfigMiddleware}],
+        options: { ...yargsCoreConfigOptionsDefinition() },
+        middlewares: [{ middlewareFunction: coreConfigMiddleware }],
       },
     ).parseAsync();
 
-    expect(persist).toEqual(expect.objectContaining({
-      outputDir: PERSIST_OUTPUT_DIR,
-      format: PERSIST_FORMAT,
-      filename: PERSIST_FILENAME,
-    }));
+    expect(persist).toEqual(
+      expect.objectContaining({
+        outputDir: PERSIST_OUTPUT_DIR,
+        format: PERSIST_FORMAT,
+        filename: PERSIST_FILENAME,
+      }),
+    );
   });
 
   it('should take values for persist when provided over the cli', async () => {
-    const {persist} = await yargsCli<CoreConfig>(
+    const { persist } = await yargsCli<CoreConfig>(
       [
         '--persist.outputDir=tmp-cli',
         '--persist.format=md',
@@ -36,20 +76,22 @@ describe('cliWithConfigOptionsAndMiddleware', () => {
         `--config=./test/code-pushup.persist-only-filename.config.ts`,
       ],
       {
-        options: {...yargsCoreConfigOptionsDefinition()},
-        middlewares: [{middlewareFunction: coreConfigMiddleware}],
+        options: { ...yargsCoreConfigOptionsDefinition() },
+        middlewares: [{ middlewareFunction: coreConfigMiddleware }],
       },
     ).parseAsync();
 
-    expect(persist).toEqual(expect.objectContaining({
-      outputDir: 'tmp-cli',
-      format: 'md',
-      filename: 'cli-report'
-    }));
+    expect(persist).toEqual(
+      expect.objectContaining({
+        outputDir: 'tmp-cli',
+        format: 'md',
+        filename: 'cli-report',
+      }),
+    );
   });
 
   it('should take values for persist when provided over the rc', async () => {
-    const {persist} = await yargsCli<CoreConfig>(
+    const { persist } = await yargsCli<CoreConfig>(
       [
         '--persist.outputDir=tmp-cli',
         '--persist.format=md',
@@ -57,20 +99,22 @@ describe('cliWithConfigOptionsAndMiddleware', () => {
         `--config=./code-pushup.persist.config.ts`,
       ],
       {
-        options: {...yargsCoreConfigOptionsDefinition()},
-        middlewares: [{middlewareFunction: coreConfigMiddleware}],
+        options: { ...yargsCoreConfigOptionsDefinition() },
+        middlewares: [{ middlewareFunction: coreConfigMiddleware }],
       },
     ).parseAsync();
 
-    expect(persist).toEqual(expect.objectContaining({
-      outputDir: 'rc-tmp',
-      format: ['json', 'md'],
-      filename: 'rc-report',
-    }));
+    expect(persist).toEqual(
+      expect.objectContaining({
+        outputDir: 'rc-tmp',
+        format: ['json', 'md'],
+        filename: 'rc-report',
+      }),
+    );
   });
 
   it('should take the CLI values as precedence if both, the rc config and the CLI arguments are given for persist', async () => {
-    const {persist} = await yargsCli<CoreConfig>(
+    const { persist } = await yargsCli<CoreConfig>(
       [
         '--persist.outputDir=tmddp-cli',
         '--persist.format=md',
@@ -78,35 +122,39 @@ describe('cliWithConfigOptionsAndMiddleware', () => {
         '--config=code-pushup.persist-only-filename.config', // cwd is also mocked
       ],
       {
-        options: {...yargsCoreConfigOptionsDefinition()},
-        middlewares: [{middlewareFunction: coreConfigMiddleware}],
+        options: { ...yargsCoreConfigOptionsDefinition() },
+        middlewares: [{ middlewareFunction: coreConfigMiddleware }],
       },
     ).parseAsync();
 
-    expect(persist).toEqual(expect.objectContaining({
-      outputDir: 'tmp-cli',
-      format: 'md',
-      filename: 'cli-report',
-    }));
+    expect(persist).toEqual(
+      expect.objectContaining({
+        outputDir: 'tmp-cli',
+        format: 'md',
+        filename: 'cli-report',
+      }),
+    );
   });
 
   it('should take values for persist when provided over both, the rc config, the cli and the defaults', async () => {
-    const {persist} = await yargsCli<CoreConfig>(
+    const { persist } = await yargsCli<CoreConfig>(
       [
         '--persist.outputDir=cli-outputdir',
         '--config=./persist-only-filename.config.ts', // cwd is also mocked
       ],
       {
-        options: {...yargsCoreConfigOptionsDefinition()},
-        middlewares: [{middlewareFunction: coreConfigMiddleware}],
+        options: { ...yargsCoreConfigOptionsDefinition() },
+        middlewares: [{ middlewareFunction: coreConfigMiddleware }],
       },
     ).parseAsync();
 
-    expect(persist).toEqual(expect.objectContaining({
-      outputDir: 'tmp-cli',
-      format: 'md',
-      filename: 'cli-report',
-    }));
+    expect(persist).toEqual(
+      expect.objectContaining({
+        outputDir: 'tmp-cli',
+        format: 'md',
+        filename: 'cli-report',
+      }),
+    );
   });
 
   // TODO THIS IS AN EXAMPLE
@@ -120,8 +168,8 @@ describe('cliWithConfigOptionsAndMiddleware', () => {
           '--config=./code-pushup.persist-only-filename.config.ts', // cwd is also mocked
         ],
         {
-          options: {...yargsCoreConfigOptionsDefinition()},
-          middlewares: [{middlewareFunction: coreConfigMiddleware}],
+          options: { ...yargsCoreConfigOptionsDefinition() },
+          middlewares: [{ middlewareFunction: coreConfigMiddleware }],
         },
       ).parseAsync(),
     ).resolves.toEqual(
@@ -134,7 +182,4 @@ describe('cliWithConfigOptionsAndMiddleware', () => {
       }),
     );
   });
-
 });
-
-
