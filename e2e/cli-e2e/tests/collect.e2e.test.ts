dfab86ef5101af460d29920a9e488af479297162
Michael <michael.hladky@push-based.io>
2024-01-25 16:20:37 +0100

    test(cli): fix test

--- e2e/cli-e2e/tests/collect.e2e.test.ts
+++ e2e/cli-e2e/tests/collect.e2e.test.ts
@@ -3,7 +3,7 @@ import { executeProcess, readJsonFile, readTextFile } from '@code-pushup/utils';
 
 describe('CLI collect', () => {
   const exampleCategoryTitle = 'Code style';
-  const exampleAuditTitle = 'Require `const` declarations for variables';
+  const exampleAuditTitle = 'Require `const`';
 
   /* eslint-disable @typescript-eslint/no-unused-vars */
   const omitVariableData = ({
