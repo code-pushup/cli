46defe9b8fe5ba5724c050348045b13441a2ecda
Michael <michael.hladky@push-based.io>
2024-01-25 12:26:06 +0100

    test(cli): add tests for config loading

--- packages/cli/src/lib/implementation/core-config.middleware.integration.test.ts
+++ packages/cli/src/lib/implementation/core-config.middleware.integration.test.ts
@@ -1,28 +1,49 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { describe, expect } from 'vitest';
+import { describe, expect, vi } from 'vitest';
+import { ConfigPathError } from '@code-pushup/core';
+import { CoreConfig } from '@code-pushup/models';
+import { CORE_CONFIG_MOCK } from '@code-pushup/testing-utils';
 import { coreConfigMiddleware } from './core-config.middleware';
 
-const configDirPath = join(
-  fileURLToPath(dirname(import.meta.url)),
-  '..',
-  '..',
-  '..',
-  '..',
-  '..',
-  'testing-utils',
-  'src',
-  'lib',
-  'fixtures',
-  'configs',
-);
+vi.mock('@code-pushup/core', async () => {
+  const core = await vi.importActual('@code-pushup/core');
+  return {
+    ...(core as object),
+    readRcByPath: vi.fn().mockImplementation((filepath: string): CoreConfig => {
+      const extension = filepath.split('.');
+      if (filepath.includes('throw-error')) {
+        throw new ConfigPathError(filepath);
+      }
+      return {
+        ...CORE_CONFIG_MOCK,
+        upload: {
+          ...CORE_CONFIG_MOCK.upload,
+          project: `cli-${extension}`,
+        },
+      };
+    }),
+    autoloadRc: vi.fn().mockImplementation((): CoreConfig => {
+      return {
+        ...CORE_CONFIG_MOCK,
+        upload: {
+          ...CORE_CONFIG_MOCK.upload,
+          project: `cli-autoload`,
+        },
+      };
+    }),
+  };
+});
 
 describe('coreConfigMiddleware', () => {
+  it('should load code-pushup.config.(ts|mjs|js) by default', async () => {
+    const config = await coreConfigMiddleware({});
+    expect(config?.upload?.project).toBe('cli-autoload');
+  });
+
   it.each(['ts', 'mjs', 'js'])(
     'should load a valid .%s config',
     async extension => {
       const config = await coreConfigMiddleware({
-        config: join(configDirPath, `code-pushup.config.${extension}`),
+        config: `code-pushup.config.${extension}`,
       });
       expect(config.config).toContain(`code-pushup.config.${extension}`);
       expect(config.upload?.project).toContain(extension);
@@ -31,7 +52,7 @@ describe('coreConfigMiddleware', () => {
 
   it('should throw with invalid config path', async () => {
     await expect(
-      coreConfigMiddleware({ config: 'wrong/path/to/config' }),
+      coreConfigMiddleware({ config: 'throw-error' }),
     ).rejects.toThrow(/Provided path .* is not valid./);
   });
 });
