207bf077335986f9a58335ebd406e64409c2dc7b
Katka Pilátová <katerina.pilatova@flowup.cz>
2024-01-27 11:32:41 +0100

    chore: clean up core config tests, remove obsolete logic (#435)
    - fixed unit tests for `configMiddleware` to check only the immediate
    underlying logic
    - `readCodePushUpConfig` function and related tests were removed as they
    are no longer used (replaced by`readRcByPath`)
    - polished comments

--- packages/cli/src/lib/implementation/core-config.middleware.unit.test.ts
+++ packages/cli/src/lib/implementation/core-config.middleware.unit.test.ts
@@ -1,58 +1,27 @@
 import { describe, expect, vi } from 'vitest';
-import { ConfigPathError } from '@code-pushup/core';
-import { CoreConfig } from '@code-pushup/models';
-import { CORE_CONFIG_MOCK } from '@code-pushup/testing-utils';
+import { autoloadRc, readRcByPath } from '@code-pushup/core';
 import { coreConfigMiddleware } from './core-config.middleware';
 
 vi.mock('@code-pushup/core', async () => {
+  const { CORE_CONFIG_MOCK }: typeof import('@code-pushup/testing-utils') =
+    await vi.importActual('@code-pushup/testing-utils');
   const core: object = await vi.importActual('@code-pushup/core');
   return {
     ...core,
-    readRcByPath: vi.fn().mockImplementation((filepath: string): CoreConfig => {
-      const extension = filepath.split('.');
-      if (filepath.includes('throw-error')) {
-        throw new ConfigPathError(filepath);
-      }
-      return {
-        ...CORE_CONFIG_MOCK,
-        upload: {
-          ...CORE_CONFIG_MOCK.upload,
-          project: `cli-${extension}`,
-        },
-      };
-    }),
-    autoloadRc: vi.fn().mockImplementation(
-      (): CoreConfig => ({
-        ...CORE_CONFIG_MOCK,
-        upload: {
-          ...CORE_CONFIG_MOCK.upload,
-          project: `cli-autoload`,
-        },
-      }),
-    ),
+    readRcByPath: vi.fn().mockResolvedValue(CORE_CONFIG_MOCK),
+    autoloadRc: vi.fn().mockResolvedValue(CORE_CONFIG_MOCK),
   };
 });
 
 describe('coreConfigMiddleware', () => {
-  it('should load code-pushup.config.(ts|mjs|js) by default', async () => {
-    const config = await coreConfigMiddleware({});
-    expect(config?.upload?.project).toBe('cli-autoload');
+  it('should attempt to load code-pushup.config.(ts|mjs|js) by default', async () => {
+    await coreConfigMiddleware({});
+    expect(autoloadRc).toHaveBeenCalled();
   });
 
-  it.each(['ts', 'mjs', 'js'])(
-    'should load a valid .%s config',
-    async extension => {
-      const config = await coreConfigMiddleware({
-        config: `code-pushup.config.${extension}`,
-      });
-      expect(config.config).toContain(`code-pushup.config.${extension}`);
-      expect(config.upload?.project).toContain(extension);
-    },
-  );
-
-  it('should throw with invalid config path', async () => {
-    await expect(
-      coreConfigMiddleware({ config: 'throw-error' }),
-    ).rejects.toThrow(/Provided path .* is not valid./);
+  it('should directly attempt to load passed config', async () => {
+    await coreConfigMiddleware({ config: 'cli/custom-config.mjs' });
+    expect(autoloadRc).not.toHaveBeenCalled();
+    expect(readRcByPath).toHaveBeenCalledWith('cli/custom-config.mjs');
   });
 });
