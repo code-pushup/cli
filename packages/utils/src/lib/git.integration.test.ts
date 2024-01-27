2b71832ccae8b55d1ad01f7383bd3eb8d6adc9cf
Michael <michael.hladky@push-based.io>
2024-01-27 10:22:58 +0100

    wip

--- packages/utils/src/lib/git.integration.test.ts
+++ packages/utils/src/lib/git.integration.test.ts
@@ -20,12 +20,12 @@ describe('getLatestCommit', () => {
 
 describe('branchHasChanges', () => {
   it('should log changes if some are given', async () => {
+    await makeStatusDirty();
     await expect(branchHasChanges()).resolves.toEqual(false);
+    await makeStatusClean();
   });
   it('should log no changes if non are given', async () => {
-    await makeStatusDirty();
     await expect(branchHasChanges()).resolves.toEqual(true);
-    await makeStatusClean();
   });
 });
 
