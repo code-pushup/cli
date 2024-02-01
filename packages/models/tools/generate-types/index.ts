3a71d96628dabf28d5f5fabc89c1a7b1519600dd
Michael <michael.hladky@push-based.io>
2024-02-01 18:46:38 +0100

    fix lint

--- packages/models/tools/generate-types/index.ts
+++ packages/models/tools/generate-types/index.ts
@@ -7,7 +7,9 @@ const nodeString = generateTypesString({
   reportSchema,
 });
 // eslint disable-next-line
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
 const destDir: string = join(process.cwd(), '..', 'docs');
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
 const dest: string = join(destDir, 'core-config-types.md');
 const content: string = generateMdFile(nodeString);
-safeWriteFile(dest, content);
+void safeWriteFile(dest, content);
