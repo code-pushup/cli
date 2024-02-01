9090654254ae79db6118262161555be5e170ac8b
Michael <michael.hladky@push-based.io>
2024-02-01 18:59:36 +0100

    test: adopt error

--- examples/plugins/src/lighthouse/src/utils.unit.test.ts
+++ examples/plugins/src/lighthouse/src/utils.unit.test.ts
@@ -79,8 +79,8 @@ describe('getLighthouseCliArguments', () => {
     ).toEqual(expect.arrayContaining(['https://code-pushup-portal.com']));
   });
 
-  it('should parse options for headless to new if true is given', () => {
-    const pluginConfig = create({
+  it('should parse options for headless to new if true is given', async () => {
+    const pluginConfig = await create({
       url: LIGHTHOUSE_URL,
       headless: true,
     });
@@ -89,8 +89,8 @@ describe('getLighthouseCliArguments', () => {
     );
   });
 
-  it('should parse options for headless to new if false is given', () => {
-    const pluginConfig = create({
+  it('should parse options for headless to new if false is given', async () => {
+    const pluginConfig = await create({
       url: LIGHTHOUSE_URL,
       headless: false,
     });
@@ -99,8 +99,8 @@ describe('getLighthouseCliArguments', () => {
     );
   });
 
-  it('should parse options for userDataDir correctly', () => {
-    const pluginConfig = create({
+  it('should parse options for userDataDir correctly', async () => {
+    const pluginConfig = await create({
       url: LIGHTHOUSE_URL,
       userDataDir: 'test',
     });
