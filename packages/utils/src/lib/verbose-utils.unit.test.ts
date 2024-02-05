640908a7b20ab03bbcd7c36a0ae43d7980ad8338
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-02-05 17:49:32 +0100

    Update packages/utils/src/lib/verbose-utils.unit.test.ts
    Co-authored-by: Katka Pilátová <katerina.pilatova@flowup.cz>

--- packages/utils/src/lib/verbose-utils.unit.test.ts
+++ packages/utils/src/lib/verbose-utils.unit.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { verboseUtils } from './verbose-utils';
 
 describe('verbose-utils', () => {
-  it('exec should off by default', () => {
+  it('exec should be off by default', () => {
     const spy = vi.fn();
     verboseUtils().exec(spy);
     expect(spy).not.toHaveBeenCalled();
