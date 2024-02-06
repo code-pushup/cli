6c928aa0c8697738313f8a51085d1d7ef63de61c
Michael <michael.hladky@push-based.io>
2024-02-06 12:54:14 +0100

    refactor(x-plugin): use utils

--- packages/nx-plugin/src/utils/versions.ts
+++ packages/nx-plugin/src/utils/versions.ts
@@ -1,13 +1,13 @@
-import { readFile } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { readJsonFile } from '@code-pushup/utils';
 
 const workspaceRoot = join(fileURLToPath(dirname(import.meta.url)), '../../');
 const projectsFolder = join(
   fileURLToPath(dirname(import.meta.url)),
   '../../../',
 );
-type PkgJson = { version: string };
+
 export const cpuNxPluginVersion = (await loadPackageJson(workspaceRoot))
   .version;
 export const cpuModelVersion = (
@@ -20,7 +20,8 @@ export const cpuCliVersion = (
   await loadPackageJson(join(projectsFolder, 'models'))
 ).version;
 
-async function loadPackageJson(folderPath: string): Promise<PkgJson> {
-  const packageJsonContent = await readFile(join(folderPath, 'package.json'));
-  return JSON.parse(packageJsonContent.toString()) as PkgJson;
+async function loadPackageJson(
+  folderPath: string,
+): Promise<{ version: string }> {
+  return readJsonFile<{ version: string }>(join(folderPath, 'package.json'));
 }
