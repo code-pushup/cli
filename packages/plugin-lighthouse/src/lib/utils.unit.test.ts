7fbebe0082f43d8658fec7831684cf9580b3298e
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-02-06 04:31:56 +0100

    Update packages/plugin-lighthouse/src/lib/utils.unit.test.ts

--- packages/plugin-lighthouse/src/lib/utils.unit.test.ts
+++ packages/plugin-lighthouse/src/lib/utils.unit.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { expect } from 'vitest';
 import { getLighthouseCliArguments } from './utils';
 
 describe('getLighthouseCliArguments', () => {
