aa0c4008ddf82651e29993c55ab1fc12b6b94b1a
Michael <michael.hladky@push-based.io>
2024-02-06 13:10:32 +0100

    refactor(nx-plugin): adopt comments

--- testing-utils/src/index.ts
+++ testing-utils/src/index.ts
@@ -1,7 +1,6 @@
 export * from './lib/constants';
 export * from './lib/setup/test-folder.setup';
 export * from './lib/utils/execute-process-helper.mock';
-// utils
 export * from './lib/utils/git';
 
 // static mocks
