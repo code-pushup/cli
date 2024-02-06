66046ea048679b56f62691378fe6a6549f790ec7
Michael <michael.hladky@push-based.io>
2024-02-06 15:59:24 +0100

    test(utils): format

--- packages/cli/src/lib/print-config/print-config-command.unit.test.ts
+++ packages/cli/src/lib/print-config/print-config-command.unit.test.ts
@@ -1,11 +1,11 @@
-import {beforeAll, describe, expect} from 'vitest';
-import {DEFAULT_CLI_CONFIGURATION} from '../../../mocks/constants';
-import {ui} from '../implementation/logging';
-import {yargsCli} from '../yargs-cli';
-import {yargsConfigCommandObject} from './print-config-command';
+import { beforeAll, describe, expect } from 'vitest';
+import { DEFAULT_CLI_CONFIGURATION } from '../../../mocks/constants';
+import { ui } from '../implementation/logging';
+import { yargsCli } from '../yargs-cli';
+import { yargsConfigCommandObject } from './print-config-command';
 
 vi.mock('@code-pushup/core', async () => {
-  const {CORE_CONFIG_MOCK}: typeof import('@code-pushup/testing-utils') =
+  const { CORE_CONFIG_MOCK }: typeof import('@code-pushup/testing-utils') =
     await vi.importActual('@code-pushup/testing-utils');
   const core: object = await vi.importActual('@code-pushup/core');
   return {
@@ -28,25 +28,33 @@ describe('print-config-command', () => {
         `--config=/test/code-pushup.config.ts`,
         '--persist.outputDir=destinationDir',
       ],
-      {...DEFAULT_CLI_CONFIGURATION, commands: [yargsConfigCommandObject()]},
+      { ...DEFAULT_CLI_CONFIGURATION, commands: [yargsConfigCommandObject()] },
     ).parseAsync();
 
     const log = ui().logger.getLogs()[0];
 
-    expect(log).toEqual(expect.objectContaining({
-      message: expect.not.stringContaining('"$0":')
-    }));
+    expect(log).toEqual(
+      expect.objectContaining({
+        message: expect.not.stringContaining('"$0":'),
+      }),
+    );
 
-    expect(log).toEqual(expect.objectContaining({
-      message: expect.not.stringContaining('"_":'),
-    }));
+    expect(log).toEqual(
+      expect.objectContaining({
+        message: expect.not.stringContaining('"_":'),
+      }),
+    );
 
-    expect(log).toEqual(expect.objectContaining({
-      message: expect.stringContaining('"outputDir": "destinationDir"'),
-    }));
+    expect(log).toEqual(
+      expect.objectContaining({
+        message: expect.stringContaining('"outputDir": "destinationDir"'),
+      }),
+    );
 
-    expect(log).toEqual(expect.objectContaining({
-      message: expect.not.stringContaining('"output-dir":'),
-    }));
+    expect(log).toEqual(
+      expect.objectContaining({
+        message: expect.not.stringContaining('"output-dir":'),
+      }),
+    );
   });
 });
