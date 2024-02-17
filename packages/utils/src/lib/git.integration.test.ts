5292e22098450b453d4d0c6d65d9c3c95b2798a7
Michael <michael.hladky@push-based.io>
2024-02-17 22:30:33 +0530

    test(utils): add git tests

--- packages/utils/src/lib/git.integration.test.ts
+++ packages/utils/src/lib/git.integration.test.ts
@@ -13,7 +13,7 @@ import {
 } from './git';
 import { toUnixPath } from './transform';
 
-describe('git utils', () => {
+describe('git utils in a git repo with a branch and commits', () => {
   const baseDir = join(process.cwd(), 'tmp', 'testing-git-repo');
   const changesDir = join(baseDir, 'changes-dir');
   let git: SimpleGit;
@@ -120,10 +120,14 @@ describe('git utils', () => {
   });
 
   it('safeCheckout should clean local changes and check out to master', async () => {
+    // needs to get reset to be clean
     await mkdir(changesDir, { recursive: true });
     await writeFile(join(changesDir, 'change.md'), '# hello-change\n');
+    // needs to get cleaned to be clean
+    await writeFile(join(baseDir, 'README.md'), '# hello-world-2\n');
+
     await expect(
-      safeCheckout('feature-branch', { clean: true }, git),
+      safeCheckout('feature-branch', { forceCleanStatus: true }, git),
     ).resolves.toBeUndefined();
     await expect(git.branch()).resolves.toEqual(
       expect.objectContaining({ current: 'feature-branch' }),
@@ -132,5 +136,37 @@ describe('git utils', () => {
 
   it('getCurrentBranchOrTag should log current branch', async () => {
     await expect(getCurrentBranchOrTag(git)).resolves.toBe('master');
+  });
+});
+
+describe('git utils in a git repo without a branch and commits', () => {
+  const baseDir = join(process.cwd(), 'tmp', 'testing-git-repo');
+  const changesDir = join(baseDir, 'changes-dir');
+  let git: SimpleGit;
+
+  beforeAll(async () => {
+    await mkdir(baseDir, { recursive: true });
+    await writeFile(join(baseDir, 'README.md'), '# hello-world\n');
+
+    git = simpleGit(baseDir);
+    await git.init();
+
+    await git.addConfig('user.name', 'John Doe');
+    await git.addConfig('user.email', 'john.doe@example.com');
+  });
+
+  afterAll(async () => {
+    await rm(baseDir, { recursive: true, force: true });
+  });
+
+  afterEach(async () => {
+    await rm(changesDir, { recursive: true, force: true });
+    await git.checkout(['master']);
+  });
+
+  it('getCurrentBranchOrTag should throw if no branch is given', async () => {
+    await expect(getCurrentBranchOrTag(git)).rejects.toThrow(
+      "git: 'describe --tags --exact-match' is not a git command. See 'git --help'",
+    );
   });
 });
