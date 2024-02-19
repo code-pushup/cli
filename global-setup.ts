f7e6ab62f336998dc306deed8b2c826aee6de830
Katerina Pilatova <katerina.pilatova@flowup.cz>
2024-02-19 18:39:31 +0100

    test: remove tmp folder in global teardown for all tests

--- global-setup.ts
+++ global-setup.ts
@@ -1,3 +1,9 @@
+import { teardownTestFolder } from './testing/test-setup/src';
+
 export async function setup() {
   process.env.TZ = 'UTC';
+}
+
+export async function teardown() {
+  await teardownTestFolder('tmp');
 }
