7dfd97d2405c012ebc03535b068fc8de64289c43
Michael <michael.hladky@push-based.io>
2024-02-27 10:13:07 +0100

    refactor: set better weight

--- code-pushup.config.ts
+++ code-pushup.config.ts
@@ -130,7 +130,7 @@ const config: CoreConfig = {
           type: 'group',
           plugin: 'benchmark-js',
           slug: `${suit}-benchmark-js`,
-          weight: suit === 'current-implementation' ? 2 : 1,
+          weight: suit === ('current-implementation') ? 2 : 1,
         })),
       ],
     },
