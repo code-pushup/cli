76aefb4b74fb12cc3f5ea30eeda2de160031d1eb
Katerina Pilatova <katerina.pilatova@flowup.cz>
2024-03-04 11:06:24 +0100

    test: use nested describe within tmp folder

--- packages/utils/src/lib/git.integration.test.ts
+++ packages/utils/src/lib/git.integration.test.ts
@@ -12,188 +12,187 @@ import {
 } from './git';
 import { toUnixPath } from './transform';
 
-// we need a separate folder that is not cleaned in `global-setup.ts`, otherwise the tests can't execute in parallel
-const gitTestFolder = 'git-test';
-
-describe('git utils in a git repo without a branch and commits', () => {
-  const baseDir = join(
-    process.cwd(),
-    gitTestFolder,
-    'testing-git-repo-without-branch-and-commits',
-  );
-  let emptyGit: SimpleGit;
-
-  beforeAll(async () => {
-    await mkdir(baseDir, { recursive: true });
-    emptyGit = simpleGit(baseDir);
-    await emptyGit.init();
-  });
-
-  afterAll(async () => {
-    await rm(baseDir, { recursive: true, force: true });
-  });
-
-  it('getCurrentBranchOrTag should throw if no branch or tag is given', async () => {
-    await expect(getCurrentBranchOrTag(emptyGit)).rejects.toThrow(
-      'Could not get current tag or branch.',
+describe('git helper tests', () => {
+  describe('git utils in a git repo without a branch and commits', () => {
+    const baseDir = join(
+      process.cwd(),
+      'tmp',
+      'testing-git-repo-without-branch-and-commits',
     );
-  });
-});
+    let emptyGit: SimpleGit;
 
-describe('git utils in a git repo with a branch and commits clean', () => {
-  const baseDir = join(
-    process.cwd(),
-    gitTestFolder,
-    'testing-git-repo-with-branch-and-commits-clean',
-  );
-  let intiGit: SimpleGit;
+    beforeAll(async () => {
+      await mkdir(baseDir, { recursive: true });
+      emptyGit = simpleGit(baseDir);
+      await emptyGit.init();
+    });
 
-  beforeAll(async () => {
-    await mkdir(baseDir, { recursive: true });
+    afterAll(async () => {
+      await rm(baseDir, { recursive: true, force: true });
+    });
 
-    intiGit = simpleGit(baseDir);
-    await intiGit.init();
-
-    await intiGit.addConfig('user.name', 'John Doe');
-    await intiGit.addConfig('user.email', 'john.doe@example.com');
-
-    await writeFile(join(baseDir, 'README.md'), '# hello-world\n');
-    await intiGit.add('README.md');
-    await intiGit.commit('Create README');
-
-    await intiGit.branch(['feature-branch']);
-    await intiGit.checkout(['master']);
-  });
-
-  afterAll(async () => {
-    await rm(baseDir, { recursive: true, force: true });
-  });
-
-  it('should log latest commit', async () => {
-    const gitCommitDateRegex =
-      /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{1,2} \d{2}:\d{2}:\d{2} \d{4} [+|-]\d{4}$/;
-
-    await expect(getLatestCommit(intiGit)).resolves.toEqual({
-      hash: expect.stringMatching(/^[\da-f]{40}$/),
-      message: 'Create README',
-      author: 'John Doe',
-      date: expect.stringMatching(gitCommitDateRegex),
+    it('getCurrentBranchOrTag should throw if no branch or tag is given', async () => {
+      await expect(getCurrentBranchOrTag(emptyGit)).rejects.toThrow(
+        'Could not get current tag or branch.',
+      );
     });
   });
 
-  it('should find Git root', async () => {
-    await expect(getGitRoot(intiGit)).resolves.toBe(toUnixPath(baseDir));
-  });
-
-  it('should convert absolute path to relative Git path', async () => {
-    await expect(
-      toGitPath(join(baseDir, 'src', 'utils.ts'), intiGit),
-    ).resolves.toBe('src/utils.ts');
-  });
-
-  it('should convert relative Windows path to relative Git path', async () => {
-    await expect(toGitPath('Backend\\API\\Startup.cs', intiGit)).resolves.toBe(
-      '../../Backend/API/Startup.cs',
+  describe('git utils in a git repo with a branch and commits clean', () => {
+    const baseDir = join(
+      process.cwd(),
+      'tmp',
+      'testing-git-repo-with-branch-and-commits-clean',
     );
+    let cleanGit: SimpleGit;
+
+    beforeAll(async () => {
+      await mkdir(baseDir, { recursive: true });
+
+      cleanGit = simpleGit(baseDir);
+      await cleanGit.init();
+
+      await cleanGit.addConfig('user.name', 'John Doe');
+      await cleanGit.addConfig('user.email', 'john.doe@example.com');
+
+      await writeFile(join(baseDir, 'README.md'), '# hello-world\n');
+      await cleanGit.add('README.md');
+      await cleanGit.commit('Create README');
+
+      await cleanGit.branch(['feature-branch']);
+      await cleanGit.checkout(['master']);
+    });
+
+    afterAll(async () => {
+      await rm(baseDir, { recursive: true, force: true });
+    });
+
+    it('should log latest commit', async () => {
+      const gitCommitDateRegex =
+        /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{1,2} \d{2}:\d{2}:\d{2} \d{4} [+|-]\d{4}$/;
+
+      await expect(getLatestCommit(cleanGit)).resolves.toEqual({
+        hash: expect.stringMatching(/^[\da-f]{40}$/),
+        message: 'Create README',
+        author: 'John Doe',
+        date: expect.stringMatching(gitCommitDateRegex),
+      });
+    });
+
+    it('should find Git root', async () => {
+      await expect(getGitRoot(cleanGit)).resolves.toBe(toUnixPath(baseDir));
+    });
+
+    it('should convert absolute path to relative Git path', async () => {
+      await expect(
+        toGitPath(join(baseDir, 'src', 'utils.ts'), cleanGit),
+      ).resolves.toBe('src/utils.ts');
+    });
+
+    it('should convert relative Windows path to relative Git path', async () => {
+      await expect(
+        toGitPath('Backend\\API\\Startup.cs', cleanGit),
+      ).resolves.toBe('../../Backend/API/Startup.cs');
+    });
+
+    it('should keep relative Unix path as is (already a Git path)', async () => {
+      await expect(toGitPath('Backend/API/Startup.cs', cleanGit)).resolves.toBe(
+        '../../Backend/API/Startup.cs',
+      );
+    });
+
+    it('getCurrentBranchOrTag should log current branch', async () => {
+      await expect(getCurrentBranchOrTag(cleanGit)).resolves.toBe('master');
+    });
+
+    it('guardAgainstLocalChanges should not throw if history is clean', async () => {
+      await expect(guardAgainstLocalChanges(cleanGit)).resolves.toBeUndefined();
+    });
+
+    it('safeCheckout should checkout feature-branch in clean state', async () => {
+      await expect(
+        safeCheckout('feature-branch', {}, cleanGit),
+      ).resolves.toBeUndefined();
+      await expect(cleanGit.branch()).resolves.toEqual(
+        expect.objectContaining({ current: 'feature-branch' }),
+      );
+    });
+
+    it('safeCheckout should throw if a given branch does not exist', async () => {
+      await expect(
+        safeCheckout('non-existing-branch', {}, cleanGit),
+      ).rejects.toThrow(
+        "pathspec 'non-existing-branch' did not match any file(s) known to git",
+      );
+    });
   });
 
-  it('should keep relative Unix path as is (already a Git path)', async () => {
-    await expect(toGitPath('Backend/API/Startup.cs', intiGit)).resolves.toBe(
-      '../../Backend/API/Startup.cs',
+  describe('git utils in a git repo with a branch and commits dirty', () => {
+    const baseDir = join(
+      process.cwd(),
+      'tmp',
+      'testing-git-repo-with-branch-and-commits-dirty',
     );
-  });
+    const newFilePath = join(baseDir, 'new-file.md');
+    let dirtyGit: SimpleGit;
 
-  it('getCurrentBranchOrTag should log current branch', async () => {
-    await expect(getCurrentBranchOrTag(intiGit)).resolves.toBe('master');
-  });
+    beforeAll(async () => {
+      await mkdir(baseDir, { recursive: true });
 
-  it('guardAgainstLocalChanges should not throw if history is clean', async () => {
-    await expect(guardAgainstLocalChanges(intiGit)).resolves.toBeUndefined();
-  });
+      dirtyGit = simpleGit(baseDir);
+      await dirtyGit.init();
+      await dirtyGit.addConfig('user.name', 'John Doe');
+      await dirtyGit.addConfig('user.email', 'john.doe@example.com');
 
-  it('safeCheckout should checkout feature-branch in clean state', async () => {
-    await expect(
-      safeCheckout('feature-branch', {}, intiGit),
-    ).resolves.toBeUndefined();
-    await expect(intiGit.branch()).resolves.toEqual(
-      expect.objectContaining({ current: 'feature-branch' }),
-    );
-  });
+      await writeFile(join(baseDir, 'README.md'), '# hello-world\n');
+      await dirtyGit.add('README.md');
+      await dirtyGit.commit('Create README');
 
-  it('safeCheckout should throw if a given branch does not exist', async () => {
-    await expect(
-      safeCheckout('non-existing-branch', {}, intiGit),
-    ).rejects.toThrow(
-      "pathspec 'non-existing-branch' did not match any file(s) known to git",
-    );
-  });
-});
+      await dirtyGit.branch(['feature-branch']);
+      await dirtyGit.checkout(['master']);
+    });
 
-describe('git utils in a git repo with a branch and commits dirty', () => {
-  const baseDir = join(
-    process.cwd(),
-    gitTestFolder,
-    'testing-git-repo-with-branch-and-commits-dirty',
-  );
-  const newFilePath = join(baseDir, 'new-file.md');
-  let dirtyGt: SimpleGit;
+    beforeEach(async () => {
+      await writeFile(newFilePath, '# New File\n');
+    });
 
-  beforeAll(async () => {
-    await mkdir(baseDir, { recursive: true });
-
-    dirtyGt = simpleGit(baseDir);
-    await dirtyGt.init();
-    await dirtyGt.addConfig('user.name', 'John Doe');
-    await dirtyGt.addConfig('user.email', 'john.doe@example.com');
-
-    await writeFile(join(baseDir, 'README.md'), '# hello-world\n');
-    await dirtyGt.add('README.md');
-    await dirtyGt.commit('Create README');
-
-    await dirtyGt.branch(['feature-branch']);
-    await dirtyGt.checkout(['master']);
-  });
-
-  beforeEach(async () => {
-    await writeFile(newFilePath, '# New File\n');
-  });
-
-  afterEach(async () => {
-    try {
-      const s = await stat(newFilePath);
-      if (s.isFile()) {
-        await rm(newFilePath);
+    afterEach(async () => {
+      try {
+        const s = await stat(newFilePath);
+        if (s.isFile()) {
+          await rm(newFilePath);
+        }
+      } catch {
+        // file not present (already cleaned)
       }
-    } catch {
-      // file not present (already cleaned)
-    }
-  });
+    });
 
-  afterAll(async () => {
-    await rm(baseDir, { recursive: true, force: true });
-  });
+    afterAll(async () => {
+      await rm(baseDir, { recursive: true, force: true });
+    });
 
-  it('safeCheckout should clean local changes and check out to feature-branch', async () => {
-    await expect(
-      safeCheckout('feature-branch', { forceCleanStatus: true }, dirtyGt),
-    ).resolves.toBeUndefined();
-    await expect(dirtyGt.branch()).resolves.toEqual(
-      expect.objectContaining({ current: 'feature-branch' }),
-    );
-    await expect(dirtyGt.status()).resolves.toEqual(
-      expect.objectContaining({ files: [] }),
-    );
-  });
+    it('safeCheckout should clean local changes and check out to feature-branch', async () => {
+      await expect(
+        safeCheckout('feature-branch', { forceCleanStatus: true }, dirtyGit),
+      ).resolves.toBeUndefined();
+      await expect(dirtyGit.branch()).resolves.toEqual(
+        expect.objectContaining({ current: 'feature-branch' }),
+      );
+      await expect(dirtyGit.status()).resolves.toEqual(
+        expect.objectContaining({ files: [] }),
+      );
+    });
 
-  it('safeCheckout should throw if history is dirty', async () => {
-    await expect(safeCheckout('master', {}, dirtyGt)).rejects.toThrow(
-      'Working directory needs to be clean before we you can proceed. Commit your local changes or stash them.',
-    );
-  });
+    it('safeCheckout should throw if history is dirty', async () => {
+      await expect(safeCheckout('master', {}, dirtyGit)).rejects.toThrow(
+        'Working directory needs to be clean before we you can proceed. Commit your local changes or stash them.',
+      );
+    });
 
-  it('guardAgainstLocalChanges should throw if history is dirty', async () => {
-    await expect(guardAgainstLocalChanges(dirtyGt)).rejects.toThrow(
-      'Working directory needs to be clean before we you can proceed. Commit your local changes or stash them.',
-    );
+    it('guardAgainstLocalChanges should throw if history is dirty', async () => {
+      await expect(guardAgainstLocalChanges(dirtyGit)).rejects.toThrow(
+        'Working directory needs to be clean before we you can proceed. Commit your local changes or stash them.',
+      );
+    });
   });
 });
