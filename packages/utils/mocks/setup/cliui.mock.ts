c243904de3daf6f409737f5497172fc8104bbf05
Michael <michael.hladky@push-based.io>
2024-03-08 16:57:14 +0100

    wip

--- packages/utils/mocks/setup/cliui.mock.ts
+++ packages/utils/mocks/setup/cliui.mock.ts
@@ -1,8 +1,8 @@
 import {beforeEach, vi} from "vitest";
-import {ui} from "../../src/lib/logging";
+import {ui} from "../../src";
 
 vi.mock('../../src/lib/logging', async () => {
-  const module = await vi.importActual('../../src/lib/logging');
+  const module = await vi.importActual('../../src');
 
   (module['ui'] as () => {switchMode: (mode: string) => void})().switchMode('raw');
   return module;
