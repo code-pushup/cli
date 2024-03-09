8b6b62c9dae58246709963d0ed168987a6ae99b5
Michael <michael.hladky@push-based.io>
2024-03-09 01:42:42 +0100

    wip

--- packages/plugin-lighthouse/src/lib/lighthouse-plugin.integration.test.ts
+++ packages/plugin-lighthouse/src/lib/lighthouse-plugin.integration.test.ts
@@ -17,7 +17,7 @@ describe('getRunner', () => {
     const runner = getRunner('https://example.com', {
       onlyAudits: ['is-on-https'],
     });
-    await expect(runner(() => void 0)).resolves.toThrow('');
+    await expect(runner(() => void 0)).rejects.toThrow('');
     /*.resolves.toEqual([
       expect.objectContaining({ slug: 'is-on-https' }),
     ]);*/
