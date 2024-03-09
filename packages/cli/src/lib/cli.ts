a914dcf581e75da1ab7acf165fa278b370871a57
Michael <michael.hladky@push-based.io>
2024-03-09 03:51:13 +0100

    docs(cli): add option grouping and examples

--- packages/cli/src/lib/cli.ts
+++ packages/cli/src/lib/cli.ts
@@ -1,7 +1,7 @@
 import { commands } from './commands';
 import { CLI_NAME, CLI_SCRIPT_NAME } from './constants';
 import { middlewares } from './middlewares';
-import { options } from './options';
+import { groups, options } from './options';
 import { yargsCli } from './yargs-cli';
 
 export const cli = (args: string[]) =>
@@ -9,6 +9,17 @@ export const cli = (args: string[]) =>
     usageMessage: CLI_NAME,
     scriptName: CLI_SCRIPT_NAME,
     options,
+    groups,
+    examples: [
+      [
+        'code-pushup collect --config tsconfig.test.json',
+        'Use the config from `tsconfig.test.json` and only collect audits',
+      ],
+      [
+        'code-pushup print-config --onlyPlugins',
+        'print the config object but from the plugins only list given slugs',
+      ],
+    ],
     middlewares,
     commands,
   });
--- packages/cli/src/lib/options.ts
+++ packages/cli/src/lib/options.ts
@@ -1,7 +1,17 @@
-import { yargsCoreConfigOptionsDefinition } from './implementation/core-config.options';
+import {
+  yargsCoreConfigOptionsDefinition,
+  yargsPersistConfigOptionsDefinition,
+  yargsUploadConfigOptionsDefinition,
+} from './implementation/core-config.options';
 import { yargsGlobalOptionsDefinition } from './implementation/global.options';
 
 export const options = {
   ...yargsGlobalOptionsDefinition(),
   ...yargsCoreConfigOptionsDefinition(),
+};
+
+export const groups = {
+  'Global Options:': Object.keys(yargsGlobalOptionsDefinition()),
+  'Persist Options:': Object.keys(yargsPersistConfigOptionsDefinition()),
+  'Upload Options:': Object.keys(yargsUploadConfigOptionsDefinition()),
 };
