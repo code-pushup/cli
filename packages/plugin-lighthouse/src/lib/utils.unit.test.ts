8760cf68ec52c2181cd38947ec434197f460502e
Michael <michael.hladky@push-based.io>
2024-03-10 15:53:52 +0100

    wip

--- packages/plugin-lighthouse/src/lib/utils.unit.test.ts
+++ packages/plugin-lighthouse/src/lib/utils.unit.test.ts
@@ -507,7 +507,7 @@ describe('toAuditOutputs', () => {
               data: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwY...',
             },
           ],
-        },
+        } satisfies Details.Filmstrip,
       },
     ]);
 
