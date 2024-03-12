549acd38800ced486c920e7172ee98dbf4a64a6d
Matěj Chalk <matej.chalk@flowup.cz>
2024-03-12 19:10:22 +0100

    test(test-utils): prepare realistic reports diff mock

--- testing/test-utils/src/lib/utils/dynamic-mocks/lighthouse-audits.mock.ts
+++ testing/test-utils/src/lib/utils/dynamic-mocks/lighthouse-audits.mock.ts
@@ -1,9 +1,34 @@
 import { AuditReport } from '@code-pushup/models';
 
-export const LIGHTHOUSE_AUDIT_REPORTS_MAP: Record<string, AuditReport> = {
+export const LIGHTHOUSE_AUDITS_CHANGES: Partial<
+  Record<
+    keyof typeof LIGHTHOUSE_AUDITS_MAP,
+    Pick<AuditReport, 'score' | 'value' | 'displayValue'>
+  >
+> = {
+  'largest-contentful-paint': {
+    score: 0.85,
+    value: 1369,
+    displayValue: '1.4 s',
+  },
+  'first-contentful-paint': {
+    score: 0.79,
+    value: 1146,
+    displayValue: '1.1 s',
+  },
+  'speed-index': {
+    score: 0.94,
+    value: 1146,
+    displayValue: '1.1 s',
+  },
+};
+
+export const LIGHTHOUSE_AUDITS_MAP = {
   'first-contentful-paint': {
     slug: 'first-contentful-paint',
     title: 'First Contentful Paint',
+    description:
+      'First Contentful Paint marks the time at which the first text or image is painted.',
     docsUrl:
       'https://developer.chrome.com/docs/lighthouse/performance/first-contentful-paint/',
     score: 0.76,
@@ -13,6 +38,8 @@ export const LIGHTHOUSE_AUDIT_REPORTS_MAP: Record<string, AuditReport> = {
   'largest-contentful-paint': {
     slug: 'largest-contentful-paint',
     title: 'Largest Contentful Paint',
+    description:
+      'Largest Contentful Paint marks the time at which the largest text or image is painted.',
     docsUrl:
       'https://developer.chrome.com/docs/lighthouse/performance/largest-contentful-paint/',
     score: 0.81,
@@ -22,6 +49,8 @@ export const LIGHTHOUSE_AUDIT_REPORTS_MAP: Record<string, AuditReport> = {
   'total-blocking-time': {
     slug: 'total-blocking-time',
     title: 'Total Blocking Time',
+    description:
+      'Sum of all time periods between FCP and Time to Interactive, when task length exceeded 50ms, expressed in milliseconds.',
     docsUrl:
       'https://developer.chrome.com/docs/lighthouse/performance/lighthouse-total-blocking-time/',
     score: 1,
@@ -31,6 +60,8 @@ export const LIGHTHOUSE_AUDIT_REPORTS_MAP: Record<string, AuditReport> = {
   'cumulative-layout-shift': {
     slug: 'cumulative-layout-shift',
     title: 'Cumulative Layout Shift',
+    description:
+      'Cumulative Layout Shift measures the movement of visible elements within the viewport.',
     docsUrl: 'https://web.dev/cls/',
     score: 1,
     value: 0,
@@ -39,10 +70,16 @@ export const LIGHTHOUSE_AUDIT_REPORTS_MAP: Record<string, AuditReport> = {
   'speed-index': {
     slug: 'speed-index',
     title: 'Speed Index',
+    description:
+      'Speed Index shows how quickly the contents of a page are visibly populated.',
     docsUrl:
       'https://developer.chrome.com/docs/lighthouse/performance/speed-index/',
     score: 0.93,
     value: 1189,
     displayValue: '1.2 s',
   },
-};
+} satisfies Record<string, AuditReport>;
+
+export const LIGHTHOUSE_AUDIT_SLUGS = Object.keys(
+  LIGHTHOUSE_AUDITS_MAP,
+) as (keyof typeof LIGHTHOUSE_AUDITS_MAP)[];
