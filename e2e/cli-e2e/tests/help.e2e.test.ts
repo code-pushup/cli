073e1ea29a33a296df5884304770e4f7b88707da
Michael <michael.hladky@push-based.io>
2024-03-12 13:08:09 +0100

    fix

--- e2e/cli-e2e/tests/help.e2e.test.ts
+++ e2e/cli-e2e/tests/help.e2e.test.ts
@@ -1,10 +1,20 @@
 import { executeProcess } from '@code-pushup/utils';
 
 describe('CLI help', () => {
-  it('should print help with command help', async () => {
+  it('should print help with help command', async () => {
     const { code, stdout, stderr } = await executeProcess({
       command: 'code-pushup',
       args: ['help'],
+    });
+    expect(code).toBe(0);
+    expect(stderr).toBe('');
+    expect(stdout).toMatchSnapshot();
+  });
+
+  it('should print help with help argument', async () => {
+    const { code, stdout, stderr } = await executeProcess({
+      command: 'code-pushup',
+      args: ['--help'],
     });
     expect(code).toBe(0);
     expect(stderr).toBe('');
