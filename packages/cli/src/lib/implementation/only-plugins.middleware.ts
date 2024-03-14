d431d3f4f0df01c3b4f3e2553a2248763db4a02a
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-03-14 16:47:53 +0100

    refactor: adjust only plugins logic to reuse utils helper (#554)

--- packages/cli/src/lib/implementation/only-plugins.middleware.ts
+++ packages/cli/src/lib/implementation/only-plugins.middleware.ts
@@ -1,23 +1,35 @@
-import { CoreConfig } from '@code-pushup/models';
-import { GeneralCliOptions } from './global.model';
+import { filterItemRefsBy } from '@code-pushup/utils';
 import { OnlyPluginsOptions } from './only-plugins.model';
-import {
-  filterCategoryByPluginSlug,
-  filterPluginsBySlug,
-  validateOnlyPluginsOption,
-} from './only-plugins.utils';
+import { validateOnlyPluginsOption } from './only-plugins.utils';
 
-export function onlyPluginsMiddleware<
-  T extends GeneralCliOptions &
-    Omit<CoreConfig, 'categories'> &
-    Required<Pick<CoreConfig, 'categories'>> &
-    OnlyPluginsOptions,
->(processArgs: T): GeneralCliOptions & CoreConfig & OnlyPluginsOptions {
-  validateOnlyPluginsOption(processArgs.plugins, processArgs);
+export function onlyPluginsMiddleware<T extends OnlyPluginsOptions>(
+  originalProcessArgs: T,
+): T {
+  const { categories = [], onlyPlugins: originalOnlyPlugins } =
+    originalProcessArgs;
+
+  if (originalOnlyPlugins && originalOnlyPlugins.length > 0) {
+    const { verbose, plugins, onlyPlugins } = originalProcessArgs;
+
+    validateOnlyPluginsOption(
+      { plugins, categories },
+      { onlyPlugins, verbose },
+    );
+
+    const onlyPluginsSet = new Set(onlyPlugins);
+
+    return {
+      ...originalProcessArgs,
+      plugins: plugins.filter(({ slug }) => onlyPluginsSet.has(slug)),
+      categories: filterItemRefsBy(categories, ({ plugin }) =>
+        onlyPluginsSet.has(plugin),
+      ),
+    };
+  }
 
   return {
-    ...processArgs,
-    plugins: filterPluginsBySlug(processArgs.plugins, processArgs),
-    categories: filterCategoryByPluginSlug(processArgs.categories, processArgs),
+    ...originalProcessArgs,
+    // if undefined fill categories with empty array
+    categories,
   };
 }
