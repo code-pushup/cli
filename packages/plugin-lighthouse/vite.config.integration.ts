17fffbe89601e94fcfd02b3e9b17eec92ff7cca0
Michael <michael.hladky@push-based.io>
2024-03-17 17:27:17 -0600

    test(plugin-lighthouse): adjust test data 7

--- packages/plugin-lighthouse/vite.config.integration.ts
+++ packages/plugin-lighthouse/vite.config.integration.ts
@@ -23,7 +23,6 @@ export default defineConfig({
     setupFiles: [
       '../../testing/test-setup/src/lib/cliui.mock.ts',
       '../../testing/test-setup/src/lib/reset.mocks.ts',
-      '../../testing/test-setup/src/lib/chrome-path.setup.ts',
     ],
   },
 });
