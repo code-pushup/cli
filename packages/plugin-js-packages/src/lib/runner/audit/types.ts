ee3209d397738acd4eefa18df0357d7c4b3de67e
Katerina Pilatova <katerina.pilatova@flowup.cz>
2024-03-20 19:20:54 +0100

    feat(plugin-js-packages): introduce yarn v1 audit, unify type

--- packages/plugin-js-packages/src/lib/runner/audit/types.ts
+++ packages/plugin-js-packages/src/lib/runner/audit/types.ts
@@ -1,32 +1,79 @@
 import type { PackageAuditLevel } from '../../config';
 
 // Subset of NPM audit JSON type
-type Advisory = {
+export type NpmAdvisory = {
   title: string;
   url: string;
 };
 
-type FixInformation = {
+export type NpmFixInformation = {
   name: string;
   version: string;
   isSemVerMajor: boolean;
 };
 
-export type Vulnerability = {
-  name: string;
-  severity: PackageAuditLevel;
-  via: Advisory[] | string[];
-  range: string;
-  fixAvailable: boolean | FixInformation;
-};
-
-export type Vulnerabilities = {
-  [key: string]: Vulnerability;
-};
+export type NpmVulnerabilities = Record<
+  string,
+  {
+    name: string;
+    severity: PackageAuditLevel;
+    isDirect: boolean;
+    effects: string[];
+    via: NpmAdvisory[] | string[];
+    range: string;
+    fixAvailable: boolean | NpmFixInformation;
+  }
+>;
 
 export type NpmAuditResultJson = {
-  vulnerabilities: Vulnerabilities;
+  vulnerabilities: NpmVulnerabilities;
   metadata: {
     vulnerabilities: Record<PackageAuditLevel | 'total', number>;
   };
+};
+
+// Subset of Yarn v1 audit JSON type
+/* eslint-disable @typescript-eslint/naming-convention */
+export type Yarnv1AuditAdvisory = {
+  type: 'auditAdvisory';
+  data: {
+    resolution: {
+      path: string;
+    };
+    advisory: {
+      module_name: string;
+      severity: PackageAuditLevel;
+      vulnerable_versions: string;
+      recommendation: string;
+      title: string;
+      url: string;
+    };
+  };
+};
+
+export type Yarnv1AuditSummary = {
+  type: 'auditSummary';
+  data: {
+    vulnerabilities: Record<PackageAuditLevel, number>;
+  };
+};
+
+// NOTE: When rest operator can be at the beginning, the process will be much simpler
+export type Yarnv1AuditResultJson = [
+  ...Yarnv1AuditAdvisory[],
+  Yarnv1AuditSummary,
+];
+
+// Unified Audit result type
+export type AuditResult = {
+  vulnerabilities: {
+    name: string;
+    title?: string;
+    url?: string;
+    severity: PackageAuditLevel;
+    versionRange: string;
+    directDependency: string | true; // either name of direct dependency this one affects or true
+    fixInformation: string | false; // either guide on how to fix the vulnerability or false
+  }[];
+  summary: Record<PackageAuditLevel | 'total', number>;
 };
---
+++ packages/plugin-js-packages/src/lib/runner/audit/unify-type.ts
@@ -0,0 +1,168 @@
+import { objectToEntries } from '@code-pushup/utils';
+import {
+  AuditResult,
+  NpmAdvisory,
+  NpmAuditResultJson,
+  NpmFixInformation,
+  NpmVulnerabilities,
+  Yarnv1AuditAdvisory,
+  Yarnv1AuditResultJson,
+  Yarnv1AuditSummary,
+} from './types';
+
+export function npmToAuditResult(output: string): AuditResult {
+  const npmAudit = JSON.parse(output) as NpmAuditResultJson;
+
+  const vulnerabilities = objectToEntries(npmAudit.vulnerabilities).map<
+    AuditResult['vulnerabilities'][number]
+  >(([name, detail]) => {
+    const advisory = npmToAdvisory(name, npmAudit.vulnerabilities);
+    return {
+      name: name.toString(),
+      severity: detail.severity,
+      versionRange: detail.range,
+      directDependency: detail.isDirect ? true : detail.effects[0] ?? '',
+      fixInformation: npmToFixInformation(detail.fixAvailable),
+      ...(advisory != null && {
+        title: advisory.title,
+        url: advisory.url,
+      }),
+    };
+  });
+
+  return {
+    vulnerabilities,
+    summary: npmAudit.metadata.vulnerabilities,
+  };
+}
+
+export function npmToFixInformation(
+  fixAvailable: boolean | NpmFixInformation,
+): string {
+  if (typeof fixAvailable === 'boolean') {
+    return fixAvailable ? 'Fix is available.' : '';
+  }
+
+  return `Fix available: Update \`${fixAvailable.name}\` to version **${
+    fixAvailable.version
+  }**${fixAvailable.isSemVerMajor ? ' (breaking change).' : '.'}`;
+}
+
+export function npmToAdvisory(
+  name: string,
+  vulnerabilities: NpmVulnerabilities,
+  prevNodes: Set<string> = new Set(),
+): NpmAdvisory | null {
+  const advisory = vulnerabilities[name]?.via;
+
+  if (
+    Array.isArray(advisory) &&
+    advisory.length > 0 &&
+    typeof advisory[0] === 'object'
+  ) {
+    return { title: advisory[0].title, url: advisory[0].url };
+  }
+
+  // Cross-references another vulnerability
+  if (
+    Array.isArray(advisory) &&
+    advisory.length > 0 &&
+    typeof advisory[0] === 'string'
+  ) {
+    /* eslint-disable functional/no-let, functional/immutable-data, functional/no-loop-statements, prefer-const */
+    let advisoryInfo: NpmAdvisory | null = null;
+    let newReferences: string[] = [];
+    let advisoryInfoFound = false;
+    /* eslint-enable functional/no-let, prefer-const */
+
+    for (const via of advisory as string[]) {
+      if (!prevNodes.has(via)) {
+        newReferences.push(via);
+      }
+    }
+
+    while (newReferences.length > 0 && !advisoryInfoFound) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const ref = newReferences.pop()!;
+      const result = npmToAdvisory(ref, vulnerabilities, prevNodes);
+
+      if (result == null) {
+        prevNodes.add(ref);
+        continue;
+      }
+
+      advisoryInfo = { title: result.title, url: result.url };
+      advisoryInfoFound = true;
+    }
+    /* eslint-enable functional/immutable-data, functional/no-loop-statements */
+
+    return advisoryInfo;
+  }
+
+  return null;
+}
+
+export function yarnv1ToAuditResult(output: string): AuditResult {
+  const yarnv1Result = JSON.parse(output) as Yarnv1AuditResultJson;
+
+  const [yarnv1Advisory, yarnv1Summary] = validateYarnv1Result(yarnv1Result);
+
+  const vulnerabilities = yarnv1Advisory.map<AuditResult['vulnerabilities'][0]>(
+    ({ data: { resolution, advisory } }) => {
+      const directDependency = resolution.path.slice(
+        0,
+        resolution.path.indexOf('>'),
+      );
+
+      return {
+        name: advisory.module_name,
+        title: advisory.title,
+        url: advisory.url,
+        severity: advisory.severity,
+        versionRange: advisory.vulnerable_versions,
+        directDependency:
+          advisory.module_name === directDependency ? true : directDependency,
+        fixInformation: advisory.recommendation,
+      };
+    },
+  );
+
+  const summary = {
+    ...yarnv1Summary.data.vulnerabilities,
+    total: Object.values(yarnv1Summary.data.vulnerabilities).reduce(
+      (acc, amount) => acc + amount,
+      0,
+    ),
+  };
+
+  return {
+    vulnerabilities,
+    summary,
+  };
+}
+
+function validateYarnv1Result(
+  result: Yarnv1AuditResultJson,
+): [Yarnv1AuditAdvisory[], Yarnv1AuditSummary] {
+  if (result.length === 0) {
+    throw new Error('Invalid Yarn v1 audit result - no properties found.');
+  }
+
+  if (result.length === 1) {
+    if (result[0].type !== 'auditSummary') {
+      throw new Error('Invalid Yarn v1 audit result - no summary found.');
+    }
+
+    return [[], result[0]];
+  }
+
+  const vulnerabilities = result.filter(
+    item => item.type === 'auditAdvisory',
+  ) as Yarnv1AuditAdvisory[];
+
+  const summary = result.find(
+    item => item.type === 'auditSummary',
+  ) as Yarnv1AuditSummary;
+
+  return [vulnerabilities, summary];
+}
