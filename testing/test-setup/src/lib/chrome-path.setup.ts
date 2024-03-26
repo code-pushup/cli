932b876f6b0fcc60b0867622235cd5fde5816650
Michael <michael.hladky@push-based.io>
2024-03-26 19:46:39 +0100

    ci(plugin-lighthouse): add custom chrome path to CI

--- testing/test-setup/src/lib/chrome-path.setup.ts
+++ testing/test-setup/src/lib/chrome-path.setup.ts
@@ -1,18 +1,14 @@
-import { getChromePath } from 'chrome-launcher';
-import * as process from 'process';
-import { beforeEach, vi } from 'vitest';
+import {getChromePath} from 'chrome-launcher';
+import * as process from 'node:process';
+import {beforeEach, vi} from 'vitest';
 
 beforeEach(() => {
-  try {
-    vi.stubEnv('CHROME_PATH', getChromePath());
-  } catch (error) {
-    const customChromePath = (process.env as { CUSTOM_CHROME_PATH: string })
-      .CUSTOM_CHROME_PATH;
-    if (customChromePath == null || customChromePath === '') {
-      throw new Error(
-        (error as { message: string }).message + 'Chrome path not found. Please read the in the packages CONTRIBUTING.md/#chrome-path section.',
-      );
-    }
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const customChromePath = (process.env as { CUSTOM_CHROME_PATH: string | undefined })
+    .CUSTOM_CHROME_PATH;
+  if (customChromePath) {
     vi.stubEnv('CHROME_PATH', customChromePath);
+  } else {
+    vi.stubEnv('CHROME_PATH', getChromePath());
   }
 });
