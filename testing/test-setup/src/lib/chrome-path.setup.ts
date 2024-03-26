dde09c52b3ca8fd23949ddd57685fe2229cb8252
Michael <michael.hladky@push-based.io>
2024-03-26 19:47:06 +0100

    refactor(plugin-lighthouse): format

--- testing/test-setup/src/lib/chrome-path.setup.ts
+++ testing/test-setup/src/lib/chrome-path.setup.ts
@@ -1,11 +1,12 @@
-import {getChromePath} from 'chrome-launcher';
+import { getChromePath } from 'chrome-launcher';
 import * as process from 'node:process';
-import {beforeEach, vi} from 'vitest';
+import { beforeEach, vi } from 'vitest';
 
 beforeEach(() => {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  const customChromePath = (process.env as { CUSTOM_CHROME_PATH: string | undefined })
-    .CUSTOM_CHROME_PATH;
+  const customChromePath = (
+    process.env as { CUSTOM_CHROME_PATH: string | undefined }
+  ).CUSTOM_CHROME_PATH;
   if (customChromePath) {
     vi.stubEnv('CHROME_PATH', customChromePath);
   } else {
