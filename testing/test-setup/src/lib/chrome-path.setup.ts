d82f1abbcd1e1b20c7bf106d7e6736a47153a571
Michael <michael.hladky@push-based.io>
2024-03-26 19:37:34 +0100

    refactor(plugin-lighthouse): adopt path

--- testing/test-setup/src/lib/chrome-path.setup.ts
+++ testing/test-setup/src/lib/chrome-path.setup.ts
@@ -5,12 +5,12 @@ import { beforeEach, vi } from 'vitest';
 beforeEach(() => {
   try {
     vi.stubEnv('CHROME_PATH', getChromePath());
-  } catch (e) {
+  } catch (error) {
     const customChromePath = (process.env as { CUSTOM_CHROME_PATH: string })
       .CUSTOM_CHROME_PATH;
     if (customChromePath == null || customChromePath === '') {
       throw new Error(
-        'Chrome path not found. Please read the in the packages CONTRIBUTING.md/#chrome-path section.',
+        (error as { message: string }).message + 'Chrome path not found. Please read the in the packages CONTRIBUTING.md/#chrome-path section.',
       );
     }
     vi.stubEnv('CHROME_PATH', customChromePath);
