59b93f27ccb07eb772a3265c8cfd4945594c6148
Michael <michael.hladky@push-based.io>
2024-03-26 20:13:12 +0100

    refactor: debug chrome path 3

--- testing/test-setup/src/lib/chrome-path.setup.ts
+++ testing/test-setup/src/lib/chrome-path.setup.ts
@@ -3,14 +3,14 @@ import * as process from 'node:process';
 import { beforeEach, vi } from 'vitest';
 
 beforeEach(() => {
-  const customChromePath = (
+  const customChromePath =
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    process.env as { CUSTOM_CHROME_PATH: string | undefined }
-  ).CUSTOM_CHROME_PATH;
+    (process.env as { CUSTOM_CHROME_PATH: string | undefined })
+      .CUSTOM_CHROME_PATH;
 
-  if (customChromePath != null) {
-    vi.stubEnv('CHROME_PATH', customChromePath);
-  } else {
+  if (customChromePath == null) {
     vi.stubEnv('CHROME_PATH', getChromePath());
+  } else {
+    vi.stubEnv('CHROME_PATH', customChromePath);
   }
 });
