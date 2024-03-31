efc5d569b439ca1d1e811bce0d73463b12b104b1
Michael <michael.hladky@push-based.io>
2024-04-01 00:28:08 +0200

    remove perfect score

--- examples/plugins/src/js-benchmarking/src/config.ts
+++ examples/plugins/src/js-benchmarking/src/config.ts
@@ -6,14 +6,6 @@ export const jsBenchmarkingPluginConfigSchema = z.object({
   runnerPath: z.string().default(JS_BENCHMARKING_DEFAULT_RUNNER_PATH),
   tsconfig: z.string().optional(),
   outputDir: z.string().optional(),
-  verbose: z.boolean().optional(),
-  perfectScoreThreshold: z
-    .number({
-      description:
-        'Score will be 1 (perfect) for this difference in % and above. Score range is 0 - 1.',
-    })
-    .gt(0)
-    .max(1)
-    .optional(),
+  verbose: z.boolean().optional()
 });
 export type JsBenchmarkingPluginConfig = z.input<typeof jsBenchmarkingPluginConfigSchema>;
