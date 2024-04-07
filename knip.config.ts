1df53097361c3febb208aa352764e760f38127be
Michael <michael.hladky@push-based.io>
2024-04-08 01:37:29 +0200

    knip config update

--- knip.config.ts
+++ knip.config.ts
@@ -10,26 +10,45 @@ import {
   withVitest,
 } from '@beaussan/nx-knip';
 
-export const withIgnoreMockInLibs = () =>
+const withIgnoreMockInLibs = () =>
   withLibraryMapper({
     mapperFn: ({ rootFolder }) => {
       return {
-        ignore: [rootFolder + '/mocks/**'],
+        ignore: [rootFolder + '/mocks/**', /*rootFolder + '/perf/**' SHOULD WORK */],
         entry: [rootFolder + '/src/bin.ts', rootFolder + '/perf/**/index.ts'],
       };
     },
   });
 
-export const withCuNxStandards = (): KnipConfigPlugin => () => {
+const withExamplePlugins = (): KnipConfigPlugin => () => {
+  return {
+    entry: ['examples/plugins/src/index.ts'],
+  };
+};
+
+const withReactExample = (): KnipConfigPlugin => () => {
+  return {
+    entry: ['examples/react-todos-app/src/index.jsx'],
+    eslint: {
+      // Given there is no lint target on the project, we need to manually specify the entry point
+      config: ['examples/react-todos-app/.eslintrc.js'],
+    },
+  };
+};
+
+const withNxStandards = (): KnipConfigPlugin => () => {
   return {
     project: ['**/*.{ts,js,tsx,jsx}'],
-    ignore: ['tmp/**', 'node_modules/**', 'examples/**'],
+    ignore: ['tmp/**', 'node_modules/**'],
+    commitlint: {
+      config: ['commitlint.config.js'],
+    },
     entry: [
+      // unknown why this is needed, it should be picked up by knip from the vitest setup files
       'testing/test-utils/src/index.ts',
-      'testing/test-utils/src/lib/fixtures/configs/*.{js,mjs,ts,cjs,mts,cts}',
+      'testing/test-utils/src/lib/fixtures/configs/*.ts',
       'testing/test-setup/src/index.ts',
       'testing/test-setup/src/lib/**/*.{js,mjs,ts,cjs,mts,cts}',
-      // Same issue as the other vitest related, it should be picked up by knip from the vitest setup files
       'global-setup.ts',
       'global-setup.e2e.ts',
       // missing knip plugin for now, so this is in the root entry
@@ -44,7 +63,17 @@ export const withCuNxStandards = (): KnipConfigPlugin => () => {
       '@swc/cli',
       '@nx/plugin',
       '@nx/workspace',
-      '@example/custom-plugin',
+      // Same issue as the other vitest related, it should be picked up by knip from the vitest setup files
+      'global-setup.ts',
+      'global-setup.e2e.ts',
+
+      // Should be picked up by knip from the vitest setup files
+      'basic',
+      // Should be picked up by the commit lint knip config
+      'commitlint-plugin-tense',
+
+      // Prettier magic resolve is not picked up by knip
+      '@trivago/prettier-plugin-sort-imports',
     ],
   };
 };
@@ -57,5 +86,7 @@ export default combineNxKnipPlugins(
   withVitest(),
   withIgnoreMockInLibs(),
   withEslint(),
-  withCuNxStandards(),
+  withReactExample(),
+  withExamplePlugins(),
+  withNxStandards(),
 );
