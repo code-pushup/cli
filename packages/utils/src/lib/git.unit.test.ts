ba49dc7b2566785b7679307d7aa13859184d6008
Michael <michael.hladky@push-based.io>
2024-04-10 18:06:59 +0200

    cleanup

--- packages/utils/src/lib/git.unit.test.ts
+++ packages/utils/src/lib/git.unit.test.ts
@@ -1,12 +1,16 @@
-import {describe, expect} from 'vitest';
-import {guardAgainstLocalChanges} from "@code-pushup/utils";
-import {SimpleGit, StatusResult} from "simple-git";
-import {GitStatusError} from "./git";
+import { SimpleGit, StatusResult } from 'simple-git';
+import { describe, expect } from 'vitest';
+import { guardAgainstLocalChanges } from '@code-pushup/utils';
+import { GitStatusError } from './git';
 
 describe('guardAgainstLocalChanges', () => {
   it('should throw if no files are present', async () => {
     await expect(
-      guardAgainstLocalChanges({status: () => Promise.resolve({files: ['']})} as unknown as SimpleGit),
-    ).rejects.toThrow(new GitStatusError({files: ['']} as unknown as StatusResult));
+      guardAgainstLocalChanges({
+        status: () => Promise.resolve({ files: [''] }),
+      } as unknown as SimpleGit),
+    ).rejects.toThrow(
+      new GitStatusError({ files: [''] } as unknown as StatusResult),
+    );
   });
 });
