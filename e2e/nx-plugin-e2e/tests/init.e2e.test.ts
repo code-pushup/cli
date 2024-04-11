7ac82a50a0a0f411185c26d710000faaa5c77b0b
Michael <michael.hladky@push-based.io>
2024-04-11 20:00:53 +0200

    fix(e2e): nx-plugin tests

--- e2e/nx-plugin-e2e/tests/init.e2e.test.ts
+++ e2e/nx-plugin-e2e/tests/init.e2e.test.ts
@@ -5,7 +5,10 @@ describe('nx-plugin g init', () => {
   it('should run init generator and execute correctly', async () => {
     const { code, stderr } = await executeProcess({
       command: 'nx',
-      args: ['g', './dist/packages/nx-plugin:init examples-react-todos-app'],
+      args: [
+        'g',
+        './dist/packages/nx-plugin:init examples-react-todos-app --dryRun',
+      ],
       observer: { onStdout: console.info },
     });
 
@@ -18,12 +21,13 @@ describe('nx-plugin g configuration', () => {
   it('should run init generator on react-todos-app', async () => {
     const { code, stderr } = await executeProcess({
       command: 'nx',
-      args: ['g', './dist/packages/nx-plugin:init examples-react-todos-app'],
+      args: [
+        'g',
+        './dist/packages/nx-plugin:init examples-react-todos-app --dryRun',
+      ],
     });
 
     expect(code).toBe(0);
-    expect(stderr).toBe('');
-
-    // add code-pushup.config.ts
+    expect(stderr).toContain('dryRun');
   });
 });
