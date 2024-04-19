ad8a43cd304587548eb9d1d7700c4ee06f9c99d1
Michael <michael.hladky@push-based.io>
2024-04-19 23:45:20 +0200

    refactor(utils): adjust typing

--- packages/core/src/lib/implementation/report-to-gql.ts
+++ packages/core/src/lib/implementation/report-to-gql.ts
@@ -80,7 +80,7 @@ function auditToGQL(audit: AuditReport): PortalAudit {
     formattedValue,
     ...(details && {
       details: {
-        ...(issues.length > 0 ? { issues: issues.map(issueToGQL) } : {}),
+        ...(issues && { issues: issues.map(issueToGQL) }),
         // @TODO add when https://github.com/code-pushup/cli/issues/530 is implemented
         // ...(Object.keys(table).length ? {table} : {}),
       },
