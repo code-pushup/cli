9851c7220e383b244a6bcad11850d8f007e5c493
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-04-22 20:23:14 +0200

    Update packages/models/src/index.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -34,8 +34,6 @@ export {
 export { MaterialIcon, materialIconSchema } from './lib/implementation/schemas';
 export { exists } from './lib/implementation/utils';
 export {
-  TableHeading,
-  tableHeadingSchema,
   Table,
   tableSchema,
 } from './lib/table';
