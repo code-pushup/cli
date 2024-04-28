f02c3bfb04feffd36dd6e41771a764bfdddcde7d
Michael <michael.hladky@push-based.io>
2024-04-28 12:37:22 +0200

    test(test-setup): add fallback for CHROME_PATH in test setup

--- testing/test-setup/src/lib/chrome-path.setup.ts
+++ testing/test-setup/src/lib/chrome-path.setup.ts
@@ -1,4 +1,5 @@
 import { getChromePath } from 'chrome-launcher';
+import chromium from 'chromium';
 import * as process from 'node:process';
 import { beforeEach, vi } from 'vitest';
 
@@ -6,7 +7,20 @@ beforeEach(() => {
   const customChromePath = process.env['CUSTOM_CHROME_PATH'];
 
   if (customChromePath == null) {
-    vi.stubEnv('CHROME_PATH', getChromePath());
+    try {
+      const path = getChromePath();
+      vi.stubEnv('CHROME_PATH', path);
+    } catch (e) {
+      if ((e as Error).message.includes('No Chrome installations found.')) {
+        console.info(
+          `${(e as Error).message} Using chromium from node_modules instead: ${
+            chromium.path
+          }`,
+        );
+        vi.stubEnv('CHROME_PATH', chromium.path);
+      }
+      throw e;
+    }
   } else {
     vi.stubEnv('CHROME_PATH', customChromePath);
   }
