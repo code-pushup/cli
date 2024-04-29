87f108f2c35e97af9f18b0e73106406ec7a18c01
Michael <michael.hladky@push-based.io>
2024-04-29 09:21:05 +0200

    adjust types

--- testing/test-setup/src/lib/chrome-path.setup.ts
+++ testing/test-setup/src/lib/chrome-path.setup.ts
@@ -1,25 +1,28 @@
 import { getChromePath } from 'chrome-launcher';
-import chromium from 'chromium';
 import * as process from 'node:process';
 import { beforeEach, vi } from 'vitest';
 
-beforeEach(() => {
+beforeEach(async () => {
   const customChromePath = process.env['CUSTOM_CHROME_PATH'];
 
   if (customChromePath == null) {
     try {
       const path = getChromePath();
       vi.stubEnv('CHROME_PATH', path);
-    } catch (e) {
-      if ((e as Error).message.includes('No Chrome installations found.')) {
+    } catch (error) {
+      if ((error as Error).message.includes('No Chrome installations found.')) {
+        const chromium = (await import('chromium' as string)) as Record<
+          'path',
+          string
+        >;
         console.info(
-          `${(e as Error).message} Using chromium from node_modules instead: ${
-            chromium.path
-          }`,
+          `${
+            (error as Error).message
+          } Using chromium from node_modules instead: ${chromium.path}`,
         );
         vi.stubEnv('CHROME_PATH', chromium.path);
       }
-      throw e;
+      throw error;
     }
   } else {
     vi.stubEnv('CHROME_PATH', customChromePath);
--- types/chromium/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'chromium' {
-  export const path: string;
-  const chromium: {
-    path: string;
-  };
-  export default chromium;
-}
