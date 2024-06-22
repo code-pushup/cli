f2ea70d6b59ca39e9766b7004a7232f4db0935b1
Michael <michael.hladky@push-based.io>
2024-06-22 16:34:49 +0200

    refactor(plugin-lighthouse): fix lint

--- packages/plugin-lighthouse/src/lib/runner/details/opportunity.type.unit.test.ts
+++ packages/plugin-lighthouse/src/lib/runner/details/opportunity.type.unit.test.ts
@@ -2,7 +2,6 @@ import type Details from 'lighthouse/types/lhr/audit-details';
 import { describe, expect, it } from 'vitest';
 import type { Table } from '@code-pushup/models';
 import { parseOpportunityToAuditDetailsTable } from './opportunity.type';
-import { parseTableToAuditDetailsTable } from './table.type';
 import { LighthouseAuditDetailsParsingError } from './utils';
 
 describe('parseOpportunityDetails', () => {
