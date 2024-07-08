1e83a23d9171213fe3bbe7034595d3626b88092c
Michael <michael.hladky@push-based.io>
2024-07-08 21:26:54 +0200

    wip

--- packages/nx-plugin/mock/fixtures/env.ts
+++ packages/nx-plugin/mock/fixtures/env.ts
@@ -2,5 +2,5 @@ export const ENV = {
   CP_SERVER: 'https://portal.code.pushup.dev',
   CP_ORGANIZATION: 'code-pushup',
   CP_PROJECT: 'utils',
-  CP_API_KEY: 'ws435d6f7tg8zh9ujw0erfkßgtht0t8937r4gzews',
+  CP_API_KEY: '23456789098765432345678909876543',
 };
