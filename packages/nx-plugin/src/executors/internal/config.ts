1aeaabe1eb38e358ae8d0abafd42719fb515052c
Michael <michael.hladky@push-based.io>
2024-07-12 18:09:55 +0200

    refactor(nx-plugins): reduce logic

--- packages/nx-plugin/src/executors/internal/config.ts
+++ packages/nx-plugin/src/executors/internal/config.ts
@@ -1,7 +1,6 @@
-import { join } from 'node:path';
-import type { PersistConfig, UploadConfig } from '@code-pushup/models';
-import { slugify } from '../../internal/utils';
-import { parseEnv } from './env';
+import {join} from 'node:path';
+import type {PersistConfig, UploadConfig} from '@code-pushup/models';
+import {parseEnv} from './env';
 import {
   BaseNormalizedExecutorContext,
   GlobalExecutorOptions,
@@ -12,10 +11,10 @@ export function globalConfig(
   options: Partial<GlobalExecutorOptions>,
   context: BaseNormalizedExecutorContext,
 ): Required<GlobalExecutorOptions> {
-  const { projectConfig } = context;
-  const { root: projectRoot = '' } = projectConfig ?? {};
+  const {projectConfig} = context;
+  const {root: projectRoot = ''} = projectConfig ?? {};
   // For better debugging use `--verbose --no-progress` as default
-  const { verbose, progress, config } = options;
+  const {verbose, progress, config} = options;
   return {
     verbose: !!verbose,
     progress: !!progress,
@@ -27,20 +26,20 @@ export function persistConfig(
   options: Partial<PersistConfig & ProjectExecutorOnlyOptions>,
   context: BaseNormalizedExecutorContext,
 ): Partial<PersistConfig> {
-  const { projectConfig } = context;
+  const {projectConfig} = context;
 
-  const { name: projectName = '', root: projectRoot = '' } =
-    projectConfig ?? {};
+  const {name: projectName = '', root: projectRoot = ''} =
+  projectConfig ?? {};
   const {
     format,
     outputDir = join(projectRoot, '.code-pushup', projectName), // always in <root>/.code-pushup/<project-name>,
-    filename: filenameOptions,
+    filename,
   } = options;
 
   return {
     outputDir,
-    ...(format ? { format } : {}),
-    ...(filenameOptions ? { filename: slugify(filenameOptions) } : {}),
+    ...(format ? {format} : {}),
+    ...(filename ? {filename} : {}),
   };
 }
 
@@ -48,22 +47,22 @@ export function uploadConfig(
   options: Partial<UploadConfig & ProjectExecutorOnlyOptions>,
   context: BaseNormalizedExecutorContext,
 ): Partial<UploadConfig> {
-  const { projectConfig, workspaceRoot } = context;
+  const {projectConfig, workspaceRoot} = context;
 
-  const { name: projectName } = projectConfig ?? {};
-  const { projectPrefix, server, apiKey, organization, project, timeout } =
+  const {name: projectName} = projectConfig ?? {};
+  const {projectPrefix, server, apiKey, organization, project, timeout} =
     options;
   const applyPrefix = workspaceRoot === '.';
   const prefix = projectPrefix ? `${projectPrefix}-` : '';
   return {
     ...(projectName
       ? {
-          project: applyPrefix ? `${prefix}${projectName}` : projectName, // provide correct project
-        }
+        project: applyPrefix ? `${prefix}${projectName}` : projectName, // provide correct project
+      }
       : {}),
     ...parseEnv(process.env),
     ...Object.fromEntries(
-      Object.entries({ server, apiKey, organization, project, timeout }).filter(
+      Object.entries({server, apiKey, organization, project, timeout}).filter(
         ([_, v]) => v !== undefined,
       ),
     ),
