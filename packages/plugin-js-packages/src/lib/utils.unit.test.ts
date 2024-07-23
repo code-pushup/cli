de90366224ca48fbbd5cc4e5c8750f24e392ca7f
Michael <michael.hladky@push-based.io>
2024-07-23 19:47:51 +0200

    feat(plugin-js-packages): update docs

--- packages/plugin-js-packages/src/lib/utils.unit.test.ts
+++ packages/plugin-js-packages/src/lib/utils.unit.test.ts
@@ -17,22 +17,32 @@ describe('derivePackageManager', () => {
   });
 
   it('should return npm if a package.jock.json is present', async () => {
-    fileExistsSpy.mockImplementation(path => Promise.resolve(path.endsWith('package-lock.json')));
+    fileExistsSpy.mockImplementation(path =>
+      Promise.resolve(path.endsWith('package-lock.json')),
+    );
     await expect(derivePackageManager()).resolves.toBe('npm');
   });
 
   it('should return pnpm if pnpm-lock.yaml is present', async () => {
-    fileExistsSpy.mockImplementation(path => Promise.resolve(path.endsWith('pnpm-lock.yaml')));
+    fileExistsSpy.mockImplementation(path =>
+      Promise.resolve(path.endsWith('pnpm-lock.yaml')),
+    );
     await expect(derivePackageManager()).resolves.toBe('pnpm');
   });
 
   it('should return yarn-classic if yarn.lock is present', async () => {
-    fileExistsSpy.mockImplementation(path => Promise.resolve(path.endsWith('yarn.lock')));
+    fileExistsSpy.mockImplementation(path =>
+      Promise.resolve(path.endsWith('yarn.lock')),
+    );
     await expect(derivePackageManager()).resolves.toBe('yarn-classic');
   });
 
   it('should return yarn-modern if yarn.lock and .yarnrc.yml is present', async () => {
-    fileExistsSpy.mockImplementation(path => Promise.resolve(path.endsWith('yarn.lock') || path.endsWith('.yarnrc.yml')));
+    fileExistsSpy.mockImplementation(path =>
+      Promise.resolve(
+        path.endsWith('yarn.lock') || path.endsWith('.yarnrc.yml'),
+      ),
+    );
     await expect(derivePackageManager()).resolves.toBe('yarn-modern');
   });
 });
