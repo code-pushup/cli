e576eb7a4d1f0e4c0f846aaaf22f97f999ad0dad
Michael <michael.hladky@push-based.io>
2024-07-23 12:18:23 +0200

    refactor: fix main and configure models cp config

--- code-pushup.config.ts
+++ code-pushup.config.ts
@@ -1,5 +1,6 @@
 import { DEFAULT_FLAGS } from 'chrome-launcher/dist/flags.js';
 import 'dotenv/config';
+import { z } from 'zod';
 import {
   fileSizePlugin,
   fileSizeRecommendedRefs,
@@ -20,7 +21,25 @@ import {
 } from './dist/packages/plugin-lighthouse';
 import type { CoreConfig } from './packages/models/src';
 
+// load upload configuration from environment
+const envSchema = z.object({
+  CP_SERVER: z.string().url(),
+  CP_API_KEY: z.string().min(1),
+  CP_ORGANIZATION: z.string().min(1),
+  CP_PROJECT: z.string().min(1),
+});
+const { data: env } = await envSchema.safeParseAsync(process.env);
+
 const config: CoreConfig = {
+  ...(env && {
+    upload: {
+      server: env.CP_SERVER,
+      apiKey: env.CP_API_KEY,
+      organization: env.CP_ORGANIZATION,
+      project: env.CP_PROJECT,
+    },
+  }),
+
   plugins: [
     await eslintPlugin(await eslintConfigFromAllNxProjects()),
 
--- packages/models/code-pushup.config.ts
+++ packages/models/code-pushup.config.ts
@@ -1,12 +1,56 @@
 import eslintPlugin from '../../dist/packages/plugin-eslint';
 import { eslintConfigFromNxProject } from '../../dist/packages/plugin-eslint';
+import jsPackagesPlugin from '../../dist/packages/plugin-js-packages';
 import type { CoreConfig } from '../../packages/models/src';
 
-const eslintPluginPath = await eslintConfigFromNxProject('models');
-//throw new Error(JSON.stringify(await eslintPlugin(eslintPluginPath), null, 2))
-// see: https://github.com/code-pushup/cli/blob/main/packages/models/docs/models-reference.md#coreconfig
 const config: CoreConfig = {
-  plugins: [await eslintPlugin(eslintPluginPath)],
+  plugins: [
+    await eslintPlugin(await eslintConfigFromNxProject('models')),
+    await jsPackagesPlugin({ packageManager: 'npm' }),
+  ],
+  categories: [
+    {
+      slug: 'bug-prevention',
+      title: 'Bug prevention',
+      description: 'Lint rules that find **potential bugs** in your code.',
+      refs: [{ type: 'group', plugin: 'eslint', slug: 'problems', weight: 1 }],
+    },
+    {
+      slug: 'code-style',
+      title: 'Code style',
+      description:
+        'Lint rules that promote **good practices** and consistency in your code.',
+      refs: [
+        { type: 'group', plugin: 'eslint', slug: 'suggestions', weight: 1 },
+      ],
+    },
+    {
+      slug: 'security',
+      title: 'Security',
+      description: 'Finds known **vulnerabilities** in 3rd-party packages.',
+      refs: [
+        {
+          type: 'group',
+          plugin: 'js-packages',
+          slug: 'npm-audit',
+          weight: 1,
+        },
+      ],
+    },
+    {
+      slug: 'updates',
+      title: 'Updates',
+      description: 'Finds **outdated** 3rd-party packages.',
+      refs: [
+        {
+          type: 'group',
+          plugin: 'js-packages',
+          slug: 'npm-outdated',
+          weight: 1,
+        },
+      ],
+    },
+  ],
 };
 
 export default config;
