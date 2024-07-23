1a589b35fa3b945103f109f0c5e4344e3ecefdcb
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-07-23 16:09:45 +0200

    Update packages/nx-plugin/src/plugin/target/configuration.target.unit.test.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/nx-plugin/src/plugin/target/configuration.target.unit.test.ts
+++ packages/nx-plugin/src/plugin/target/configuration.target.unit.test.ts
@@ -3,9 +3,8 @@ import { createConfigurationTarget } from './configuration-target';
 
 describe('createConfigurationTarget', () => {
   it('should return code-pushup--configuration target for given project', () => {
-    const projectName = 'plugin-my-plugin';
-    expect(createConfigurationTarget(projectName)).toStrictEqual({
-      command: `nx g nx-plugin:configuration --project=${projectName}`,
+    expect(createConfigurationTarget('my-project')).toStrictEqual({
+      command: 'nx g nx-plugin:configuration --project=my-project',
     });
   });
 
