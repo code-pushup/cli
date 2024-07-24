cf426bb2b57ca2cf87d23d44d843110cf9b016f9
Michael <michael.hladky@push-based.io>
2024-07-24 17:30:29 +0200

    fix

--- packages/plugin-js-packages/src/lib/utils.ts
+++ packages/plugin-js-packages/src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { ExecException } from 'child_process';
+import { exec } from 'node:child_process';
 import { join } from 'node:path';
 import { fileExists } from '@code-pushup/utils';
 import { PackageManagerId } from './config';
@@ -12,7 +14,23 @@ export async function derivePackageManager(
   } else if (await fileExists(join(currentDir, 'pnpm-lock.yaml'))) {
     return 'pnpm';
   } else if (await fileExists(join(currentDir, 'yarn.lock'))) {
-    if (await fileExists(join(currentDir, '.yarnrc.yml'))) {
+    const yarnVersion = await new Promise<string>((resolve, reject) => {
+      const { stdout } = exec(
+        'yarn -v',
+        (error: ExecException | null, stdout: string, stderr: string) => {
+          if (error) {
+            reject(error);
+          }
+          if (stderr) {
+            reject(stderr);
+          }
+
+          resolve(stdout?.toString().trim().at(0) ?? '');
+        },
+      );
+    });
+
+    if (yarnVersion === '2' || yarnVersion === '3') {
       return 'yarn-modern';
     }
     return 'yarn-classic';
--- packages/plugin-js-packages/src/lib/utils.unit.test.ts
+++ packages/plugin-js-packages/src/lib/utils.unit.test.ts
@@ -1,48 +1,97 @@
-import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { vol } from 'memfs';
+import { exec } from 'node:child_process';
+import { MockInstance, beforeEach, describe, expect, it, vi } from 'vitest';
+import { MEMFS_VOLUME } from '@code-pushup/test-utils';
 import * as utils from '@code-pushup/utils';
 import { derivePackageManager } from './utils';
 
+vi.mock('child_process', () => {
+  const actual = vi.importActual('child_process');
+  return {
+    ...actual,
+    exec: vi.fn(),
+  };
+});
+
 describe('derivePackageManager', () => {
   const fileExistsSpy = vi.spyOn(utils, 'fileExists');
+  // const execSpy = vi.spyOn(child_process, 'exec');
 
   beforeEach(() => {
-    fileExistsSpy.mockResolvedValue(false);
+    fileExistsSpy.mockClear();
   });
-  afterEach(() => {
-    fileExistsSpy.mockReset();
+
+  it('should return npm if a package.jock.json is present', async () => {
+    vol.fromJSON(
+      {
+        'package-lock.json': '{}',
+      },
+      MEMFS_VOLUME,
+    );
+    await expect(derivePackageManager()).resolves.toBe('npm');
+    expect(fileExistsSpy).toHaveBeenCalledWith('/test/package-lock.json');
+    expect(exec).not.toHaveBeenCalled();
+  });
+
+  it('should return pnpm if pnpm-lock.yaml is present', async () => {
+    vol.fromJSON(
+      {
+        'pnpm-lock.yaml': '',
+      },
+      MEMFS_VOLUME,
+    );
+    await expect(derivePackageManager()).resolves.toBe('pnpm');
+    expect(fileExistsSpy).toHaveBeenCalledWith('/test/pnpm-lock.yaml');
+    expect(exec).not.toHaveBeenCalled();
+  });
+
+  it('should return yarn-classic if yarn.lock is present', async () => {
+    vol.fromJSON(
+      {
+        'yarn.lock': '',
+      },
+      MEMFS_VOLUME,
+    );
+    (exec as MockInstance).mockImplementation((_, fn) => fn(null, '1.22.19'));
+
+    await expect(derivePackageManager()).resolves.toBe('yarn-classic');
+    expect(fileExistsSpy).toHaveBeenCalledWith('/test/yarn.lock');
+    expect(exec).toHaveBeenCalledTimes(1);
+    expect(exec).toHaveBeenCalledWith('yarn -v', expect.any(Function));
+  });
+
+  it('should return yarn-modern if yarn version is 2', async () => {
+    vol.fromJSON(
+      {
+        'yarn.lock': '',
+      },
+      MEMFS_VOLUME,
+    );
+    (exec as MockInstance).mockImplementation((_, fn) => fn(null, '2.22.19'));
+
+    await expect(derivePackageManager()).resolves.toBe('yarn-modern');
+    expect(fileExistsSpy).toHaveBeenCalledWith('/test/yarn.lock');
+    expect(exec).toHaveBeenCalledTimes(1);
+    expect(exec).toHaveBeenCalledWith('yarn -v', expect.any(Function));
+  });
+
+  it('should return yarn-modern if yarn version is 3', async () => {
+    vol.fromJSON(
+      {
+        'yarn.lock': '',
+      },
+      MEMFS_VOLUME,
+    );
+    (exec as MockInstance).mockImplementation((_, fn) => fn(null, '3.22.19'));
+
+    await expect(derivePackageManager()).resolves.toBe('yarn-modern');
+    expect(fileExistsSpy).toHaveBeenCalledWith('/test/yarn.lock');
+    expect(exec).toHaveBeenCalledTimes(1);
+    expect(exec).toHaveBeenCalledWith('yarn -v', expect.any(Function));
   });
 
   it('should fall back to NPM if neither filesystem nor env shows hints', async () => {
     await expect(derivePackageManager()).resolves.toBe('npm');
-  });
-
-  it('should return npm if a package.jock.json is present', async () => {
-    fileExistsSpy.mockImplementation(path =>
-      Promise.resolve(path.endsWith('package-lock.json')),
-    );
-    await expect(derivePackageManager()).resolves.toBe('npm');
-  });
-
-  it('should return pnpm if pnpm-lock.yaml is present', async () => {
-    fileExistsSpy.mockImplementation(path =>
-      Promise.resolve(path.endsWith('pnpm-lock.yaml')),
-    );
-    await expect(derivePackageManager()).resolves.toBe('pnpm');
-  });
-
-  it('should return yarn-classic if yarn.lock is present', async () => {
-    fileExistsSpy.mockImplementation(path =>
-      Promise.resolve(path.endsWith('yarn.lock')),
-    );
-    await expect(derivePackageManager()).resolves.toBe('yarn-classic');
-  });
-
-  it('should return yarn-modern if yarn.lock and .yarnrc.yml is present', async () => {
-    fileExistsSpy.mockImplementation(path =>
-      Promise.resolve(
-        path.endsWith('yarn.lock') || path.endsWith('.yarnrc.yml'),
-      ),
-    );
-    await expect(derivePackageManager()).resolves.toBe('yarn-modern');
+    expect(fileExistsSpy).toHaveBeenCalledWith('/test/package-lock.json');
   });
 });
