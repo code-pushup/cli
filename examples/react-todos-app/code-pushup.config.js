14749a4253ef9d1471202a97b780cf3a5c8abf5f
Michael <michael.hladky@push-based.io>
2024-08-02 03:01:17 +0200

    wip

--- examples/react-todos-app/code-pushup.config.js
+++ examples/react-todos-app/code-pushup.config.js
@@ -39,7 +39,7 @@ export default {
         // seo category
         'hreflang',
       ],
-      chromeFlags: DEFAULT_FLAGS.concat(['--headless', '--timeout=200000']),
+      chromeFlags: DEFAULT_FLAGS.concat(['--headless']),
     }),
   ],
   categories: [
