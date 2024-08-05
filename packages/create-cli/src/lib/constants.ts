1e72a72227ca57191a7d8f45d07d0751065ca6ff
Michael <michael.hladky@push-based.io>
2024-08-05 21:20:30 +0200

    fix

--- packages/create-cli/src/lib/constants.ts
+++ packages/create-cli/src/lib/constants.ts
@@ -1,7 +1,6 @@
 export const NX_JSON_FILENAME = 'nx.json';
 export const NX_JSON_CONTENT = JSON.stringify({
   $schema: './node_modules/nx/schemas/nx-schema.json',
-  releaseTagPattern: 'v{version}',
   targetDefaults: {},
 });
 export const PROJECT_NAME = 'source-root';
