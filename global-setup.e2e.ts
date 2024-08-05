898945ca2df62e39b170f667c96bd576da35df31
Michael <michael.hladky@push-based.io>
2024-08-05 20:20:35 +0200

    ci: improve registry logic to e2e test packages

--- global-setup.e2e.ts
+++ global-setup.e2e.ts
@@ -4,22 +4,36 @@ import { setupTestFolder, teardownTestFolder } from './testing/test-setup/src';
 import startLocalRegistry from './tools/scripts/start-local-registry';
 import stopLocalRegistry from './tools/scripts/stop-local-registry';
 
+const packages = [
+  'cli',
+  'nx-plugin',
+  'eslint-plugin',
+  'coverage-plugin',
+  'js-packages-plugin',
+  'lighthouse-plugin',
+];
+
 export async function setup() {
   await globalSetup();
-  await startLocalRegistry();
-  execSync('npm install -D @code-pushup/cli@e2e');
-  execSync('npm install -D @code-pushup/nx-plugin@e2e');
-  execSync('npm install -D @code-pushup/eslint-plugin@e2e');
-  execSync('npm install -D @code-pushup/coverage-plugin@e2e');
-  await setupTestFolder('tmp/e2e');
+  const registry = await startLocalRegistry();
+  // we can only install packages released by nx, see nx.json release.projects globs
+  try {
+    packages.forEach(packageName => {
+      execSync(
+        `npm install -D @code-pushup/${packageName}@e2e --force --registry=${registry}`,
+      );
+    });
+    await setupTestFolder('tmp/e2e');
+  } catch (e) {
+    stopLocalRegistry();
+  }
 }
 
 export async function teardown() {
   stopLocalRegistry();
-  execSync('npm uninstall @code-pushup/cli');
-  execSync('npm uninstall @code-pushup/nx-plugin');
-  execSync('npm uninstall @code-pushup/eslint-plugin');
-  execSync('npm uninstall @code-pushup/coverage-plugin');
+  packages.forEach(packageName => {
+    execSync(`npm uninstall @code-pushup/${packageName}`);
+  });
   await teardownTestFolder('tmp/e2e');
   await teardownTestFolder('tmp/local-registry');
 }
--- tools/scripts/start-local-registry.ts
+++ tools/scripts/start-local-registry.ts
@@ -2,41 +2,79 @@
  * This script starts a local registry for e2e testing purposes.
  * It is meant to be called in jest's globalSetup.
  */
-import { execFileSync, execSync, spawn } from 'child_process';
+import { execSync, spawn } from 'node:child_process';
+import { join } from 'node:path';
+import { releasePublish, releaseVersion } from 'nx/release';
+
+function isProcessRunning(pid) {
+  try {
+    // Send a signal of 0 to check if the process exists
+    process.kill(pid, 0);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+function killSafe(pid) {
+  if (isProcessRunning(pid)) {
+    process.kill(pid);
+  }
+}
 
 export default async () => {
   // local registry target to run
   const localRegistryTarget = '@code-pushup/cli-source:local-registry';
   // storage folder for the local registry
-  const storage = './tmp/local-registry/storage';
+  const storage = join(
+    'tmp',
+    'local-registry',
+    'storage',
+    process.env.NX_TASK_TARGET_PROJECT ?? '',
+  );
 
-  global.stopLocalRegistry = await startLocalRegistry({
+  // using storage
+  const { registry, stop } = await startLocalRegistry({
     localRegistryTarget,
     storage,
     verbose: true,
   });
 
-  // find latest version
-  const version = execSync('git describe --tags --abbrev=0')
-    .toString()
-    .trim()
-    .replace(/^v/, '');
+  if (!registry) {
+    console.error('Local registry not started');
+    process.exit(1);
+  }
+  global.stopLocalRegistry = stop;
+  global.registry = registry;
+  try {
+    //this enables getting the latest version of the packages (since versioning is tag-based and unique across all projects.)
+    const version = execSync('git describe --tags --abbrev=0')
+      .toString()
+      .trim();
 
-  // is is also possible to use nx release to publish the packages to the local registry
-  execFileSync(
-    'npx',
-    [
-      'nx',
-      'run-many',
-      '--targets',
-      'publish',
-      '--ver',
-      version,
-      '--tag',
-      'e2e',
-    ],
-    { env: process.env, stdio: 'inherit', shell: true },
-  );
+    await releaseVersion({
+      specifier: version.substring(1),
+      stageChanges: false,
+      gitCommit: false,
+      gitTag: false,
+      firstRelease: true,
+      generatorOptionsOverrides: {
+        skipLockFileUpdate: true,
+      },
+    });
+
+    await releasePublish({
+      tag: 'e2e',
+      registry,
+    });
+    // Adaptation of [the setup suggested by Nx](https://nx.dev/recipes/nx-release/update-local-registry-setup).
+    // Define explicitly the registry to remove dependency on NPM config.
+    return registry;
+  } catch (e) {
+    console.error(e);
+    global.stopLocalRegistry();
+    process.exit(1);
+  }
 };
 
 // soft copy from https://github.com/nrwl/nx/blob/16.9.x/packages/js/src/plugins/jest/start-local-registry.ts
@@ -54,63 +92,78 @@ function startLocalRegistry({
   if (!localRegistryTarget) {
     throw new Error(`localRegistryTarget is required`);
   }
-  return new Promise<() => void>((resolve, reject) => {
-    const childProcess = spawn(
-      'npx',
-      [
-        'nx',
-        ...`run ${localRegistryTarget} --location none --clear true`.split(' '),
-        ...(storage ? [`--storage`, storage] : []),
-      ],
-      { stdio: 'pipe', shell: true },
-    );
 
-    const listener = data => {
-      if (verbose) {
-        process.stdout.write(data);
-      }
-      if (data.toString().includes('http://localhost:')) {
-        const port = parseInt(
-          data.toString().match(/localhost:(?<port>\d+)/)?.groups?.port,
-        );
-        console.info('Local registry started on port ' + port);
+  return new Promise<{ stop: () => void; registry: string | null }>(
+    (resolve, reject) => {
+      const childProcess = spawn(
+        'npx',
+        [
+          'nx',
+          ...`run ${localRegistryTarget} --location none --clear true`.split(
+            ' ',
+          ),
+          ...(storage ? [`--storage`, storage] : []),
+        ],
+        { stdio: 'pipe', shell: true, detached: true },
+      );
 
-        const registry = `http://localhost:${port}`;
-        process.env.npm_config_registry = registry;
-        execSync(
-          `npm config set //localhost:${port}/:_authToken "secretVerdaccioToken"`,
-        );
+      const listener = data => {
+        if (verbose) {
+          process.stdout.write(data);
+        }
+        if (data.toString().includes('http://localhost:')) {
+          const port = parseInt(
+            data.toString().match(/localhost:(?<port>\d+)/)?.groups?.port,
+          );
+          console.info('Local registry started on port ' + port);
 
-        // yarnv1
-        process.env.YARN_REGISTRY = registry;
-        // yarnv2
-        process.env.YARN_NPM_REGISTRY_SERVER = registry;
-        process.env.YARN_UNSAFE_HTTP_WHITELIST = 'localhost';
+          const registry = `http://localhost:${port}`;
+          process.env.npm_config_registry = registry;
+          execSync(
+            `npm config set //localhost:${port}/:_authToken "secretVerdaccioToken"`,
+          );
 
-        console.info('Set npm and yarn config registry to ' + registry);
+          // yarnv1
+          process.env.YARN_REGISTRY = registry;
+          // yarnv2
+          process.env.YARN_NPM_REGISTRY_SERVER = registry;
+          process.env.YARN_UNSAFE_HTTP_WHITELIST = 'localhost';
 
-        resolve(() => {
-          childProcess.kill();
-          execSync(`npm config delete //localhost:${port}/:_authToken`);
-        });
-        childProcess?.stdout?.off('data', listener);
-      }
-    };
-    childProcess?.stdout?.on('data', listener);
-    childProcess?.stderr?.on('data', data => {
-      process.stderr.write(data);
-    });
-    childProcess.on('error', err => {
-      console.error('local registry error', err);
-      reject(err);
-    });
-    childProcess.on('exit', code => {
-      console.info('local registry exit', code);
-      if (code !== 0) {
-        reject(code);
-      } else {
-        resolve(() => {});
-      }
-    });
-  });
+          console.info('Set npm and yarn config registry to ' + registry);
+
+          resolve({
+            registry,
+            stop: () => {
+              if (childProcess.pid) {
+                killSafe(-childProcess.pid);
+              }
+              // does not kill the underlying process, see https://github.com/nodejs/node/issues/46865
+              childProcess.kill();
+              execSync(`npm config delete //localhost:${port}/:_authToken`);
+            },
+          });
+          childProcess?.stdout?.off('data', listener);
+        }
+      };
+      childProcess?.stdout?.on('data', listener);
+      childProcess?.stderr?.on('data', data => {
+        process.stderr.write(data);
+      });
+      childProcess.on('error', err => {
+        console.error('local registry error', err);
+        reject(err);
+      });
+      childProcess.on('exit', code => {
+        console.info('local registry exit', code);
+        if (code !== 0) {
+          reject(code);
+        } else {
+          resolve({
+            registry: null,
+            stop: () => {},
+          });
+        }
+      });
+    },
+  );
 }
