1041524d4bb3af3d7b2c68d9339f38ecf0238499
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-08-06 11:58:31 +0200

    feat(nx-plugin): extend config generator (#778)

--- packages/nx-plugin/src/generators/configuration/generator.integration.test.ts
+++ packages/nx-plugin/src/generators/configuration/generator.integration.test.ts
@@ -4,81 +4,9 @@ import {
   readProjectConfiguration,
 } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
-import { join } from 'node:path';
 import { afterEach, describe, expect, it } from 'vitest';
-import {
-  addTargetToProject,
-  configurationGenerator,
-  generateCodePushupConfig,
-} from './generator';
-
-describe('generateCodePushupConfig', () => {
-  let tree: Tree;
-  const testProjectName = 'test-app';
-  beforeEach(() => {
-    tree = createTreeWithEmptyWorkspace();
-    addProjectConfiguration(tree, testProjectName, {
-      root: 'test-app',
-    });
-  });
-
-  it('should add code-pushup.config.ts to the project root', () => {
-    generateCodePushupConfig(
-      tree,
-      {
-        root: testProjectName,
-        projectType: 'library',
-        sourceRoot: `${testProjectName}/src`,
-        targets: {},
-      },
-      {
-        project: testProjectName,
-      },
-    );
-
-    expect(tree.exists('test-app/code-pushup.config.ts')).toBe(true);
-    expect(
-      tree.read('test-app/code-pushup.config.ts')?.toString(),
-    ).toMatchSnapshot();
-  });
-
-  it('should skip code-pushup.config.ts generation if config in ts, mjs or js format already exists', () => {
-    tree.write(join('code-pushup.config.js'), 'export default {}');
-
-    generateCodePushupConfig(
-      tree,
-      {
-        root: testProjectName,
-        projectType: 'library',
-        sourceRoot: `${testProjectName}/src`,
-        targets: {},
-      },
-      {
-        project: testProjectName,
-      },
-    );
-
-    expect(tree.exists('code-pushup.config.ts')).toBe(false);
-  });
-
-  it('should skip code-pushup.config.ts generation if skipConfig is given', () => {
-    generateCodePushupConfig(
-      tree,
-      {
-        root: testProjectName,
-        projectType: 'library',
-        sourceRoot: `${testProjectName}/src`,
-        targets: {},
-      },
-      {
-        project: testProjectName,
-        skipConfig: true,
-      },
-    );
-
-    expect(tree.exists('code-pushup.config.ts')).toBe(false);
-  });
-});
+import { DEFAULT_TARGET_NAME } from '../../internal/constants';
+import { addTargetToProject, configurationGenerator } from './generator';
 
 describe('addTargetToProject', () => {
   let tree: Tree;
@@ -113,7 +41,7 @@ describe('addTargetToProject', () => {
       testProjectName,
     );
 
-    expect(projectConfiguration.targets?.['code-pushup']).toEqual({
+    expect(projectConfiguration.targets?.[DEFAULT_TARGET_NAME]).toEqual({
       executor: '@code-pushup/nx-plugin:autorun',
     });
   });
@@ -142,28 +70,6 @@ describe('addTargetToProject', () => {
       executor: '@code-pushup/nx-plugin:autorun',
     });
   });
-
-  it('should skip target creation if skipTarget is used', () => {
-    addTargetToProject(
-      tree,
-      {
-        root: testProjectName,
-        projectType: 'library',
-        sourceRoot: `${testProjectName}/src`,
-        targets: {},
-      },
-      {
-        project: testProjectName,
-        skipTarget: true,
-      },
-    );
-
-    const projectConfiguration = readProjectConfiguration(
-      tree,
-      testProjectName,
-    );
-    expect(projectConfiguration.targets).toBeUndefined();
-  });
 });
 
 describe('configurationGenerator', () => {
@@ -190,8 +96,21 @@ describe('configurationGenerator', () => {
       testProjectName,
     );
 
-    expect(projectConfiguration.targets?.['code-pushup']).toEqual({
+    expect(projectConfiguration.targets?.[DEFAULT_TARGET_NAME]).toEqual({
       executor: '@code-pushup/nx-plugin:autorun',
     });
+  });
+
+  it('should skip target creation if skipTarget is used', async () => {
+    await configurationGenerator(tree, {
+      project: testProjectName,
+      skipTarget: true,
+    });
+
+    const projectConfiguration = readProjectConfiguration(
+      tree,
+      testProjectName,
+    );
+    expect(projectConfiguration.targets).toBeUndefined();
   });
 });
--- packages/nx-plugin/src/generators/configuration/generator.ts
+++ packages/nx-plugin/src/generators/configuration/generator.ts
@@ -1,13 +1,12 @@
 import {
   Tree,
   formatFiles,
-  generateFiles,
   readProjectConfiguration,
   updateProjectConfiguration,
 } from '@nx/devkit';
-import { join } from 'node:path';
 import { ProjectConfiguration } from 'nx/src/config/workspace-json-project-json';
 import { DEFAULT_TARGET_NAME } from '../../internal/constants';
+import { generateCodePushupConfig } from './code-pushup-config';
 import { ConfigurationGeneratorOptions } from './schema';
 
 export async function configurationGenerator(
@@ -16,11 +15,19 @@ export async function configurationGenerator(
 ) {
   const projectConfiguration = readProjectConfiguration(tree, options.project);
 
-  generateCodePushupConfig(tree, projectConfiguration, options);
+  const { skipConfig, skipTarget, skipFormat } = options;
 
-  addTargetToProject(tree, projectConfiguration, options);
+  if (skipConfig !== true) {
+    generateCodePushupConfig(tree, projectConfiguration.root);
+  }
 
-  await formatFiles(tree);
+  if (skipTarget !== true) {
+    addTargetToProject(tree, projectConfiguration, options);
+  }
+
+  if (skipFormat !== true) {
+    await formatFiles(tree);
+  }
 }
 
 export function addTargetToProject(
@@ -29,11 +36,7 @@ export function addTargetToProject(
   options: ConfigurationGeneratorOptions,
 ) {
   const { targets } = projectConfiguration;
-  const { targetName, project, skipTarget } = options;
-
-  if (skipTarget) {
-    return;
-  }
+  const { targetName, project } = options;
 
   const codePushupTargetConfig = {
     executor: '@code-pushup/nx-plugin:autorun',
@@ -46,25 +49,6 @@ export function addTargetToProject(
       [targetName ?? DEFAULT_TARGET_NAME]: codePushupTargetConfig,
     },
   });
-}
-
-export function generateCodePushupConfig(
-  tree: Tree,
-  projectConfiguration: ProjectConfiguration,
-  options: ConfigurationGeneratorOptions,
-) {
-  const { root } = projectConfiguration;
-  const supportedFormats = ['ts', 'mjs', 'js'];
-  const firstExistingFormat = supportedFormats.find(ext =>
-    tree.exists(join(root, `code-pushup.config.${ext}`)),
-  );
-  if (firstExistingFormat) {
-    console.warn(
-      `NOTE: No config file created as code-pushup.config.${firstExistingFormat} file already exists.`,
-    );
-  } else {
-    generateFiles(tree, join(__dirname, 'files'), root, options);
-  }
 }
 
 export default configurationGenerator;
