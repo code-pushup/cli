ed63b7177818177611f58e73786c843585be2d91
Michael <michael.hladky@push-based.io>
2024-08-07 01:28:46 +0200

    format

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -9,8 +9,8 @@
 import devkit from '@nx/devkit';
 import { execSync } from 'node:child_process';
 import { readFileSync, writeFileSync } from 'node:fs';
-import yargs from "yargs";
-import {hideBin} from "yargs/helpers";
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 const { readCachedProjectGraph } = devkit;
 
@@ -26,18 +26,23 @@ const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
 
 // Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
 // Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const {name, ver: version, tag} = yargs(hideBin(process.argv)).options({
-  name: { type: 'string', demandOption: true },
-  ver: { type: 'string', demandOption: true },
-  tag: { type: 'string', default: 'next' },
-}).coerce('ver', (ver) => {
-  invariant(
-    ver && validVersion.test(ver),
-    `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${ver}.`,
-  );
-  return ver;
-}).argv;
-
+const {
+  name,
+  ver: version,
+  tag,
+} = yargs(hideBin(process.argv))
+  .options({
+    name: { type: 'string', demandOption: true },
+    ver: { type: 'string', demandOption: true },
+    tag: { type: 'string', default: 'next' },
+  })
+  .coerce('ver', ver => {
+    invariant(
+      ver && validVersion.test(ver),
+      `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${ver}.`,
+    );
+    return ver;
+  }).argv;
 
 const graph = readCachedProjectGraph();
 const project = graph.nodes[name];
