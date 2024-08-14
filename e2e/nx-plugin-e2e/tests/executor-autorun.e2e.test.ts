b6993caedc9aaa57085c0d9d31d3ab18ccf4f10f
Michael <michael.hladky@push-based.io>
2024-08-14 15:22:40 +0200

    refactor tests

--- e2e/nx-plugin-e2e/tests/executor-autorun.e2e.test.ts
+++ e2e/nx-plugin-e2e/tests/executor-autorun.e2e.test.ts
@@ -7,14 +7,53 @@ import { generateCodePushupConfig } from '@code-pushup/nx-plugin';
 import {
   generateWorkspaceAndProject,
   materializeTree,
-  registerPluginInWorkspace,
 } from '@code-pushup/test-nx-utils';
 import { removeColorCodes } from '@code-pushup/test-utils';
 import { executeProcess, readJsonFile } from '@code-pushup/utils';
 
 // @TODO replace with default bin after https://github.com/code-pushup/cli/issues/643
-export function relativePathToCwd(testDir: string): string {
+function relativePathToCwd(testDir: string): string {
   return relative(join(process.cwd(), testDir), process.cwd());
+}
+
+async function addTargetToWorkspace(
+  tree: Tree,
+  options: { cwd: string; project: string },
+) {
+  const { cwd, project } = options;
+  const pathRelativeToPackage = relative(join(cwd, 'libs', project), cwd);
+  const projectCfg = readProjectConfiguration(tree, project);
+  updateProjectConfiguration(tree, project, {
+    ...projectCfg,
+    targets: {
+      ...projectCfg.targets,
+      ['code-pushup']: {
+        executor: `${join(
+          relativePathToCwd(cwd),
+          'dist/packages/nx-plugin',
+        )}:autorun`,
+      },
+    },
+  });
+  const { root } = projectCfg;
+  generateCodePushupConfig(tree, root, {
+    fileImports: `import type {CoreConfig} from "${join(
+      relativePathToCwd(cwd),
+      pathRelativeToPackage,
+      'dist/packages/models',
+    )}";`,
+    plugins: [
+      {
+        fileImports: `import {customPlugin} from "${join(
+          relativePathToCwd(cwd),
+          pathRelativeToPackage,
+          'dist/testing/test-utils',
+        )}";`,
+        codeStrings: 'customPlugin()',
+      },
+    ],
+  });
+  await materializeTree(tree, cwd);
 }
 
 describe('executor autorun', () => {
@@ -32,43 +71,7 @@ describe('executor autorun', () => {
 
   it('should execute autorun executor', async () => {
     const cwd = join(baseDir, 'execute-dynamic-executor');
-    const pathRelativeToPackage = relative(join(cwd, 'libs', project), cwd);
-    registerPluginInWorkspace(tree, {
-      plugin: join(relativePathToCwd(cwd), 'dist/packages/nx-plugin'),
-      options: {
-        bin: join(relativePathToCwd(cwd), 'dist/packages/nx-plugin'),
-      },
-    });
-    const projectCfg = readProjectConfiguration(tree, project);
-    updateProjectConfiguration(tree, project, {
-      ...projectCfg,
-      targets: {
-        ...projectCfg.targets,
-        ['code-pushup']: {
-          executor: '@code-pushup/nx-plugin:autorun',
-        },
-      },
-    });
-    const { root } = readProjectConfiguration(tree, project);
-    generateCodePushupConfig(tree, root, {
-      fileImports: `import type {CoreConfig} from "${join(
-        relativePathToCwd(cwd),
-        pathRelativeToPackage,
-        'dist/packages/models',
-      )}";`,
-      plugins: [
-        {
-          fileImports: `import {customPlugin} from "${join(
-            relativePathToCwd(cwd),
-            pathRelativeToPackage,
-            'dist/testing/test-utils',
-          )}";`,
-          codeStrings: 'customPlugin()',
-        },
-      ],
-    });
-
-    await materializeTree(tree, cwd);
+    await addTargetToWorkspace(tree, { cwd, project });
 
     const { stdout } = await executeProcess({
       command: 'npx',
