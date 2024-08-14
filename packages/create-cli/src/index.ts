b1f9b90308377b8a81a99bbb31ae324f879d2713
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-08-14 18:02:51 +0200

    Update packages/create-cli/src/index.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/create-cli/src/index.ts
+++ packages/create-cli/src/index.ts
@@ -1,5 +1,4 @@
 #! /usr/bin/env node
 import { initCodePushup } from './lib/init';
 
-// eslint-disable-next-line unicorn/prefer-top-level-await
-initCodePushup().catch(console.error);
+await initCodePushup();
