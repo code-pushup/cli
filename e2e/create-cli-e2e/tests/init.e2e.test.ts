26a1201ccc712f262495e041efb0e52de9ed20c7
Michael <michael.hladky@push-based.io>
2024-08-18 22:46:49 +0200

    CI stability - create-cli, exec

--- e2e/create-cli-e2e/tests/init.e2e.test.ts
+++ e2e/create-cli-e2e/tests/init.e2e.test.ts
@@ -32,8 +32,8 @@ describe('create-cli-node', () => {
     );
   });
 
-  // eslint-disable-next-line vitest/no-disabled-tests
-  it.skip('should execute package correctly over npm exec', async () => {
+
+  it('should execute package correctly over npm exec', async () => {
     const cwd = join(baseDir, 'npm-exec');
     await createNpmWorkspace(cwd);
     const { code, stdout } = await executeProcess({
@@ -50,8 +50,8 @@ describe('create-cli-node', () => {
     );
   });
 
-
-  it('should execute package correctly over npm init', async () => {
+// eslint-disable-next-line vitest/no-disabled-tests
+  it.skip('should execute package correctly over npm init', async () => {
     const cwd = join(baseDir, 'npm-init');
     await createNpmWorkspace(cwd);
     const { code, stdout } = await executeProcess({
