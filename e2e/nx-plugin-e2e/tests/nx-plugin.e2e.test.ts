74faf75fde465235df5d1342bddf6507fbe096bc
Michael <michael.hladky@push-based.io>
2024-08-18 21:54:56 +0200

    CI stability - nx-plugin

--- e2e/nx-plugin-e2e/tests/nx-plugin.e2e.test.ts
+++ e2e/nx-plugin-e2e/tests/nx-plugin.e2e.test.ts
@@ -224,7 +224,7 @@ describe('nx-plugin', () => {
     registerPluginInWorkspace(tree, {
       plugin: join(relativePathToCwd(cwd), 'dist/packages/nx-plugin'),
       options: {
-        bin: 'WXYZ',
+        bin: 'XYZ',
       },
     });
     const { root } = readProjectConfiguration(tree, project);
@@ -237,7 +237,7 @@ describe('nx-plugin', () => {
 
     expect(projectJson.targets).toStrictEqual({
       ['code-pushup']: expect.objectContaining({
-        executor: 'WXYZ:autorun',
+        executor: 'XYZ:autorun',
       }),
     });
   });
