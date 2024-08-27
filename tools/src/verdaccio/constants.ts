e56f44e715f3e4328a670b6fa20ca05af6d8e4f6
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2024-08-27 18:50:16 +0200

    Update tools/src/verdaccio/constants.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- tools/src/verdaccio/constants.ts
+++ tools/src/verdaccio/constants.ts
@@ -4,5 +4,5 @@ import 'verdaccio';
 export const START_VERDACCIO_SERVER_TARGET_NAME = 'start-verdaccio-server';
 export const START_VERDACCIO_ENV_TARGET_NAME = 'start-verdaccio-env';
 export const STOP_VERDACCIO_TARGET_NAME = 'stop-verdaccio';
-export const DEFAULT_VERDACCIO_STORAGE = join('tmp', 'verdaccio/storage');
-export const DEFAULT_VERDACCIO_CONFIG = '.verdaccio/config.yml';
+export const DEFAULT_VERDACCIO_STORAGE = join('tmp', 'verdaccio', 'storage');
+export const DEFAULT_VERDACCIO_CONFIG = join('.verdaccio', 'config.yml)';
