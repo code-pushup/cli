49d0042013e5bf335638c1fdebedd535fc1eb4d7
Michael <michael.hladky@push-based.io>
2024-08-29 01:36:27 +0200

    add comment

--- e2e/plugin-lighthouse-e2e/tests/collect.e2e.test.ts
+++ e2e/plugin-lighthouse-e2e/tests/collect.e2e.test.ts
@@ -1,26 +1,33 @@
-import {join} from 'node:path';
-import {createTreeWithEmptyWorkspace} from 'nx/src/generators/testing-utils/create-tree-with-empty-workspace';
-import {afterEach, expect} from 'vitest';
-import {type Report, reportSchema} from '@code-pushup/models';
-import {generateCodePushupConfig} from '@code-pushup/nx-plugin';
-import {materializeTree} from '@code-pushup/test-nx-utils';
-import {teardownTestFolder} from '@code-pushup/test-setup';
-import {createNpmWorkspace, omitVariableReportData, removeColorCodes,} from '@code-pushup/test-utils';
-import {executeProcess, readJsonFile} from '@code-pushup/utils';
+import { join } from 'node:path';
+import {
+  createTreeWithEmptyV1Workspace,
+  createTreeWithEmptyWorkspace,
+} from 'nx/src/generators/testing-utils/create-tree-with-empty-workspace';
+import { afterEach, expect } from 'vitest';
+import { type Report, reportSchema } from '@code-pushup/models';
+import { generateCodePushupConfig } from '@code-pushup/nx-plugin';
+import { materializeTree } from '@code-pushup/test-nx-utils';
+import { teardownTestFolder } from '@code-pushup/test-setup';
+import {
+  createNpmWorkspace,
+  omitVariableReportData,
+  removeColorCodes,
+} from '@code-pushup/test-utils';
+import { executeProcess, readJsonFile } from '@code-pushup/utils';
 
 async function setupWorkspace(cwd: string) {
-  const tree = createTreeWithEmptyWorkspace({layout: 'apps-libs'});
+  // @TODO create empty tree without workspace
+  const tree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
   generateCodePushupConfig(tree, '.', {
     fileImports: 'import type {CoreConfig} from "@code-pushup/models";',
     plugins: [
       {
         fileImports: [
           'import lighthousePlugin from "@code-pushup/lighthouse-plugin";',
-          'import { DEFAULT_FLAGS } from "chrome-launcher/dist/flags.js";'
+          'import { DEFAULT_FLAGS } from "chrome-launcher/dist/flags.js";',
         ],
         // @TODO fix ` hack, add support for string literals in generateCodePushupConfig
-        codeStrings:
-          `await lighthousePlugin(\`https://codepushup.dev/\`, {
+        codeStrings: `await lighthousePlugin(\`https://codepushup.dev/\`, {
       onlyAudits: [
         // performance category
         \`largest-contentful-paint\`,
@@ -33,7 +40,6 @@ async function setupWorkspace(cwd: string) {
       ],
       chromeFlags: DEFAULT_FLAGS.concat([\`--headless\`]),
     }),`,
-
       },
     ],
   });
@@ -51,7 +57,7 @@ describe('collect report with lighthouse-plugin NPM package', () => {
   it('should run plugin over CLI and creates report.json', async () => {
     const cwd = join(baseDir, 'create-report');
     await setupWorkspace(cwd);
-    const {code, stdout} = await executeProcess({
+    const { code, stdout } = await executeProcess({
       command: 'npx',
       args: ['@code-pushup/cli', 'collect', '--no-progress'],
       cwd,
@@ -65,7 +71,7 @@ describe('collect report with lighthouse-plugin NPM package', () => {
     const report = await readJsonFile(join(cwd, '.code-pushup', 'report.json'));
     expect(() => reportSchema.parse(report)).not.toThrow();
     expect(
-      omitVariableReportData(report as Report, {omitAuditData: true}),
+      omitVariableReportData(report as Report, { omitAuditData: true }),
     ).toMatchSnapshot();
   });
 });
