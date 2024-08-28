89639f9108710606029366d528cbdcbd4b5c1ed2
Michael <michael.hladky@push-based.io>
2024-08-28 18:37:48 +0200

    merge main

--- e2e/nx-plugin-e2e/tests/executor-cli.e2e.test.ts
+++ e2e/nx-plugin-e2e/tests/executor-cli.e2e.test.ts
@@ -9,7 +9,7 @@ import {
   materializeTree,
 } from '@code-pushup/test-nx-utils';
 import { removeColorCodes } from '@code-pushup/test-utils';
-import { executeProcess } from '@code-pushup/utils';
+import { executeProcess, readJsonFile } from '@code-pushup/utils';
 
 function relativePathToCwd(testDir: string): string {
   return relative(join(process.cwd(), testDir), process.cwd());
@@ -27,7 +27,7 @@ async function addTargetToWorkspace(
     targets: {
       ...projectCfg.targets,
       ['code-pushup']: {
-        executor: '@code-pushup/nx-plugin:autorun',
+        executor: '@code-pushup/nx-plugin:cli',
       },
     },
   });
@@ -59,13 +59,12 @@ describe('executor command', () => {
   });
 
   afterEach(async () => {
-    await rm(baseDir, { recursive: true, force: true });
+    // await rm(baseDir, { recursive: true, force: true });
   });
 
-  it('should execute command executor', async () => {
-    const cwd = join(baseDir, 'execute-dynamic-executor');
+  it('should execute no specific command by default', async () => {
+    const cwd = join(baseDir, 'execute-default-command');
     await addTargetToWorkspace(tree, { cwd, project });
-
     const { stdout, code } = await executeProcess({
       command: 'npx',
       args: ['nx', 'run', `${project}:code-pushup`, '--dryRun'],
@@ -74,6 +73,109 @@ describe('executor command', () => {
 
     expect(code).toBe(0);
     const cleanStdout = removeColorCodes(stdout);
-    expect(cleanStdout).toContain('nx run my-lib:code-pushup --dryRun');
+    expect(cleanStdout).toContain('nx run my-lib:code-pushup');
+  });
+
+  it('should execute print-config executor', async () => {
+    const cwd = join(baseDir, 'execute-print-config-command');
+    await addTargetToWorkspace(tree, { cwd, project });
+
+    const { stdout, code } = await executeProcess({
+      command: 'npx',
+      args: ['nx', 'run', `${project}:code-pushup`, 'print-config'],
+      cwd,
+    });
+
+    expect(code).toBe(0);
+    const cleanStdout = removeColorCodes(stdout);
+    expect(cleanStdout).toContain('nx run my-lib:code-pushup print-config');
+
+    await expect(() =>
+      readJsonFile(join(cwd, '.code-pushup', project, 'report.json')),
+    ).rejects.toThrow('');
+  });
+
+  it('should execute collect executor', async () => {
+    const cwd = join(baseDir, 'execute-collect-command');
+    await addTargetToWorkspace(tree, { cwd, project });
+
+    const { stdout, code } = await executeProcess({
+      command: 'npx',
+      args: ['nx', 'run', `${project}:code-pushup`, 'collect'],
+      cwd,
+    });
+
+    expect(code).toBe(0);
+    const cleanStdout = removeColorCodes(stdout);
+    expect(cleanStdout).toContain('nx run my-lib:code-pushup collect');
+
+    const report = await readJsonFile(
+      join(cwd, '.code-pushup', project, 'report.json'),
+    );
+    expect(report).toStrictEqual(
+      expect.objectContaining({
+        plugins: [
+          expect.objectContaining({
+            slug: 'good-feels',
+            audits: [
+              expect.objectContaining({
+                displayValue: '✅ Perfect! 👌',
+                slug: 'always-perfect',
+              }),
+            ],
+          }),
+        ],
+      }),
+    );
+  });
+
+  it('should execute upload executor to throw if no report is present', async () => {
+    const cwd = join(baseDir, 'execute-upload-command');
+    await addTargetToWorkspace(tree, { cwd, project });
+
+    await expect(
+      executeProcess({
+        command: 'npx',
+        args: ['nx', 'run', `${project}:code-pushup`, 'upload'],
+        cwd,
+      }),
+    ).rejects.toThrow(/report.json/);
+  });
+
+  it('should execute autorun executor', async () => {
+    const cwd = join(baseDir, 'execute-autorun-command');
+    await addTargetToWorkspace(tree, { cwd, project });
+
+    const { stdout, code } = await executeProcess({
+      command: 'npx',
+      args: ['nx', 'run', `${project}:code-pushup`, 'autorun'],
+      cwd,
+    });
+
+    expect(code).toBe(0);
+    const cleanStdout = removeColorCodes(stdout);
+    expect(cleanStdout).toContain('nx run my-lib:code-pushup autorun');
+    expect(cleanStdout).toContain(
+      '>  NX   Successfully ran target code-pushup for project my-lib',
+    );
+
+    const report = await readJsonFile(
+      join(cwd, '.code-pushup', project, 'report.json'),
+    );
+    expect(report).toStrictEqual(
+      expect.objectContaining({
+        plugins: [
+          expect.objectContaining({
+            slug: 'good-feels',
+            audits: [
+              expect.objectContaining({
+                displayValue: '✅ Perfect! 👌',
+                slug: 'always-perfect',
+              }),
+            ],
+          }),
+        ],
+      }),
+    );
   });
 });
