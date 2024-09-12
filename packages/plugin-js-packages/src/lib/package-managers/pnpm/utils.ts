fbd69680275b68abec2e5172c2b7321e365e9688
hanna-skryl <hanna.skryl@gmail.com>
2024-09-12 15:16:22 -0400

    fix(plugin-js-packages): filter out warnings

--- packages/plugin-js-packages/src/lib/package-managers/pnpm/utils.ts
+++ packages/plugin-js-packages/src/lib/package-managers/pnpm/utils.ts
@@ -1,5 +1,5 @@
 export const filterOutWarnings = (output: string): string =>
   output
     .split('\n')
-    .filter(line => !line.includes('WARN'))
+    .filter(line => !line.trim().startsWith('WARN'))
     .join('\n');
