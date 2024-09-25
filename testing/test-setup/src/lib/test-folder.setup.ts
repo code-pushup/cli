822a03a1e6b2bd4ebb30cb1208798554d79f35cf
Michael <michael.hladky@push-based.io>
2024-09-25 15:05:25 +0200

    wip

--- testing/test-setup/src/lib/test-folder.setup.ts
+++ testing/test-setup/src/lib/test-folder.setup.ts
@@ -14,7 +14,7 @@ export async function cleanTestFolder(dirName: string) {
 export async function teardownTestFolder(dirName: string) {
   try {
     // eslint-disable-next-line no-magic-numbers
-    await rm(dirName, { recursive: true, force: true, maxRetries: 3 });
+    await rm(dirName, { recursive: true, force: true, maxRetries: 2, retryDelay: 100 });
   } catch {
     logger.error(
       `⚠️ Failed to delete test artefact ${bold(
