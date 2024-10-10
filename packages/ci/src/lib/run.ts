d599887f8fc602d71f2ef77032beb4d36f5fd3eb
Matěj Chalk <matej.chalk@flowup.cz>
2024-10-10 17:01:44 +0200

    refactor(ci): refine type definitions

--- packages/ci/src/lib/run.ts
+++ packages/ci/src/lib/run.ts
@@ -1,3 +1,17 @@
-export function runInCI() {
-  // TODO
+import { DEFAULT_SETTINGS } from './constants';
+import type {
+  GitRefs,
+  Options,
+  ProviderAPIClient,
+  RunResult as RunOutput,
+  Settings,
+} from './models';
+
+export function runInCI(
+  refs: GitRefs,
+  api: ProviderAPIClient,
+  options?: Options,
+): Promise<RunOutput> {
+  const settings: Settings = { ...DEFAULT_SETTINGS, ...options };
+  // TODO: implement main flow
 }
