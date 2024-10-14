76d08da6b2def7d5cdf812641e1d1bb9d6840fff
Matěj Chalk <matej.chalk@flowup.cz>
2024-10-14 19:08:04 +0200

    docs(ci): document package and exports

--- packages/ci/src/lib/models.ts
+++ packages/ci/src/lib/models.ts
@@ -1,6 +1,10 @@
 import type { SourceFileIssue } from './issues';
 import type { MonorepoTool } from './monorepo';
 
+/**
+ * Customization options for {@link runInCI}
+ * @see https://github.com/code-pushup/cli/tree/main/packages/ci#options
+ */
 export type Options = {
   monorepo?: boolean | MonorepoTool;
   projects?: string[] | null;
@@ -14,32 +18,51 @@ export type Options = {
   logger?: Logger;
 };
 
+/**
+ * {@link Options} with filled-in defaults.
+ */
 export type Settings = Required<Options>;
 
+/**
+ * Branches/commits for {@link runInCI}
+ */
 export type GitRefs = {
   head: GitBranch;
   base?: GitBranch;
 };
 
+/**
+ * API client for {@link runInCI} - adapter for CI provider
+ * @see https://github.com/code-pushup/cli/tree/main/packages/ci#provider-api-client
+ */
 export type ProviderAPIClient = {
   maxCommentChars: number;
-  downloadReportArtifact?: () => Promise<string>;
+  downloadReportArtifact?: () => Promise<string | null>;
   listComments: () => Promise<Comment[]>;
   updateComment: (id: number, body: string) => Promise<Comment>;
   createComment: (body: string) => Promise<Comment>;
 };
 
+/**
+ * PR comment from {@link ProviderAPIClient}
+ */
 export type Comment = {
   id: number;
   body: string;
   url: string;
 };
 
+/**
+ * Git ref (e.g. 'main') and commit sha (e.g. '9d7568265bd5b17d1bb51a6eb6407e0d387058d2')
+ */
 export type GitBranch = {
   ref: string;
   sha: string;
 };
 
+/**
+ * Logger instance (e.g. `console`) for reporting progress and problems
+ */
 export type Logger = {
   error: (message: string) => void;
   warn: (message: string) => void;
@@ -47,13 +70,22 @@ export type Logger = {
   debug: (message: string) => void;
 };
 
+/**
+ * Resolved return value of {@link runInCI}
+ */
 export type RunResult = StandaloneRunResult | MonorepoRunResult;
 
+/**
+ * Resolved return value of {@link runInCI} in standalone mode
+ */
 export type StandaloneRunResult = Omit<ProjectRunResult, 'name'> & {
   mode: 'standalone';
   commentId?: number;
 };
 
+/**
+ * Resolved return value of {@link runInCI} in monorepo mode
+ */
 export type MonorepoRunResult = {
   mode: 'monorepo';
   projects: ProjectRunResult[];
@@ -61,6 +93,9 @@ export type MonorepoRunResult = {
   diffArtifact?: ArtifactData;
 };
 
+/**
+ * Result of {@link runInCI} for a single project
+ */
 export type ProjectRunResult = {
   name: string;
   artifacts: {
@@ -70,6 +105,9 @@ export type ProjectRunResult = {
   newIssues?: SourceFileIssue[];
 };
 
+/**
+ * Paths to output files from {@link runInCI}, for uploading as job artifact
+ */
 export type ArtifactData = {
   rootDir: string;
   files: string[];
