8cc75ee47c71f08c7ca173c271b5a6dea1f6d35c
Michael <michael.hladky@push-based.io>
2024-11-09 03:34:40 +0100

    test: add eslint e2e project

---
+++ e2e/plugin-eslint-e2e/tests/collect.e2e.test.ts
@@ -0,0 +1,35 @@
+import { join } from 'node:path';
+import { afterEach } from 'vitest';
+import { type Report, reportSchema } from '@code-pushup/models';
+import { teardownTestFolder } from '@code-pushup/test-setup';
+import { omitVariableReportData } from '@code-pushup/test-utils';
+import { executeProcess, readJsonFile } from '@code-pushup/utils';
+
+describe('collect report with eslint-plugin NPM package', () => {
+  const baseDir = 'eslint-e2e-env/__test__';
+
+  afterEach(async () => {
+    await teardownTestFolder(baseDir);
+  });
+
+  it('should run ESLint plugin and create report.json', async () => {
+    const { code, stderr } = await executeProcess({
+      command: 'npx',
+      args: [
+        '@code-pushup/cli',
+        'collect',
+        '--no-progress',
+        '--onlyPlugins=eslint',
+      ],
+      cwd: baseDir,
+    });
+
+    expect(code).toBe(0);
+    expect(stderr).toBe('');
+
+    const report = await readJsonFile(join(baseDir, 'code-pushup/report.json'));
+
+    expect(() => reportSchema.parse(report)).not.toThrow();
+    expect(omitVariableReportData(report as Report)).toMatchSnapshot();
+  });
+});
