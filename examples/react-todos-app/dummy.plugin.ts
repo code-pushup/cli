de6d0ec6fbf12358a1de586bd7098e7191860197
Michael <michael.hladky@push-based.io>
2024-11-10 14:06:19 +0100

    wip

--- examples/react-todos-app/dummy.plugin.ts
+++ examples/react-todos-app/dummy.plugin.ts
@@ -1,32 +1,27 @@
-import type {PluginConfig,} from '@code-pushup/models';
+import type { PluginConfig } from '@code-pushup/models';
 
-export type PluginOptions = {
-  directory: string;
-  pattern?: string | RegExp;
-  budget?: number;
-};
-
-export const pluginSlug = 'dummy-plugin';
+export const dummyPluginSlug = 'dummy-plugin';
 
 const dummyAuditSlug = 'dummy-audit';
 export const dummyAudit = {
-    slug: dummyAuditSlug,
-    title: 'Dummy Audit',
-    description:
-      'A dummy audit to test the cli.',
+  slug: dummyAuditSlug,
+  title: 'Dummy Audit',
+  description: 'A dummy audit to test the cli.',
 };
 export function create(): PluginConfig {
   return {
-    slug: pluginSlug,
+    slug: dummyPluginSlug,
     title: 'Dummy Plugin',
     icon: 'folder-javascript',
     description: 'A dummy plugin to test the cli.',
-    runner: () => ([{
-      ...dummyAudit,
-      slug: dummyAuditSlug,
-      score: 1,
-      value: 0
-    }]),
+    runner: () => [
+      {
+        ...dummyAudit,
+        slug: dummyAuditSlug,
+        score: 1,
+        value: 0,
+      },
+    ],
     audits: [dummyAudit],
   };
 }
