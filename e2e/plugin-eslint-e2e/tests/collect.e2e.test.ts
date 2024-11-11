8d8ecc4abb5ebb05f46211731ba9f4b18c65029f
Michael <michael.hladky@push-based.io>
2024-11-11 19:16:27 +0100

    wip

--- e2e/plugin-eslint-e2e/tests/collect.e2e.test.ts
+++ e2e/plugin-eslint-e2e/tests/collect.e2e.test.ts
@@ -1,4 +1,4 @@
-import { join, relative } from 'node:path';
+import { join } from 'node:path';
 import { afterEach } from 'vitest';
 import { type Report, reportSchema } from '@code-pushup/models';
 import { teardownTestFolder } from '@code-pushup/test-setup';
@@ -7,20 +7,18 @@ import { executeProcess, readJsonFile } from '@code-pushup/utils';
 
 describe('collect report with eslint-plugin NPM package', () => {
   const envRoot = 'e2e/plugin-eslint-e2e/__test-env__';
-  const baseDir = join(envRoot);
+  const outputDir = join(envRoot, '.code-pushup');
 
   afterEach(async () => {
-    await teardownTestFolder(baseDir);
+    await teardownTestFolder(outputDir);
   });
 
   it('should run ESLint plugin and create report.json', async () => {
-    const outputDir = relative(envRoot, join(baseDir, '.code-pushup'));
     const { code, stderr } = await executeProcess({
       command: 'npx',
       args: [
         '@code-pushup/cli',
         'collect',
-        `--persist.outputDir=${outputDir}`,
         '--no-progress',
         '--onlyPlugins=eslint',
       ],
@@ -30,7 +28,7 @@ describe('collect report with eslint-plugin NPM package', () => {
     expect(code).toBe(0);
     expect(stderr).toBe('');
 
-    const report = await readJsonFile(join(envRoot, outputDir, 'report.json'));
+    const report = await readJsonFile(join(outputDir, 'report.json'));
 
     expect(() => reportSchema.parse(report)).not.toThrow();
     expect(omitVariableReportData(report as Report)).toMatchSnapshot();
