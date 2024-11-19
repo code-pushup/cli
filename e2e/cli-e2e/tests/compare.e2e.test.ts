e27d4b6bee8bd381a3189c3ade07e12c1edb9c71
Michael <michael.hladky@push-based.io>
2024-11-19 16:03:34 +0100

    cleanup env files

--- e2e/cli-e2e/tests/compare.e2e.test.ts
+++ e2e/cli-e2e/tests/compare.e2e.test.ts
@@ -1,6 +1,6 @@
-import { cp, readFile, writeFile } from 'node:fs/promises';
+import { cp } from 'node:fs/promises';
 import { join } from 'node:path';
-import { afterEach, beforeAll } from 'vitest';
+import { beforeAll } from 'vitest';
 import type { ReportsDiff } from '@code-pushup/models';
 import { teardownTestFolder } from '@code-pushup/test-setup';
 import { executeProcess, readJsonFile, readTextFile } from '@code-pushup/utils';
@@ -11,45 +11,20 @@ describe('CLI compare', () => {
     'cli-e2e',
     'mocks',
     'fixtures',
-    'dummy-setup',
+    'existing-reports',
   );
 
   const envRoot = join('tmp', 'e2e', 'cli-e2e');
-  const testFileDir = join(envRoot, 'compare');
-  const dummyDir = join(testFileDir, 'dummy-setup');
-  const dummyOutputDir = join(dummyDir, '.code-pushup');
+  const testFileDir = join(envRoot, '__test__', 'compare');
+  const existingDir = join(testFileDir, 'existing-reports');
+  const existingOutputDir = join(existingDir, '.code-pushup');
 
   beforeAll(async () => {
-    await cp(fixtureDummyDir, dummyDir, { recursive: true });
+    await cp(fixtureDummyDir, existingDir, { recursive: true });
   });
 
   afterAll(async () => {
-    await teardownTestFolder(dummyDir);
-  });
-
-  beforeEach(async () => {
-    // create report before
-    await executeProcess({
-      command: 'npx',
-      args: ['@code-pushup/cli', 'collect', '--persist.filename=source-report'],
-      cwd: dummyDir,
-    });
-
-    // adding items to create a report diff
-    const itemsFile = join(dummyDir, 'src', 'items.json');
-    const items = JSON.parse((await readFile(itemsFile)).toString());
-    await writeFile(itemsFile, JSON.stringify([...items, 4, 5, 6, 7], null, 2));
-
-    await executeProcess({
-      command: 'npx',
-      args: ['@code-pushup/cli', 'collect', '--persist.filename=target-report'],
-      cwd: dummyDir,
-    });
-  }, 20_000);
-
-  // create report after
-  afterEach(async () => {
-    await teardownTestFolder(dummyOutputDir);
+    await teardownTestFolder(existingDir);
   });
 
   it('should compare report.json files and create report-diff.json and report-diff.md', async () => {
@@ -61,11 +36,11 @@ describe('CLI compare', () => {
         `--before=${join('.code-pushup', 'source-report.json')}`,
         `--after=${join('.code-pushup', 'target-report.json')}`,
       ],
-      cwd: dummyDir,
+      cwd: existingDir,
     });
 
     const reportsDiff = await readJsonFile<ReportsDiff>(
-      join(dummyOutputDir, 'report-diff.json'),
+      join(existingOutputDir, 'report-diff.json'),
     );
     expect(reportsDiff).toMatchSnapshot({
       commits: expect.any(Object),
@@ -75,7 +50,7 @@ describe('CLI compare', () => {
     });
 
     const reportsDiffMd = await readTextFile(
-      join(dummyOutputDir, 'report-diff.md'),
+      join(existingOutputDir, 'report-diff.md'),
     );
     // commits are variable, replace SHAs with placeholders
     const sanitizedMd = reportsDiffMd.replace(/[\da-f]{40}/g, '`<commit-sha>`');
