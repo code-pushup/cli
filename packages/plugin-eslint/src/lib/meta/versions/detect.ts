7782cf6496116b2da65e799acf242be9444ac6f4
Matěj Chalk <matej.chalk@flowup.cz>
2024-11-20 17:48:22 +0100

    feat(plugin-eslint): detect version of config format

--- packages/plugin-eslint/src/lib/meta/versions/detect.ts
+++ packages/plugin-eslint/src/lib/meta/versions/detect.ts
@@ -1,6 +1,24 @@
+import { ESLint } from 'eslint';
+import { fileExists } from '@code-pushup/utils';
 import type { ConfigFormat } from './formats';
 
+// relevant ESLint docs:
+// - https://eslint.org/docs/latest/use/configure/configuration-files
+// - https://eslint.org/docs/latest/use/configure/configuration-files-deprecated
+// - https://eslint.org/docs/v8.x/use/configure/configuration-files-new
+
 export async function detectConfigVersion(): Promise<ConfigFormat> {
-  // TODO: detect flat config
-  return 'legacy';
+  if (process.env['ESLINT_USE_FLAT_CONFIG'] === 'true') {
+    return 'flat';
+  }
+  if (process.env['ESLINT_USE_FLAT_CONFIG'] === 'false') {
+    return 'legacy';
+  }
+  if (ESLint.version.startsWith('8.')) {
+    if (await fileExists('eslint.config.js')) {
+      return 'flat';
+    }
+    return 'legacy';
+  }
+  return 'flat';
 }
