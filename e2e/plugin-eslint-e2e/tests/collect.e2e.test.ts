face9c18d5ac04e0bd421804755f9db96c90550f
Matěj Chalk <matej.chalk@flowup.cz>
2024-11-22 10:55:03 +0100

    test(plugin-eslint-e2e): add e2e test for legacy config

--- e2e/plugin-eslint-e2e/tests/collect.e2e.test.ts
+++ e2e/plugin-eslint-e2e/tests/collect.e2e.test.ts
@@ -7,30 +7,32 @@ import { omitVariableReportData } from '@code-pushup/test-utils';
 import { executeProcess, readJsonFile } from '@code-pushup/utils';
 
 describe('collect report with eslint-plugin NPM package', () => {
-  const fixturesFlatConfigDir = join(
-    'e2e',
-    'plugin-eslint-e2e',
-    'mocks',
-    'fixtures',
-    'flat-config',
-  );
+  const fixturesDir = join('e2e', 'plugin-eslint-e2e', 'mocks', 'fixtures');
+  const fixturesFlatConfigDir = join(fixturesDir, 'flat-config');
+  const fixturesLegacyConfigDir = join(fixturesDir, 'legacy-config');
+
   const envRoot = join('tmp', 'e2e', 'plugin-eslint-e2e');
   const flatConfigDir = join(envRoot, 'flat-config');
+  const legacyConfigDir = join(envRoot, 'legacy-config');
   const flatConfigOutputDir = join(flatConfigDir, '.code-pushup');
+  const legacyConfigOutputDir = join(legacyConfigDir, '.code-pushup');
 
   beforeAll(async () => {
     await cp(fixturesFlatConfigDir, flatConfigDir, { recursive: true });
+    await cp(fixturesLegacyConfigDir, legacyConfigDir, { recursive: true });
   });
 
   afterAll(async () => {
     await teardownTestFolder(flatConfigDir);
+    await teardownTestFolder(legacyConfigDir);
   });
 
   afterEach(async () => {
     await teardownTestFolder(flatConfigOutputDir);
+    await teardownTestFolder(legacyConfigOutputDir);
   });
 
-  it('should run ESLint plugin and create report.json', async () => {
+  it('should run ESLint plugin for flat config and create report.json', async () => {
     const { code, stderr } = await executeProcess({
       command: 'npx',
       args: ['@code-pushup/cli', 'collect', '--no-progress'],
@@ -41,6 +43,25 @@ describe('collect report with eslint-plugin NPM package', () => {
     expect(stderr).toBe('');
 
     const report = await readJsonFile(join(flatConfigOutputDir, 'report.json'));
+
+    expect(() => reportSchema.parse(report)).not.toThrow();
+    expect(omitVariableReportData(report as Report)).toMatchSnapshot();
+  });
+
+  it('should run ESLint plugin for legacy config and create report.json', async () => {
+    const { code, stderr } = await executeProcess({
+      command: 'npx',
+      args: ['@code-pushup/cli', 'collect', '--no-progress'],
+      cwd: legacyConfigDir,
+      env: { ...process.env, ESLINT_USE_FLAT_CONFIG: 'false' },
+    });
+
+    expect(code).toBe(0);
+    expect(stderr).toBe('');
+
+    const report = await readJsonFile(
+      join(legacyConfigOutputDir, 'report.json'),
+    );
 
     expect(() => reportSchema.parse(report)).not.toThrow();
     expect(omitVariableReportData(report as Report)).toMatchSnapshot();
