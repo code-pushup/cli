e7090ebaeb1a5fe3a113f8cb6ab4f46159d5402d
Matěj Chalk <matej.chalk@flowup.cz>
2024-12-04 17:07:15 +0100

    feat(ci): add parallel option

--- packages/ci/src/lib/constants.ts
+++ packages/ci/src/lib/constants.ts
@@ -2,6 +2,7 @@ import type { Settings } from './models.js';
 
 export const DEFAULT_SETTINGS: Settings = {
   monorepo: false,
+  parallel: false, // TODO: default to true once battle-tested?
   projects: null,
   task: 'code-pushup',
   bin: 'npx --no-install code-pushup',
