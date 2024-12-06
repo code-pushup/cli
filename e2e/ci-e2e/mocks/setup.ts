29205f059d348b24cc466c62eb65dd0634310daa
Matěj Chalk <matej.chalk@flowup.cz>
2024-12-06 17:27:22 +0100

    test(ci): prepare integration test setup to reuse for monorepo mode

--- e2e/ci-e2e/mocks/setup.ts
+++ e2e/ci-e2e/mocks/setup.ts
@@ -1,18 +1,14 @@
 import { cp } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import {
-  type FetchResult,
-  type Response,
-  type SimpleGit,
-  simpleGit,
-} from 'simple-git';
+import { simpleGit } from 'simple-git';
 import { nxTargetProject } from '@code-pushup/test-nx-utils';
 import { teardownTestFolder } from '@code-pushup/test-setup';
 import {
   E2E_ENVIRONMENTS_DIR,
   TEST_OUTPUT_DIR,
   initGitRepo,
+  simulateGitFetch,
 } from '@code-pushup/test-utils';
 
 export type TestRepo = Awaited<ReturnType<typeof setupTestRepo>>;
@@ -44,30 +40,4 @@ export async function setupTestRepo(folder: string) {
     baseDir,
     cleanup: () => teardownTestFolder(baseDir),
   };
-}
-
-// git fetch and FETCH_HEAD must be simulated because of missing remote
-async function simulateGitFetch(git: SimpleGit) {
-  let fetchHead: string = await git.branchLocal().then(resp => resp.current);
-
-  vi.spyOn(git, 'fetch').mockImplementation((...args) => {
-    fetchHead = (args as unknown as [string, string, string[]])[1];
-    return Promise.resolve({}) as Response<FetchResult>;
-  });
-
-  const originalDiffSummary = git.diffSummary.bind(git);
-  const originalDiff = git.diff.bind(git);
-
-  vi.spyOn(git, 'diffSummary').mockImplementation(args =>
-    originalDiffSummary(
-      (args as unknown as string[]).map(arg =>
-        arg === 'FETCH_HEAD' ? fetchHead : arg,
-      ),
-    ),
-  );
-  vi.spyOn(git, 'diff').mockImplementation(args =>
-    originalDiff(
-      (args as string[]).map(arg => (arg === 'FETCH_HEAD' ? fetchHead : arg)),
-    ),
-  );
 }
