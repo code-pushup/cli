3638ba67f2dc7f0b08726698192f13c0cc7b6bc7
Matěj Chalk <matej.chalk@flowup.cz>
2024-12-11 16:37:55 +0100

    feat(utils): implement type guard for nullable object props

--- packages/utils/src/lib/guards.unit.test.ts
+++ packages/utils/src/lib/guards.unit.test.ts
@@ -1,5 +1,9 @@
 import { describe } from 'vitest';
-import { isPromiseFulfilledResult, isPromiseRejectedResult } from './guards.js';
+import {
+  hasNoNullableProps,
+  isPromiseFulfilledResult,
+  isPromiseRejectedResult,
+} from './guards.js';
 
 describe('promise-result', () => {
   it('should get fulfilled result', () => {
@@ -18,5 +22,23 @@ describe('promise-result', () => {
     } as PromiseSettledResult<unknown>;
     expect(isPromiseFulfilledResult(result)).toBe(false);
     expect(isPromiseRejectedResult(result)).toBe(true);
+  });
+});
+
+describe('hasNoNullableProps', () => {
+  it('should return true if object prop values are neither null nor undefined', () => {
+    expect(hasNoNullableProps({ a: 42, b: 'foo', c: {}, d: [] })).toBe(true);
+  });
+
+  it('should return false if some prop is null', () => {
+    expect(hasNoNullableProps({ x: 42, y: null })).toBe(false);
+  });
+
+  it('should return false if some prop is set to undefined', () => {
+    expect(hasNoNullableProps({ x: undefined })).toBe(false);
+  });
+
+  it('should return true for empty object', () => {
+    expect(hasNoNullableProps({})).toBe(true);
   });
 });
