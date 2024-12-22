78d6c6c46c43d2a90968c3af19665ac906365849
Michael <michael.hladky@push-based.io>
2024-12-22 21:12:15 +0100

    wip

--- testing/test-setup/src/vitest.d.ts
+++ testing/test-setup/src/vitest.d.ts
@@ -5,6 +5,8 @@ import type {
 } from './lib/extend/path.matcher.js';
 
 declare module 'vitest' {
+  // eslint-disable @typescript-eslint/no-empty-object-type
   interface Assertion extends CustomPathMatchers {}
+  // eslint-disable @typescript-eslint/no-empty-object-type
   interface AsymmetricMatchersContaining extends CustomAsymmetricPathMatchers {}
 }
