b896dab5f9c0cfc2bd194f804dc912bdadae686e
Michael <michael.hladky@push-based.io>
2024-12-22 21:04:40 +0100

    wip

--- testing/test-setup/src/lib/extend/path.matcher.ts
+++ testing/test-setup/src/lib/extend/path.matcher.ts
@@ -19,165 +19,104 @@ export type CustomAsymmetricPathMatchers = {
 };
 
 expect.extend({
-  toMatchPath(actual: string, expected: string): SyncExpectationResult {
-    const normalizedReceived = osAgnosticPath(actual);
-    const normalizedExpected = osAgnosticPath(expected);
-
-    const pass = normalizedReceived === normalizedExpected;
-    return pass
-      ? {
-          message: () => `expected ${actual} not to match path ${expected}`,
-          pass: true,
-          actual,
-          expected,
-        }
-      : {
-          message: () => `expected ${actual} to match path ${expected}`,
-          pass: false,
-          actual,
-          expected,
-        };
-  },
-
-  pathToMatch(actual: string, expected: string): SyncExpectationResult {
-    const normalizedReceived = osAgnosticPath(actual);
-    const normalizedExpected = osAgnosticPath(expected);
-
-    const pass = normalizedReceived === normalizedExpected;
-    return pass
-      ? {
-          message: () => `expected ${actual} not to match path ${expected}`,
-          pass: true,
-          actual,
-          expected,
-        }
-      : {
-          message: () => `expected ${actual} to match path ${expected}`,
-          pass: false,
-          actual,
-          expected,
-        };
-  },
-
-  toStartWithPath(actual: string, expected: string): SyncExpectationResult {
-    const normalizedReceived = osAgnosticPath(actual);
-    const normalizedExpected = osAgnosticPath(expected);
-
-    const pass = normalizedReceived.startsWith(normalizedExpected);
-    return pass
-      ? {
-          message: () =>
-            `expected ${actual} not to start with path ${expected}`,
-          pass: true,
-          actual,
-          expected,
-        }
-      : {
-          message: () => `expected ${actual} to start with path ${expected}`,
-          pass: false,
-          actual,
-          expected,
-        };
-  },
-
-  pathToStartWith(actual: string, expected: string): SyncExpectationResult {
-    const normalizedReceived = osAgnosticPath(actual);
-    const normalizedExpected = osAgnosticPath(expected);
-
-    const pass = normalizedReceived.startsWith(normalizedExpected);
-    return pass
-      ? {
-          message: () =>
-            `expected ${actual} not to start with path ${expected}`,
-          pass: true,
-          actual,
-          expected,
-        }
-      : {
-          message: () => `expected ${actual} to start with path ${expected}`,
-          pass: false,
-          actual,
-          expected,
-        };
-  },
-
-  toContainPath(actual: string, expected: string): SyncExpectationResult {
-    const normalizedReceived = osAgnosticPath(actual);
-    const normalizedExpected = osAgnosticPath(expected);
-
-    const pass = normalizedReceived.includes(normalizedExpected);
-    return pass
-      ? {
-          message: () => `expected ${actual} not to contain path ${expected}`,
-          pass: true,
-          actual,
-          expected,
-        }
-      : {
-          message: () => `expected ${actual} to contain path ${expected}`,
-          pass: false,
-          actual,
-          expected,
-        };
-  },
-
-  pathToContain(actual: string, expected: string): SyncExpectationResult {
-    const normalizedReceived = osAgnosticPath(actual);
-    const normalizedExpected = osAgnosticPath(expected);
-
-    const pass = normalizedReceived.includes(normalizedExpected);
-    return pass
-      ? {
-          message: () => `expected ${actual} not to contain path ${expected}`,
-          pass: true,
-          actual,
-          expected,
-        }
-      : {
-          message: () => `expected ${actual} to contain path ${expected}`,
-          pass: false,
-          actual,
-          expected,
-        };
-  },
-
-  toEndWithPath(actual: string, expected: string): SyncExpectationResult {
-    const normalizedReceived = osAgnosticPath(actual);
-    const normalizedExpected = osAgnosticPath(expected);
-
-    const pass = normalizedReceived.endsWith(normalizedExpected);
-    return pass
-      ? {
-          message: () => `expected ${actual} not to end with path ${expected}`,
-          pass: true,
-          actual,
-          expected,
-        }
-      : {
-          message: () => `expected ${actual} to end with path ${expected}`,
-          pass: false,
-          actual,
-          expected,
-        };
-  },
-
-  pathToEndWith(actual: string, expected: string): SyncExpectationResult {
-    const normalizedReceived = osAgnosticPath(actual);
-    const normalizedExpected = osAgnosticPath(expected);
-
-    const pass = normalizedReceived.endsWith(normalizedExpected);
-    return pass
-      ? {
-          message: () => `expected ${actual} not to end with path ${expected}`,
-          pass: true,
-          actual,
-          expected,
-        }
-      : {
-          message: () => `expected ${actual} to end with path ${expected}`,
-          pass: false,
-          actual,
-          expected,
-        };
-  },
+  toMatchPath: assertPathMatch,
+  pathToMatch: assertPathMatch,
+  toStartWithPath: assertPathStartWith,
+  pathToStartWith: assertPathStartWith,
+  toContainPath: assertPathContain,
+  pathToContain: assertPathContain,
+  toEndWithPath: assertPathEndWith,
+  pathToEndWith: assertPathEndWith,
 });
+
+function assertPathMatch(
+  actual: string,
+  expected: string,
+): SyncExpectationResult {
+  const normalizedReceived = osAgnosticPath(actual);
+  const normalizedExpected = osAgnosticPath(expected);
+
+  const pass = normalizedReceived === normalizedExpected;
+  return pass
+    ? {
+        message: () => `expected ${actual} not to match path ${expected}`,
+        pass: true,
+        actual,
+        expected,
+      }
+    : {
+        message: () => `expected ${actual} to match path ${expected}`,
+        pass: false,
+        actual,
+        expected,
+      };
+}
+
+function assertPathStartWith(
+  actual: string,
+  expected: string,
+): SyncExpectationResult {
+  const normalizedReceived = osAgnosticPath(actual);
+  const normalizedExpected = osAgnosticPath(expected);
+
+  const pass = normalizedReceived.startsWith(normalizedExpected);
+  return pass
+    ? {
+        message: () => `expected ${actual} not to start with path ${expected}`,
+        pass: true,
+        actual,
+        expected,
+      }
+    : {
+        message: () => `expected ${actual} to start with path ${expected}`,
+        pass: false,
+        actual,
+        expected,
+      };
+}
+
+function assertPathContain(
+  actual: string,
+  expected: string,
+): SyncExpectationResult {
+  const normalizedReceived = osAgnosticPath(actual);
+  const normalizedExpected = osAgnosticPath(expected);
+
+  const pass = normalizedReceived.includes(normalizedExpected);
+  return pass
+    ? {
+        message: () => `expected ${actual} not to contain path ${expected}`,
+        pass: true,
+        actual,
+        expected,
+      }
+    : {
+        message: () => `expected ${actual} to contain path ${expected}`,
+        pass: false,
+        actual,
+        expected,
+      };
+}
+
+function assertPathEndWith(
+  actual: string,
+  expected: string,
+): SyncExpectationResult {
+  const normalizedReceived = osAgnosticPath(actual);
+  const normalizedExpected = osAgnosticPath(expected);
+
+  const pass = normalizedReceived.endsWith(normalizedExpected);
+  return pass
+    ? {
+        message: () => `expected ${actual} not to end with path ${expected}`,
+        pass: true,
+        actual,
+        expected,
+      }
+    : {
+        message: () => `expected ${actual} to end with path ${expected}`,
+        pass: false,
+        actual,
+        expected,
+      };
+}
