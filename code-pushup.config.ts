01d044e4739769b2a3d7b59d1c6f631638df043b
Michael <michael.hladky@push-based.io>
2024-12-23 20:48:50 +0100

    wip

--- code-pushup.config.ts
+++ code-pushup.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { z } from 'zod';
+import {z} from 'zod';
 import {
   coverageCoreConfigNx,
   eslintCoreConfigNx,
@@ -7,9 +7,9 @@ import {
   lighthouseCoreConfig,
   typescriptPluginConfigNx,
 } from './code-pushup.preset.js';
-import type { CoreConfig } from './packages/models/src/index.js';
-import { BASIC_CHECKES } from './packages/plugin-typescript/src/lib/constants';
-import { mergeConfigs } from './packages/utils/src/index.js';
+import type {CoreConfig} from './packages/models/src/index.js';
+import {BASIC_CHECKES} from './packages/plugin-typescript/src/lib/constants';
+import {mergeConfigs} from './packages/utils/src/index.js';
 
 // load upload configuration from environment
 const envSchema = z.object({
@@ -18,7 +18,7 @@ const envSchema = z.object({
   CP_ORGANIZATION: z.string().min(1),
   CP_PROJECT: z.string().min(1),
 });
-const { data: env } = await envSchema.safeParseAsync(process.env);
+const {data: env} = await envSchema.safeParseAsync(process.env);
 
 const config: CoreConfig = {
   ...(env && {
@@ -35,12 +35,12 @@ const config: CoreConfig = {
 
 export default mergeConfigs(
   config,
-  /*await coverageCoreConfigNx(),
+  await coverageCoreConfigNx(),
   await jsPackagesCoreConfig(),
   await lighthouseCoreConfig(
     'https://github.com/code-pushup/cli?tab=readme-ov-file#code-pushup-cli/',
   ),
-   await eslintCoreConfigNx(),*/
+  await eslintCoreConfigNx(),
   await typescriptPluginConfigNx({
     tsConfigPath: 'packages/plugin-typescript/tsconfig.lib.json',
     tsCodes: [...BASIC_CHECKES],
