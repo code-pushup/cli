004cd6d847fb061aa234d94d73d9bc2f3280f21e
Alejandro <49059458+aramirezj@users.noreply.github.com>
2025-01-17 01:54:51 +0100

    Update packages/plugin-jsdocs/src/lib/runner/models.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/plugin-jsdocs/src/lib/runner/models.ts
+++ packages/plugin-jsdocs/src/lib/runner/models.ts
@@ -1,31 +1,27 @@
 import { SyntaxKind } from 'ts-morph';
 
 /** The possible coverage types for documentation analysis */
-export const SINTAX_KIND_LITERAL_STRING = {
-  CLASSES: 'classes',
-  METHODS: 'methods',
-  FUNCTIONS: 'functions',
-  INTERFACES: 'interfaces',
-  ENUMS: 'enums',
-  VARIABLES: 'variables',
-  PROPERTIES: 'properties',
-  TYPES: 'types',
-} as const;
-
 export type CoverageType =
-  (typeof SINTAX_KIND_LITERAL_STRING)[keyof typeof SINTAX_KIND_LITERAL_STRING];
+  | 'classes'
+  | 'methods'
+  | 'functions'
+  | 'interfaces'
+  | 'enums'
+  | 'variables'
+  | 'properties'
+  | 'types';
 
 /** Maps the SyntaxKind from the library ts-morph to the coverage type. */
 export const SYNTAX_COVERAGE_MAP = new Map<SyntaxKind, CoverageType>([
-  [SyntaxKind.ClassDeclaration, SINTAX_KIND_LITERAL_STRING.CLASSES],
-  [SyntaxKind.MethodDeclaration, SINTAX_KIND_LITERAL_STRING.METHODS],
-  [SyntaxKind.FunctionDeclaration, SINTAX_KIND_LITERAL_STRING.FUNCTIONS],
-  [SyntaxKind.InterfaceDeclaration, SINTAX_KIND_LITERAL_STRING.INTERFACES],
-  [SyntaxKind.EnumDeclaration, SINTAX_KIND_LITERAL_STRING.ENUMS],
-  [SyntaxKind.VariableDeclaration, SINTAX_KIND_LITERAL_STRING.VARIABLES],
-  [SyntaxKind.VariableStatement, SINTAX_KIND_LITERAL_STRING.VARIABLES],
-  [SyntaxKind.PropertyDeclaration, SINTAX_KIND_LITERAL_STRING.PROPERTIES],
-  [SyntaxKind.TypeAliasDeclaration, SINTAX_KIND_LITERAL_STRING.TYPES],
+  [SyntaxKind.ClassDeclaration, 'classes'],
+  [SyntaxKind.MethodDeclaration, 'methods'],
+  [SyntaxKind.FunctionDeclaration, 'functions'],
+  [SyntaxKind.InterfaceDeclaration, 'interfaces'],
+  [SyntaxKind.EnumDeclaration, 'enums'],
+  [SyntaxKind.VariableDeclaration, 'variables'],
+  [SyntaxKind.VariableStatement, 'variables'],
+  [SyntaxKind.PropertyDeclaration, 'properties'],
+  [SyntaxKind.TypeAliasDeclaration, 'types'],
 ]);
 
 /** The undocumented node is the node that is not documented and has the information for the report. */
