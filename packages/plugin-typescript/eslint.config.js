e19ea8e9c2703aad07dba1dc278a8be5f4a6aa77
Michael <michael.hladky@push-based.io>
2025-01-30 13:14:16 +0100

    fix(plugin-typescript): refine eslint config

--- packages/plugin-typescript/eslint.config.js
+++ packages/plugin-typescript/eslint.config.js
@@ -4,7 +4,7 @@ import baseConfig from '../../eslint.config.js';
 export default tseslint.config(
   ...baseConfig,
   {
-    files: ['**/*.ts', '!**/mocks'],
+    files: ['**/*.ts'],
     languageOptions: {
       parserOptions: {
         projectService: true,
