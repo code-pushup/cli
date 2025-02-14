f548eeaf8a47d419783c60df0ea35683426648d5
Michael <michael.hladky@push-based.io>
2025-02-14 17:25:22 +0100

    fix(plugin-typescript): fix lint, format code

--- packages/plugin-typescript/src/lib/typescript-plugin.unit.test.ts
+++ packages/plugin-typescript/src/lib/typescript-plugin.unit.test.ts
@@ -1,38 +1,40 @@
-import {expect} from 'vitest';
-import {pluginConfigSchema} from '@code-pushup/models';
-import {AUDITS, GROUPS} from './constants.js';
-import {typescriptPlugin} from './typescript-plugin.js';
-import type {TypescriptPluginOptions} from "./types";
+import { expect } from 'vitest';
+import { pluginConfigSchema } from '@code-pushup/models';
+import { AUDITS, GROUPS } from './constants.js';
+import type { TypescriptPluginOptions } from './types.js';
+import { typescriptPlugin } from './typescript-plugin.js';
 
 describe('typescriptPlugin-config-object', () => {
-    it('should create valid plugin config without options', async () => {
-        const pluginConfig = await typescriptPlugin();
+  it('should create valid plugin config without options', async () => {
+    const pluginConfig = await typescriptPlugin();
 
-        expect(() => pluginConfigSchema.parse(pluginConfig)).not.toThrow();
+    expect(() => pluginConfigSchema.parse(pluginConfig)).not.toThrow();
 
-        const {audits, groups} = pluginConfig;
-        expect(audits).toHaveLength(AUDITS.length);
-        expect(groups).toBeDefined();
-        expect(groups!).toHaveLength(GROUPS.length);
+    const { audits, groups } = pluginConfig;
+    expect(audits).toHaveLength(AUDITS.length);
+    expect(groups).toBeDefined();
+    expect(groups!).toHaveLength(GROUPS.length);
+  });
+
+  it('should create valid plugin config', async () => {
+    const pluginConfig = await typescriptPlugin({
+      tsconfig: 'mocked-away/tsconfig.json',
+      onlyAudits: ['syntax-errors', 'semantic-errors', 'configuration-errors'],
     });
 
-    it('should create valid plugin config', async () => {
-        const pluginConfig = await typescriptPlugin({
-            tsconfig: 'mocked-away/tsconfig.json',
-            onlyAudits: ['syntax-errors', 'semantic-errors', 'configuration-errors'],
-        });
+    expect(() => pluginConfigSchema.parse(pluginConfig)).not.toThrow();
 
-        expect(() => pluginConfigSchema.parse(pluginConfig)).not.toThrow();
+    const { audits, groups } = pluginConfig;
+    expect(audits).toHaveLength(3);
+    expect(groups).toBeDefined();
+    expect(groups!).toHaveLength(2);
+  });
 
-        const {audits, groups} = pluginConfig;
-        expect(audits).toHaveLength(3);
-        expect(groups).toBeDefined();
-        expect(groups!).toHaveLength(2);
-    });
-
-    it('should throw for invalid valid params', async () => {
-        await expect(() => typescriptPlugin({
-            tsconfig: 42
-        } as unknown as TypescriptPluginOptions)).rejects.toThrow(/invalid_type/);
-    });
+  it('should throw for invalid valid params', async () => {
+    await expect(() =>
+      typescriptPlugin({
+        tsconfig: 42,
+      } as unknown as TypescriptPluginOptions),
+    ).rejects.toThrow(/invalid_type/);
+  });
 });
