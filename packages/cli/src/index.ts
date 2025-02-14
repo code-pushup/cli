7a7824f249db7a604a6d8a3e76c95175f6cd05be
Vojtech Masek <vojtech@flowup.cz>
2025-02-14 11:11:27 +0100

    fix: explicitly exit process with successful code to not leave cli hanging

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,3 +4,7 @@ import { cli } from './lib/cli.js';
 
 // bootstrap Yargs, parse arguments and execute command
 await cli(hideBin(process.argv)).argv;
+
+// we need to explicitly exit with successful code otherwise we risk hanging process in certain situations
+// eslint-disable-next-line n/no-process-exit
+process.exit(0);
