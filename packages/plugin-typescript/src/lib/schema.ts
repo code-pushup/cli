b1c746627a9b5b6103624841a98c73f6d5d0f453
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2025-02-15 12:22:42 +0100

    Update packages/plugin-typescript/src/lib/schema.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/plugin-typescript/src/lib/schema.ts
+++ packages/plugin-typescript/src/lib/schema.ts
@@ -1,20 +1,27 @@
 import { z } from 'zod';
 import { AUDITS, DEFAULT_TS_CONFIG } from './constants.js';
+import type { AuditSlug } from './types.js';
 
-const auditSlugs = AUDITS.map(({ slug }) => slug) as [string, ...string[]];
+const auditSlugs = AUDITS.map(({ slug }) => slug) as [
+  AuditSlug,
+  ...AuditSlug[],
+];
 export const typescriptPluginConfigSchema = z.object({
   tsconfig: z
     .string({
-      description: 'Path to the TsConfig',
+      description: 'Path to a tsconfig file (default is tsconfig.json)',
     })
     .default(DEFAULT_TS_CONFIG),
   onlyAudits: z
     .array(z.enum(auditSlugs), {
-      description: 'Array with specific TsCodes to measure',
+      description: 'Filters TypeScript compiler errors by diagnostic codes',
     })
     .optional(),
 });
 
-export type TypescriptPluginOptions = z.infer<
+export type TypescriptPluginOptions = z.input<
+  typeof typescriptPluginConfigSchema
+>;
+export type TypescriptPluginConfig = z.infer<
   typeof typescriptPluginConfigSchema
 >;
