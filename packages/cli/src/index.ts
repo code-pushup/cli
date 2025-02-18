55704d1831911ee628ed0b9e6a284a0288410d18
Vojtech Masek <vojtech@flowup.cz>
2025-02-18 11:20:10 +0100

    fix: add toSorted polyfill as hotfix for bug in github actions runner

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,9 @@
 #! /usr/bin/env node
 import { hideBin } from 'yargs/helpers';
 import { cli } from './lib/cli.js';
+// FIXME: this is a hot fix for github action running in node 18 instead of node 20
+// eslint-disable-next-line import/no-unassigned-import
+import './lib/polyfills.js';
 
 // bootstrap Yargs, parse arguments and execute command
 await cli(hideBin(process.argv)).argv;
