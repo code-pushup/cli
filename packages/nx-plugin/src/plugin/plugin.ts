cf572798824b76aced77b61c6a736eb2b3175452
Alejandro <alejandroramirezjimenez1999@gmail.com>
2025-03-09 22:01:44 +0100

    chore: wip

--- packages/nx-plugin/src/plugin/plugin.ts
+++ packages/nx-plugin/src/plugin/plugin.ts
@@ -1,10 +1,10 @@
-import {
+import type {
   CreateNodes,
   CreateNodesContext,
   CreateNodesResult,
   CreateNodesV2,
-  createNodesFromFiles,
 } from '@nx/devkit';
+import { createNodesFromFiles } from '@nx/devkit';
 import { PROJECT_JSON_FILE_NAME } from '../internal/constants';
 import { createTargets } from './target/targets';
 import type { CreateNodesOptions } from './types';
@@ -38,9 +38,8 @@ export const createNodes: CreateNodes = [
 
 export const createNodesV2: CreateNodesV2 = [
   `**/${PROJECT_JSON_FILE_NAME}`,
-
-  async (configFiles, options, context) => {
-    return await createNodesFromFiles(
+  async (configFiles, options, context) =>
+    createNodesFromFiles(
       async (globMatchingFile, internalOptions) => {
         const parsedCreateNodesOptions = internalOptions as CreateNodesOptions;
 
@@ -60,6 +59,5 @@ export const createNodesV2: CreateNodesV2 = [
       configFiles,
       options,
       context,
-    );
-  },
+    )
 ];
