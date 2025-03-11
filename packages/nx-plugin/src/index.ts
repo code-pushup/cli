45e275721841baba31f613948f691bcc2ea03c3f
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2025-03-11 11:55:03 +0100

    fix(nx-plugin): adjust upload config handling (#937)

--- packages/nx-plugin/src/index.ts
+++ packages/nx-plugin/src/index.ts
@@ -3,16 +3,16 @@ import { createNodes } from './plugin/index.js';
 // default export for nx.json#plugins
 export default createNodes;
 
-export * from './internal/versions.js';
-export { type InitGeneratorSchema } from './generators/init/schema.js';
-export { initGenerator, initSchematic } from './generators/init/generator.js';
-export type { ConfigurationGeneratorOptions } from './generators/configuration/schema.js';
-export { configurationGenerator } from './generators/configuration/generator.js';
+export type { AutorunCommandExecutorOptions } from './executors/cli/schema.js';
+export { objectToCliArgs } from './executors/internal/cli.js';
 export { generateCodePushupConfig } from './generators/configuration/code-pushup-config.js';
-export { createNodes } from './plugin/index.js';
+export { configurationGenerator } from './generators/configuration/generator.js';
+export type { ConfigurationGeneratorOptions } from './generators/configuration/schema.js';
+export { initGenerator, initSchematic } from './generators/init/generator.js';
+export { type InitGeneratorSchema } from './generators/init/schema.js';
 export {
   executeProcess,
   type ProcessConfig,
 } from './internal/execute-process.js';
-export { objectToCliArgs } from './executors/internal/cli.js';
-export type { AutorunCommandExecutorOptions } from './executors/cli/schema.js';
+export * from './internal/versions.js';
+export { createNodes } from './plugin/index.js';
