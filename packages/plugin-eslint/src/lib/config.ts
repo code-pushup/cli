15e97b7a31a9aee676dd4a043cb50c09965c0db4
Matěj Chalk <matej.chalk@flowup.cz>
2025-07-16 11:58:38 +0200

    feat(plugin-eslint): update zod to v4

--- packages/plugin-eslint/src/lib/config.ts
+++ packages/plugin-eslint/src/lib/config.ts
@@ -1,12 +1,13 @@
 import { z } from 'zod';
 import { toArray } from '@code-pushup/utils';
 
-const patternsSchema = z.union([z.string(), z.array(z.string()).min(1)], {
-  description:
+const patternsSchema = z
+  .union([z.string(), z.array(z.string()).min(1)])
+  .describe(
     'Lint target files. May contain file paths, directory paths or glob patterns',
-});
+  );
 
-const eslintrcSchema = z.string({ description: 'Path to ESLint config file' });
+const eslintrcSchema = z.string().describe('Path to ESLint config file');
 
 const eslintTargetObjectSchema = z.object({
   eslintrc: eslintrcSchema.optional(),
@@ -34,30 +35,26 @@ export type ESLintPluginRunnerConfig = {
   slugs: string[];
 };
 
-const customGroupRulesSchema = z.union(
-  [
+const customGroupRulesSchema = z
+  .union([
     z
       .array(z.string())
       .min(1, 'Custom group rules must contain at least 1 element'),
-    z.record(z.string(), z.number()).refine(
-      schema => Object.keys(schema).length > 0,
-      () => ({
-        code: 'too_small',
-        message: 'Custom group rules must contain at least 1 element',
+    z
+      .record(z.string(), z.number())
+      .refine(schema => Object.keys(schema).length > 0, {
+        error: 'Custom group rules must contain at least 1 element',
       }),
-    ),
-  ],
-  {
-    description:
-      'Array of rule IDs with equal weights or object mapping rule IDs to specific weights',
-  },
-);
+  ])
+  .describe(
+    'Array of rule IDs with equal weights or object mapping rule IDs to specific weights',
+  );
 
 const customGroupSchema = z.object({
-  slug: z.string({ description: 'Unique group identifier' }),
-  title: z.string({ description: 'Group display title' }),
-  description: z.string({ description: 'Group metadata' }).optional(),
-  docsUrl: z.string({ description: 'Group documentation site' }).optional(),
+  slug: z.string().describe('Unique group identifier'),
+  title: z.string().describe('Group display title'),
+  description: z.string().describe('Group metadata').optional(),
+  docsUrl: z.string().describe('Group documentation site').optional(),
   rules: customGroupRulesSchema,
 });
 export type CustomGroup = z.infer<typeof customGroupSchema>;
