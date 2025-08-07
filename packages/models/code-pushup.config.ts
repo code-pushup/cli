ec2433228e2fe87a3b563e695cd7649efa01b10a
Michael <michael.hladky@push-based.io>
2025-08-07 15:25:55 +0200

    fix: revert

--- packages/models/code-pushup.config.ts
+++ packages/models/code-pushup.config.ts
@@ -9,7 +9,6 @@ import {
   typescriptPluginConfig,
 } from '../../code-pushup.preset.js';
 import type { CoreConfig } from '../../packages/models/src/index.js';
-import { mergeConfigs } from '../../packages/utils/src/index.js';
 
 const projectName = process.env.CP_PROJECT_NAME || 'models';
 
@@ -22,6 +21,29 @@ const config: CoreConfig = {
   plugins: [],
 };
 
+export default [
+  await eslintCoreConfigNx(projectName),
+  await coverageCoreConfigNx(projectName),
+  await jsPackagesCoreConfig('package.json'), // Use workspace root package.json
+  await typescriptPluginConfig({
+    tsconfig: `packages/${projectName}/tsconfig.lib.json`,
+  }),
+  jsDocsCoreConfig([
+    `packages/${projectName}/src/**\/*.ts`,
+    ...jsDocsExclusionPatterns,
+  ]),
+].reduce((acc, curr) => {
+  curr.plugins.forEach(plugin => {
+    acc.plugins.push(plugin);
+  });
+  curr.categories?.forEach(category => {
+    acc.categories?.push(category);
+  });
+  return acc;
+}, config);
+/*
+//import { mergeConfigs } from '../../packages/utils/src/index.js';
+
 export default mergeConfigs(
   config,
   await eslintCoreConfigNx(projectName),
@@ -31,7 +53,8 @@ export default mergeConfigs(
     tsconfig: `packages/${projectName}/tsconfig.lib.json`,
   }),
   jsDocsCoreConfig([
-    `packages/${projectName}/src/**/*.ts`,
+    `packages/${projectName}/src/**\/*.ts`,
     ...jsDocsExclusionPatterns,
   ]),
 );
+*/
