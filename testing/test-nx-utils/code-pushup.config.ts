da5de155ceb81615f048ab1624fbde1f7c8d70c0
Michael <michael.hladky@push-based.io>
2025-08-12 16:06:02 +0200

    chore: add code-pushup to testing targets

---
+++ testing/test-nx-utils/code-pushup.config.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import {
+  coverageCoreConfigNx,
+  eslintCoreConfigNx,
+  jsDocsCoreConfig,
+  jsDocsExclusionPatterns,
+  jsPackagesCoreConfig,
+  loadEnv,
+  typescriptPluginConfig,
+} from '../../code-pushup.preset.js';
+import type { CoreConfig } from '../../packages/models/src/index.js';
+import { mergeConfigs } from '../../packages/utils/src/index.js';
+
+const projectName = process.env.CP_PROJECT_NAME || 'test-nx-utils';
+
+const config: CoreConfig = {
+  ...(await loadEnv()),
+  persist: {
+    filename: `${projectName}-report`,
+    outputDir: `testing/${projectName}/.code-pushup`,
+  },
+  plugins: [],
+};
+
+export default mergeConfigs(
+  config,
+  await eslintCoreConfigNx(projectName),
+  await typescriptPluginConfig({
+    tsconfig: `testing/${projectName}/tsconfig.lib.json`,
+  }),
+  jsDocsCoreConfig([
+    `testing/${projectName}/src/**/*.ts`,
+    ...jsDocsExclusionPatterns,
+  ]),
+);
