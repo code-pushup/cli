822a3235aeb188f82f25e73f9e1e58d3cf343fec
John Doe <john.doe@example.com>
2025-08-13 00:39:27 +0200

    chore: add coverage to existing test targets

--- code-pushup.preset.ts
+++ code-pushup.preset.ts
@@ -184,12 +184,7 @@ export const coverageCoreConfigNx = async (
     throw new Error('coverageCoreConfigNx for single projects not implemented');
   }
   const targetNames = ['unit-test', 'int-test'];
-  const targetArgs = [
-    '-t',
-    ...targetNames,
-    '--coverage.enabled',
-    '--skipNxCache',
-  ];
+  const targetArgs = ['-t', ...targetNames];
   return {
     plugins: [
       await coveragePlugin({
