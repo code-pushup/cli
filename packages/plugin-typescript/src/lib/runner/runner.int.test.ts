e628c9c5f6b77c6411762a006b28d10c163e1288
Michael Hladky <Michael.Hladky@push-based.io>
2025-08-20 03:37:53 +0200

    test(plugin-typescript): fix path for windows

--- packages/plugin-typescript/src/lib/runner/runner.int.test.ts
+++ packages/plugin-typescript/src/lib/runner/runner.int.test.ts
@@ -1,15 +1,36 @@
 import { describe, expect } from 'vitest';
+import { osAgnosticPath } from '@code-pushup/test-utils';
 import { getAudits } from '../utils.js';
 import { createRunnerFunction } from './runner.js';
 
 describe('createRunnerFunction', () => {
   it('should create valid audit outputs when called', async () => {
-    await expect(
-      createRunnerFunction({
-        tsconfig:
-          'packages/plugin-typescript/mocks/fixtures/basic-setup/tsconfig.all-audits.json',
-        expectedAudits: getAudits(),
-      })(() => void 0),
-    ).resolves.toMatchSnapshot();
+    const runnerFunction = createRunnerFunction({
+      tsconfig:
+        'packages/plugin-typescript/mocks/fixtures/basic-setup/tsconfig.all-audits.json',
+      expectedAudits: getAudits(),
+    });
+
+    const result = await runnerFunction();
+
+    const sanitizedResult = result.map(audit => ({
+      ...audit,
+      ...(audit.details && {
+        details: {
+          ...audit.details,
+          issues: audit.details.issues?.map(issue => ({
+            ...issue,
+            ...(issue.source && {
+              source: {
+                ...issue.source,
+                file: osAgnosticPath(issue.source.file),
+              },
+            }),
+          })),
+        },
+      }),
+    }));
+
+    await expect(sanitizedResult).toMatchSnapshot();
   }, 35_000);
 });
