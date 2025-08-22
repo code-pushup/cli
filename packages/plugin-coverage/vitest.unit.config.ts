6a961c2cfa6c927a88db165380c30941b954935c
Andrii Siuta <andriisiuta@gmail.com>
2025-08-22 13:17:00 +0300

    test: improve test coverage for plugin-coverage (#1055)
    Close #1054 
    
    This pull request improve test coverage (int, unit, e2e) for
    `plugin-coverage`
    
    ---------
    
    Co-authored-by: AS <ASiuta_ext@mapal-os.com>

--- packages/plugin-coverage/vitest.unit.config.ts
+++ packages/plugin-coverage/vitest.unit.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     coverage: {
       reporter: ['text', 'lcov'],
       reportsDirectory: '../../coverage/plugin-coverage/unit-tests',
-      exclude: ['mocks/**', '**/types.ts'],
+      exclude: ['mocks/**', '**/types.ts', '**/vitest.*.config.ts'],
     },
     environment: 'node',
     include: ['src/**/*.unit.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
