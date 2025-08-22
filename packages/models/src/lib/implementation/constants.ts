ba1149a3d2560368f877da0035bd5935c2cf53da
Matěj Chalk <matej.chalk@flowup.cz>
2025-08-22 15:43:17 +0200

    feat(models): export default persist config

--- packages/models/src/lib/implementation/constants.ts
+++ packages/models/src/lib/implementation/constants.ts
@@ -1,5 +1,12 @@
-import type { Format } from '../persist-config.js';
+import type { Format, PersistConfig } from '../persist-config.js';
 
 export const DEFAULT_PERSIST_OUTPUT_DIR = '.code-pushup';
 export const DEFAULT_PERSIST_FILENAME = 'report';
 export const DEFAULT_PERSIST_FORMAT: Format[] = ['json', 'md'];
+
+export const DEFAULT_PERSIST_CONFIG: Required<PersistConfig> = {
+  outputDir: DEFAULT_PERSIST_OUTPUT_DIR,
+  filename: DEFAULT_PERSIST_FILENAME,
+  format: DEFAULT_PERSIST_FORMAT,
+  skipReports: false,
+};
