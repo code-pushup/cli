dfc15aeb5ef718be6d31cfcbc2fb0dde6b9adb0b
Matěj Chalk <34691111+matejchalk@users.noreply.github.com>
2025-08-22 19:06:36 +0200

    feat(models): export default persist.skipReports value
    Co-authored-by: Michael Hladky <10064416+BioPhoton@users.noreply.github.com>

--- packages/models/src/lib/implementation/constants.ts
+++ packages/models/src/lib/implementation/constants.ts
@@ -3,10 +3,11 @@ import type { Format, PersistConfig } from '../persist-config.js';
 export const DEFAULT_PERSIST_OUTPUT_DIR = '.code-pushup';
 export const DEFAULT_PERSIST_FILENAME = 'report';
 export const DEFAULT_PERSIST_FORMAT: Format[] = ['json', 'md'];
+export const DEFAULT_PERSIST_SKIP_REPORT = false;
 
 export const DEFAULT_PERSIST_CONFIG: Required<PersistConfig> = {
   outputDir: DEFAULT_PERSIST_OUTPUT_DIR,
   filename: DEFAULT_PERSIST_FILENAME,
   format: DEFAULT_PERSIST_FORMAT,
-  skipReports: false,
+  skipReports: DEFAULT_PERSIST_SKIP_REPORT,
 };
