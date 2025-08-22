fd5edb09ebbc2b5ad22fed1723651e6bcbdfad14
Matěj Chalk <matej.chalk@flowup.cz>
2025-08-22 17:40:39 +0200

    feat(ci): add helper function for parsing configPatterns from json string

--- packages/ci/src/index.ts
+++ packages/ci/src/index.ts
@@ -1,8 +1,10 @@
 export type { SourceFileIssue } from './lib/issues.js';
 export type * from './lib/models.js';
 export {
-  MONOREPO_TOOLS,
   isMonorepoTool,
+  MONOREPO_TOOLS,
   type MonorepoTool,
 } from './lib/monorepo/index.js';
 export { runInCI } from './lib/run.js';
+export { configPatternsSchema } from './lib/schemas.js';
+export { parseConfigPatternsFromString } from './lib/settings.js';
