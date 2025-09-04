129f690788f321490a639aeb06d0c0a993c70803
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2025-09-04 15:46:23 +0200

    Update utils.unit.test.ts
    Co-authored-by: Matěj Chalk <34691111+matejchalk@users.noreply.github.com>

--- packages/nx-plugin/src/executors/cli/utils.unit.test.ts
+++ packages/nx-plugin/src/executors/cli/utils.unit.test.ts
@@ -9,13 +9,13 @@ import {
 } from './utils.js';
 
 describe('parsePrintConfigExecutorOptions', () => {
-  it('should provide NO default projectPrefix', () => {
+  it('should provide NO default output path', () => {
     expect(parsePrintConfigExecutorOptions({})).toStrictEqual(
       expect.not.objectContaining({ output: expect.anything() }),
     );
   });
 
-  it('should process given projectPrefix', () => {
+  it('should process given output path', () => {
     expect(
       parsePrintConfigExecutorOptions({ output: 'code-pushup.config.json' }),
     ).toStrictEqual(
