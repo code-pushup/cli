7288d8ef3d4e7c547b96d7156fde449a92bf3287
Szymon.Poltorak <szymon.poltorak@push-based.io>
2025-09-09 15:09:22 +0200

    fix: path resolution

--- testing/test-setup-config/src/lib/vitest-tsconfig-path-aliases.ts
+++ testing/test-setup-config/src/lib/vitest-tsconfig-path-aliases.ts
@@ -1,9 +1,11 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { loadConfig } from 'tsconfig-paths';
 import type { Alias, AliasOptions } from 'vite';
 
 export function tsconfigPathAliases(projectRootUrl?: URL): AliasOptions {
   const tsconfigPath = projectRootUrl
-    ? new URL('tsconfig.base.json', projectRootUrl).pathname
+    ? path.resolve(fileURLToPath(projectRootUrl), 'tsconfig.base.json')
     : 'tsconfig.base.json';
   const result = loadConfig(tsconfigPath);
   if (result.resultType === 'failed') {
@@ -18,7 +20,7 @@ export function tsconfigPathAliases(projectRootUrl?: URL): AliasOptions {
       ([importPath, relativePath]): Alias => ({
         find: importPath,
         replacement: projectRootUrl
-          ? new URL(relativePath, projectRootUrl).pathname
+          ? path.resolve(fileURLToPath(projectRootUrl), relativePath)
           : new URL(`../${relativePath}`, import.meta.url).pathname,
       }),
     );
