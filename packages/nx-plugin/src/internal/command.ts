9a5eb157d70e916ece4e6ef56bee651f98389077
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2025-10-04 15:03:23 +0200

    Apply suggestion from @BioPhoton

--- packages/nx-plugin/src/internal/command.ts
+++ packages/nx-plugin/src/internal/command.ts
@@ -74,11 +74,11 @@ export interface FormatCommandLogOptions {
  * formatCommandLog({cwd: 'tools/api', env: {API_KEY='•••' NODE_ENV='prod'}, command: 'node', args: ['cli.js', '--do', 'thing', 'fast']})
  * ┌─────────────────────────────────────────────────────────────────────────┐
  * │ tools/api $ API_KEY="•••" NODE_ENV="prod" node cli.js --do thing fast   │
- * │ │          │ │                             │    │                       │
- * │ └ cwd      │ │                             │    └ args.                 │
- * │            │ │                             └ command                    │
- * │            │ └ env variables                                            │
- * │            └ prompt symbol ($)                                          │
+ * │ │         │  │                             │    │                       │
+ * │ └ cwd     │  │                             │    └ args.                 │
+ * │           │  │                             └ command                    │
+ * │           │  └ env variables                                            │
+ * │           └ prompt symbol ($)                                           │
  * └─────────────────────────────────────────────────────────────────────────┘
  */
 export function formatCommandLog(options: FormatCommandLogOptions): string {
