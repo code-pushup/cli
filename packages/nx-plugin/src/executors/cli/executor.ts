340d7000ce960813b69d19ea5016a9e0d49b2028
John Doe <john.doe@example.com>
2025-10-23 17:43:47 +0200

    refactor: adjust logic

--- packages/nx-plugin/src/executors/cli/executor.ts
+++ packages/nx-plugin/src/executors/cli/executor.ts
@@ -50,7 +50,7 @@ export default async function runAutorunExecutor(
       return {
         success: false,
         command: commandString,
-        error: error as Error,
+        error: error instanceof Error ? error : new Error(`${error}`),
       };
     }
   }
