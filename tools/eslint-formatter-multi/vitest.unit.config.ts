1377ab95e0cd3793fc8bb285f91efa29e52d5d8c
Matěj Chalk <matej.chalk@flowup.cz>
2025-10-29 16:09:28 +0100

    test: fix vitest config type errors (unknown test property)

--- tools/eslint-formatter-multi/vitest.unit.config.ts
+++ tools/eslint-formatter-multi/vitest.unit.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 // eslint-disable-next-line import/no-useless-path-segments
 import { tsconfigPathAliases } from '../../tools/vitest-tsconfig-path-aliases.js';
 
