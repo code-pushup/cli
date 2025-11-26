043eb80e425d273797541accb76210a625b0804d
Matěj Chalk <matej.chalk@flowup.cz>
2025-11-26 16:48:33 +0100

    chore: clean up code-pushup presets and prepare for reuse on project-level

--- code-pushup.preset.ts
+++ code-pushup.preset.ts
@@ -12,199 +12,83 @@ import coveragePlugin, {
 import eslintPlugin, {
   eslintConfigFromAllNxProjects,
 } from './packages/plugin-eslint/src/index.js';
-import type { ESLintTarget } from './packages/plugin-eslint/src/lib/config.js';
-import { nxProjectsToConfig } from './packages/plugin-eslint/src/lib/nx/projects-to-config.js';
 import jsPackagesPlugin from './packages/plugin-js-packages/src/index.js';
 import jsDocsPlugin from './packages/plugin-jsdocs/src/index.js';
-import type { JsDocsPluginTransformedConfig } from './packages/plugin-jsdocs/src/lib/config.js';
 import {
   PLUGIN_SLUG,
   groups,
 } from './packages/plugin-jsdocs/src/lib/constants.js';
-import { filterGroupsByOnlyAudits } from './packages/plugin-jsdocs/src/lib/utils.js';
-import lighthousePlugin, {
+import {
   lighthouseGroupRef,
+  lighthousePlugin,
   mergeLighthouseCategories,
 } from './packages/plugin-lighthouse/src/index.js';
 import typescriptPlugin, {
-  type TypescriptPluginOptions,
   getCategories,
 } from './packages/plugin-typescript/src/index.js';
 
-export const jsPackagesCategories: CategoryConfig[] = [
-  {
-    slug: 'security',
-    title: 'Security',
-    description: 'Finds known **vulnerabilities** in 3rd-party packages.',
-    refs: [
-      {
-        type: 'group',
-        plugin: 'js-packages',
-        slug: 'npm-audit',
-        weight: 1,
-      },
-    ],
-  },
-  {
-    slug: 'updates',
-    title: 'Updates',
-    description: 'Finds **outdated** 3rd-party packages.',
-    refs: [
-      {
-        type: 'group',
-        plugin: 'js-packages',
-        slug: 'npm-outdated',
-        weight: 1,
-      },
-    ],
-  },
-];
-
-export const lighthouseCategories: CategoryConfig[] = [
-  {
-    slug: 'performance',
-    title: 'Performance',
-    refs: [lighthouseGroupRef('performance')],
-  },
-  {
-    slug: 'a11y',
-    title: 'Accessibility',
-    refs: [lighthouseGroupRef('accessibility')],
-  },
-  {
-    slug: 'best-practices',
-    title: 'Best Practices',
-    refs: [lighthouseGroupRef('best-practices')],
-  },
-  {
-    slug: 'seo',
-    title: 'SEO',
-    refs: [lighthouseGroupRef('seo')],
-  },
-];
-
-export const eslintCategories: CategoryConfig[] = [
-  {
-    slug: 'bug-prevention',
-    title: 'Bug prevention',
-    description: 'Lint rules that find **potential bugs** in your code.',
-    refs: [{ type: 'group', plugin: 'eslint', slug: 'problems', weight: 1 }],
-  },
-  {
-    slug: 'code-style',
-    title: 'Code style',
-    description:
-      'Lint rules that promote **good practices** and consistency in your code.',
-    refs: [{ type: 'group', plugin: 'eslint', slug: 'suggestions', weight: 1 }],
-  },
-];
-
-export function getJsDocsCategories(
-  config: JsDocsPluginTransformedConfig,
-): CategoryConfig[] {
-  return [
-    {
-      slug: 'docs',
-      title: 'Documentation',
-      description: 'Measures how much of your code is **documented**.',
-      refs: filterGroupsByOnlyAudits(groups, config).map(group => ({
-        weight: 1,
-        type: 'group',
-        plugin: PLUGIN_SLUG,
-        slug: group.slug,
-      })),
-    },
-  ];
-}
-
-export const coverageCategories: CategoryConfig[] = [
-  {
-    slug: 'code-coverage',
-    title: 'Code coverage',
-    description: 'Measures how much of your code is **covered by tests**.',
-    refs: [
-      {
-        type: 'group',
-        plugin: 'coverage',
-        slug: 'coverage',
-        weight: 1,
-      },
-    ],
-  },
-];
-
-export const jsPackagesCoreConfig = async (): Promise<CoreConfig> => ({
-  plugins: [await jsPackagesPlugin()],
-  categories: jsPackagesCategories,
-});
-
-export const lighthouseCoreConfig = async (
-  urls: PluginUrls,
-): Promise<CoreConfig> => {
-  const lhPlugin = await lighthousePlugin(urls);
+export function configureUpload(): CoreConfig {
   return {
-    plugins: [lhPlugin],
-    categories: mergeLighthouseCategories(lhPlugin, lighthouseCategories),
+    ...(process.env['CP_API_KEY'] && {
+      upload: {
+        server: 'https://api.staging.code-pushup.dev/graphql',
+        apiKey: process.env['CP_API_KEY'],
+        organization: 'code-pushup',
+        project: process.env['NX_TASK_TARGET_PROJECT'] ?? 'cli-workspace',
+      },
+    }),
+    plugins: [],
   };
-};
-
-export const jsDocsCoreConfig = (
-  config: JsDocsPluginTransformedConfig | string[],
-): CoreConfig => ({
-  plugins: [
-    jsDocsPlugin(Array.isArray(config) ? { patterns: config } : config),
-  ],
-  categories: getJsDocsCategories(
-    Array.isArray(config) ? { patterns: config } : config,
-  ),
-});
-
-export async function eslintConfigFromPublishableNxProjects(): Promise<
-  ESLintTarget[]
-> {
-  const { createProjectGraphAsync } = await import('@nx/devkit');
-  const projectGraph = await createProjectGraphAsync({ exitOnError: false });
-  return nxProjectsToConfig(
-    projectGraph,
-    project => project.tags?.includes('publishable') ?? false,
-  );
 }
 
-export const eslintCoreConfigNx = async (
+export async function configureEslintPlugin(
   projectName?: string,
-): Promise<CoreConfig> => ({
-  plugins: [
-    projectName
-      ? await eslintPlugin(
-          {
-            eslintrc: `packages/${projectName}/eslint.config.js`,
-            patterns: ['.'],
-          },
-          {
-            artifacts: {
-              // We leverage Nx dependsOn to only run all lint targets before we run code-pushup
-              // generateArtifactsCommand: 'npx nx run-many -t lint',
-              artifactsPaths: [
-                `packages/${projectName}/.eslint/eslint-report.json`,
-              ],
+): Promise<CoreConfig> {
+  return {
+    plugins: [
+      projectName
+        ? await eslintPlugin(
+            {
+              eslintrc: `packages/${projectName}/eslint.config.js`,
+              patterns: ['.'],
             },
-          },
-        )
-      : await eslintPlugin(await eslintConfigFromAllNxProjects()),
-  ],
-  categories: eslintCategories,
-});
+            {
+              artifacts: {
+                // We leverage Nx dependsOn to only run all lint targets before we run code-pushup
+                // generateArtifactsCommand: 'npx nx run-many -t lint',
+                artifactsPaths: [
+                  `packages/${projectName}/.eslint/eslint-report.json`,
+                ],
+              },
+            },
+          )
+        : await eslintPlugin(await eslintConfigFromAllNxProjects()),
+    ],
+    categories: [
+      {
+        slug: 'bug-prevention',
+        title: 'Bug prevention',
+        description: 'Lint rules that find **potential bugs** in your code.',
+        refs: [
+          { type: 'group', plugin: 'eslint', slug: 'problems', weight: 1 },
+        ],
+      },
+      {
+        slug: 'code-style',
+        title: 'Code style',
+        description:
+          'Lint rules that promote **good practices** and consistency in your code.',
+        refs: [
+          { type: 'group', plugin: 'eslint', slug: 'suggestions', weight: 1 },
+        ],
+      },
+    ],
+  };
+}
 
-export const typescriptPluginConfig = async (
-  options?: TypescriptPluginOptions,
-): Promise<CoreConfig> => ({
-  plugins: [await typescriptPlugin(options)],
-  categories: getCategories(),
-});
-
-export const coverageCoreConfigNx = async (
+export async function configureCoveragePlugin(
   projectName?: string,
-): Promise<CoreConfig> => {
+): Promise<CoreConfig> {
   const config: CoveragePluginConfig = projectName
     ? // We do not need to run a coverageToolCommand. This is handled over the Nx task graph.
       {
@@ -223,10 +107,124 @@ export const coverageCoreConfigNx = async (
       };
   return {
     plugins: [await coveragePlugin(config)],
-    categories: coverageCategories,
+    categories: [
+      {
+        slug: 'code-coverage',
+        title: 'Code coverage',
+        description: 'Measures how much of your code is **covered by tests**.',
+        refs: [
+          { type: 'group', plugin: 'coverage', slug: 'coverage', weight: 1 },
+        ],
+      },
+    ],
   };
-};
+}
 
-export const axeCoreConfig = (urls: PluginUrls): CoreConfig => ({
-  plugins: [axePlugin(urls)],
-});
+export async function configureJsPackagesPlugin(): Promise<CoreConfig> {
+  return {
+    plugins: [await jsPackagesPlugin()],
+    categories: [
+      {
+        slug: 'security',
+        title: 'Security',
+        description: 'Finds known **vulnerabilities** in 3rd-party packages.',
+        refs: [
+          {
+            type: 'group',
+            plugin: 'js-packages',
+            slug: 'npm-audit',
+            weight: 1,
+          },
+        ],
+      },
+      {
+        slug: 'updates',
+        title: 'Updates',
+        description: 'Finds **outdated** 3rd-party packages.',
+        refs: [
+          {
+            type: 'group',
+            plugin: 'js-packages',
+            slug: 'npm-outdated',
+            weight: 1,
+          },
+        ],
+      },
+    ],
+  };
+}
+
+export function configureTypescriptPlugin(projectName?: string): CoreConfig {
+  const tsconfig = projectName
+    ? `packages/${projectName}/tsconfig.lib.json`
+    : 'tsconfig.base.json';
+  return {
+    plugins: [typescriptPlugin({ tsconfig })],
+    categories: getCategories(),
+  };
+}
+
+export function configureJsDocsPlugin(projectName?: string): CoreConfig {
+  const patterns: string[] = [
+    `packages/${projectName ?? '*'}/src/**/*.ts`,
+    `!**/node_modules`,
+    `!**/{mocks,mock}`,
+    `!**/*.{spec,test}.ts`,
+    `!**/implementation/**`,
+    `!**/internal/**`,
+  ];
+  return {
+    plugins: [jsDocsPlugin(patterns)],
+    categories: [
+      {
+        slug: 'docs',
+        title: 'Documentation',
+        description: 'Measures how much of your code is **documented**.',
+        refs: groups.map(group => ({
+          weight: 1,
+          type: 'group',
+          plugin: PLUGIN_SLUG,
+          slug: group.slug,
+        })),
+      },
+    ],
+  };
+}
+
+export async function configureLighthousePlugin(
+  urls: PluginUrls,
+): Promise<CoreConfig> {
+  const lhPlugin = await lighthousePlugin(urls);
+  const lhCategories: CategoryConfig[] = [
+    {
+      slug: 'performance',
+      title: 'Performance',
+      refs: [lighthouseGroupRef('performance')],
+    },
+    {
+      slug: 'a11y',
+      title: 'Accessibility',
+      refs: [lighthouseGroupRef('accessibility')],
+    },
+    {
+      slug: 'best-practices',
+      title: 'Best Practices',
+      refs: [lighthouseGroupRef('best-practices')],
+    },
+    {
+      slug: 'seo',
+      title: 'SEO',
+      refs: [lighthouseGroupRef('seo')],
+    },
+  ];
+  return {
+    plugins: [lhPlugin],
+    categories: mergeLighthouseCategories(lhPlugin, lhCategories),
+  };
+}
+
+export function axeCoreConfig(urls: PluginUrls): CoreConfig {
+  return {
+    plugins: [axePlugin(urls)],
+  };
+}
