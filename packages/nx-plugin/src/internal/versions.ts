521ef3075997f260f4f4ae65807a951c3165e01f
John Doe <john.doe@example.com>
2025-12-01 14:10:33 +0100

    refactor: fix file not found of for uninstalled packages

--- packages/nx-plugin/src/internal/versions.ts
+++ packages/nx-plugin/src/internal/versions.ts
@@ -5,21 +5,31 @@ import type { PackageJson } from 'nx/src/utils/package-json';
 const workspaceRoot = path.join(__dirname, '../../');
 const projectsFolder = path.join(__dirname, '../../../');
 
-export const cpNxPluginVersion = loadPackageJson(workspaceRoot).version;
-export const cpModelVersion = loadPackageJson(
+export const cpNxPluginVersion = readDependencyVersion(workspaceRoot);
+export const cpModelVersion = readDependencyVersion(
   path.join(projectsFolder, 'cli'),
-).version;
-export const cpUtilsVersion = loadPackageJson(
+);
+export const cpUtilsVersion = readDependencyVersion(
   path.join(projectsFolder, 'utils'),
-).version;
-export const cpCliVersion = loadPackageJson(
+);
+export const cpCliVersion = readDependencyVersion(
   path.join(projectsFolder, 'models'),
-).version;
+);
 
 /**
- * Load the package.json file from the given folder path.
- * @param folderPath
+ * Load the package.json file from the given folder path and returns the package version.
+ * If the version is not given of the package.json file does not exist it returns the fallback value.
  */
-function loadPackageJson(folderPath: string): PackageJson {
-  return readJsonFile<PackageJson>(path.join(folderPath, 'package.json'));
+function readDependencyVersion(
+  folderPath: string,
+  fallbackVersion = 'latest',
+): string {
+  try {
+    return (
+      readJsonFile<PackageJson>(path.join(folderPath, 'package.json'))
+        .version ?? fallbackVersion
+    );
+  } catch {
+    return fallbackVersion;
+  }
 }
