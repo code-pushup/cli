4209da87857394d1bee4c77bb6600185e50ba6a0
Matěj Chalk <matej.chalk@flowup.cz>
2025-12-01 16:38:20 +0100

    feat(ci): style top-level logs

--- packages/ci/src/lib/run-monorepo.ts
+++ packages/ci/src/lib/run-monorepo.ts
@@ -4,7 +4,6 @@ import {
   type ExcludeNullableProps,
   asyncSequential,
   hasNoNullableProps,
-  logger,
 } from '@code-pushup/utils';
 import {
   type CommandContext,
@@ -16,6 +15,7 @@ import {
   runMergeDiffs,
 } from './cli/index.js';
 import { commentOnPR } from './comment.js';
+import { logDebug, logInfo } from './log.js';
 import type {
   GitBranch,
   MonorepoRunResult,
@@ -52,7 +52,7 @@ export async function runInMonorepoMode(
 ): Promise<MonorepoRunResult> {
   const { api, settings } = env;
 
-  logger.info('Running Code PushUp in monorepo mode');
+  logInfo('Running Code PushUp in monorepo mode');
 
   const { projects, runManyCommand } = await listMonorepoProjects(settings);
   const projectResults = runManyCommand
@@ -71,7 +71,7 @@ export async function runInMonorepoMode(
     diffJsonPaths,
     createCommandContext(settings, projects[0]),
   );
-  logger.debug(`Merged ${diffJsonPaths.length} diffs into ${tmpDiffPath}`);
+  logDebug(`Merged ${diffJsonPaths.length} diffs into ${tmpDiffPath}`);
   const { md: diffPath } = await saveOutputFiles({
     project: null,
     type: 'comparison',
@@ -105,7 +105,7 @@ function runProjectsIndividually(
   projects: ProjectConfig[],
   env: RunEnv,
 ): Promise<ProjectRunResult[]> {
-  logger.info(`Running on ${projects.length} projects individually`);
+  logInfo(`Running on ${projects.length} projects individually`);
   return asyncSequential(projects, project => runOnProject(project, env));
 }
 
@@ -119,7 +119,7 @@ async function runProjectsInBulk(
     settings,
   } = env;
 
-  logger.info(
+  logInfo(
     `Running on ${projects.length} projects in bulk (parallel: ${settings.parallel})`,
   );
 
@@ -173,7 +173,7 @@ async function loadProjectEnvs(
 
   const hasFormats = allProjectsHaveDefaultPersistFormats(projectEnvs);
 
-  logger.debug(
+  logDebug(
     [
       configPatterns
         ? `Parsed ${projectEnvs.length} persist and upload configs by interpolating configPatterns option for each project.`
@@ -211,7 +211,7 @@ async function compareProjectsInBulk(
   const uncachedProjectReports = projectReportsWithCache.filter(
     ({ prevReport }) => !prevReport,
   );
-  logger.info(
+  logInfo(
     `${currProjectReports.length - uncachedProjectReports.length} out of ${currProjectReports.length} projects loaded previous report from artifact cache`,
   );
 
@@ -314,10 +314,10 @@ async function collectPreviousReports(
       .map(({ project }) => project.name)
       .filter(name => !onlyProjects.includes(name));
     if (invalidProjects.length > 0) {
-      logger.debug(
+      logDebug(
         `Printing config failed for ${invalidProjects.length} projects - ${invalidProjects.join(', ')}`,
       );
-      logger.info(
+      logInfo(
         `Skipping ${invalidProjects.length} projects which aren't configured in base branch ${base.ref}`,
       );
     }
@@ -325,7 +325,7 @@ async function collectPreviousReports(
     if (onlyProjects.length > 0) {
       const hasFormats =
         allProjectsHaveDefaultPersistFormats(validProjectConfigs);
-      logger.info(
+      logInfo(
         `Collecting previous reports for ${onlyProjects.length} projects`,
       );
       await collectMany(runManyCommand, env, { hasFormats, onlyProjects });
@@ -377,7 +377,7 @@ async function collectMany(
   const countText = onlyProjects
     ? `${onlyProjects.length} previous`
     : 'all current';
-  logger.debug(`Collected ${countText} reports using command \`${command}\``);
+  logDebug(`Collected ${countText} reports using command \`${command}\``);
 }
 
 async function compareMany(
@@ -397,7 +397,7 @@ async function compareMany(
 
   await runCompare(ctx, { hasFormats });
 
-  logger.debug('Compared all project reports');
+  logDebug('Compared all project reports');
 }
 
 export function allProjectsHaveDefaultPersistFormats(
