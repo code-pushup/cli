7d36180fa7febd19518fd591fa3855ef9147883e
John Doe <john.doe@example.com>
2025-12-02 13:07:35 +0100

    refactor: wip

--- e2e/nx-plugin-e2e/tests/plugin-create-nodes.e2e.test.ts
+++ e2e/nx-plugin-e2e/tests/plugin-create-nodes.e2e.test.ts
@@ -103,29 +103,6 @@ describe('nx-plugin', () => {
     });
   });
 
-  it('should consider plugin option bin in configuration target', async () => {
-    const cwd = path.join(testFileDir, 'configuration-option-bin');
-    registerPluginInWorkspace(tree, {
-      plugin: '@code-pushup/nx-plugin',
-      options: {
-        bin: 'XYZ',
-      },
-    });
-    await materializeTree(tree, cwd);
-
-    const { code, projectJson } = await nxShowProjectJson(cwd, project);
-
-    expect(code).toBe(0);
-
-    expect(projectJson.targets).toStrictEqual({
-      'code-pushup--configuration': expect.objectContaining({
-        options: {
-          command: `nx g XYZ:configuration --project="${project}"`,
-        },
-      }),
-    });
-  });
-
   it('should NOT add config targets dynamically if the project is configured', async () => {
     const cwd = path.join(testFileDir, 'configuration-already-configured');
     registerPluginInWorkspace(tree, '@code-pushup/nx-plugin');
@@ -199,8 +176,9 @@ describe('nx-plugin', () => {
     // Nx command
     expect(cleanStdout).toContain('nx run my-lib:code-pushup');
     // Run CLI executor
-    expect(cleanStdout).toContain('Command: npx @code-pushup/cli');
-    expect(cleanStdout).toContain('--dryRun --verbose');
+    expect(cleanStdout).toContain('Command: <CWD> $ npx @code-pushup/cli');
+    expect(cleanStdout).toContain('--verbose');
+    expect(cleanStdout).toContain('--dryRun ');
   });
 
   it('should consider plugin option bin in executor target', async () => {
