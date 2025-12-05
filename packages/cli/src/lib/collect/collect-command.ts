db44dff0d99dd3f2fc94f501fff4705e7836481a
Matěj Chalk <matej.chalk@flowup.cz>
2025-12-05 13:54:48 +0100

    feat(cli): simplify sticker hints

--- packages/cli/src/lib/collect/collect-command.ts
+++ packages/cli/src/lib/collect/collect-command.ts
@@ -1,25 +1,22 @@
-import ansis from 'ansis';
 import type { ArgumentsCamelCase, CommandModule } from 'yargs';
 import {
   type CollectAndPersistReportsOptions,
   collectAndPersistReports,
 } from '@code-pushup/core';
-import {
-  formatAsciiLink,
-  formatAsciiSticker,
-  logger,
-} from '@code-pushup/utils';
+import { logger } from '@code-pushup/utils';
 import {
   collectSuccessfulLog,
   printCliCommand,
-  renderConfigureCategoriesHint,
+  renderCategoriesHint,
+  renderPortalHint,
+  renderUploadHint,
 } from '../implementation/logging.js';
 
 export function yargsCollectCommandObject(): CommandModule {
   const command = 'collect';
   return {
     command,
-    describe: 'Run Plugins and collect results',
+    describe: 'Run plugins and collect results',
     handler: async <T>(args: ArgumentsCamelCase<T>) => {
       printCliCommand(command);
 
@@ -28,36 +25,21 @@ export function yargsCollectCommandObject(): CommandModule {
       await collectAndPersistReports(options);
       collectSuccessfulLog();
 
-      if (!options.categories || options.categories.length === 0) {
-        renderConfigureCategoriesHint();
+      if (!options.categories?.length) {
+        logger.newline();
+        renderCategoriesHint();
       }
 
-      const { upload = {} } = args as unknown as Record<
+      const { upload } = args as unknown as Record<
         'upload',
         object | undefined
       >;
-      if (Object.keys(upload).length === 0) {
-        renderUploadAutorunHint();
+      logger.newline();
+      if (upload) {
+        renderUploadHint();
+      } else {
+        renderPortalHint();
       }
     },
   } satisfies CommandModule;
-}
-
-export function renderUploadAutorunHint(): void {
-  logger.info(
-    formatAsciiSticker([
-      ansis.bold.gray('💡 Visualize your reports'),
-      '',
-      `${ansis.gray('❯')} npx code-pushup upload - ${ansis.gray(
-        'Run upload to upload the created report to the server',
-      )}`,
-      `  ${formatAsciiLink(
-        'https://github.com/code-pushup/cli/tree/main/packages/cli#upload-command',
-      )}`,
-      `${ansis.gray('❯')} npx code-pushup autorun - ${ansis.gray('Run collect & upload')}`,
-      `  ${formatAsciiLink(
-        'https://github.com/code-pushup/cli/tree/main/packages/cli#autorun-command',
-      )}`,
-    ]),
-  );
 }
