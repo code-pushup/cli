880e997c504cab83e2c438e7972e8e03bee729ce
John Doe <john.doe@example.com>
2025-12-11 18:44:14 +0100

    refactor: remove disabled rules

--- tools/zod2md-jsdocs/eslint.config.js
+++ tools/zod2md-jsdocs/eslint.config.js
@@ -8,15 +8,4 @@ module.exports = [
       '@nx/dependency-checks': 'error',
     },
   },
-  {
-    files: ['**/*.ts', '**/*.cjs', '**/*.js'],
-    rules: {
-      'import/no-commonjs': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unicorn/prefer-module': 'off',
-      'functional/immutable-data': 'off',
-      'arrow-body-style': 'off',
-    },
-  },
 ];
