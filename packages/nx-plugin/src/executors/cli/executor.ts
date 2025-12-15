38cd7f2e6dedac54a09f94f54186d10ee97c82ef
John Doe <john.doe@example.com>
2025-12-15 20:39:55 +0100

    refactor: adjust prefix logic

--- packages/nx-plugin/src/executors/cli/executor.ts
+++ packages/nx-plugin/src/executors/cli/executor.ts
@@ -48,7 +48,7 @@ export default async function runCliExecutor(
     logger.warn(`DryRun execution of: ${commandString}`);
   } else {
     try {
-      logger.debug(`Run CLI with env vars: ${loggedEnvVars}`);
+      logger.debug(`Run CLI with env vars: ${JSON.stringify(loggedEnvVars)}`);
       await executeProcess({
         command,
         args,
