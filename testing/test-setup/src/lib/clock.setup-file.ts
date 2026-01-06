0e7a06e11ee5ddb7f297fc89122b64472d689be1
John Doe <john.doe@example.com>
2026-01-06 18:10:05 +0100

    refactor: fix lint

--- testing/test-setup/src/lib/clock.setup-file.ts
+++ testing/test-setup/src/lib/clock.setup-file.ts
@@ -6,8 +6,10 @@ const MOCK_TIME_ORIGIN = 500_000;
 const dateNow = MOCK_DATE_NOW_MS;
 const performanceTimeOrigin = MOCK_TIME_ORIGIN;
 
+/* eslint-disable functional/no-let */
 let dateNowSpy: MockInstance<[], number> | undefined;
 let performanceNowSpy: MockInstance<[], number> | undefined;
+/* eslint-enable functional/no-let */
 
 beforeEach(() => {
   dateNowSpy = vi.spyOn(Date, 'now').mockReturnValue(dateNow);
--- testing/test-setup/src/lib/process.setup-file.ts
+++ testing/test-setup/src/lib/process.setup-file.ts
@@ -1,7 +1,7 @@
 import process from 'node:process';
 import { beforeEach, vi } from 'vitest';
 
-export const MOCK_PID = 10001;
+export const MOCK_PID = 10_001;
 
 beforeEach(() => {
   vi.spyOn(process, 'pid', 'get').mockReturnValue(MOCK_PID);
