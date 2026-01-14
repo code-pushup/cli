b2f4467e6aab37fb50d69c5c483c2fc6d72baf1d
Szymon Półtorak <poltorak.szymon@gmail.com>
2026-01-14 10:17:49 +0100

    refactor: remove noise files from test coverage (#1214)
    Covers part 3 of #1168 
    
    Added more files to be excluded in test coverage report
    
    ---------
    
    Co-authored-by: Szymon.Poltorak <szymon.poltorak@push-based.io>

--- packages/utils/src/lib/performance-observer.ts
+++ packages/utils/src/lib/performance-observer.ts
@@ -4,7 +4,7 @@ import {
   type PerformanceObserverEntryList,
   performance,
 } from 'node:perf_hooks';
-import type { Buffered, Encoder, Observer, Sink } from './sink-source.types.js';
+import type { Buffered, Encoder, Observer, Sink } from './sink-source.type';
 
 const OBSERVED_TYPES = ['mark', 'measure'] as const;
 type ObservedEntryType = 'mark' | 'measure';
