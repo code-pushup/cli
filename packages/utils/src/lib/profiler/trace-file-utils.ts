e0c210ea8132c88ab5168592a21f25e4b919d384
Michael Hladky <michael.hladky@push-based.io>
2026-01-16 23:21:01 +0100

    refactor: wip

--- packages/utils/src/lib/profiler/trace-file-utils.ts
+++ packages/utils/src/lib/profiler/trace-file-utils.ts
@@ -319,17 +319,17 @@ export function decodeTraceEvent({ args, ...rest }: TraceEventRaw): TraceEvent {
 
   const processedArgs = decodeDetail(args as { detail: string });
   if ('data' in args && args.data && typeof args.data === 'object') {
-    const result: TraceEvent = {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    return {
       ...rest,
       args: {
         ...processedArgs,
         data: decodeDetail(args.data as { detail: string }),
       },
-    };
-    return result;
+    } as TraceEvent;
   }
-  const result: TraceEvent = { ...rest, args: processedArgs };
-  return result;
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  return { ...rest, args: processedArgs } as TraceEvent;
 }
 
 export function encodeTraceEvent({
