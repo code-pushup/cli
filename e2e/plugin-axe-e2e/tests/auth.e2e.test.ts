1dadeb2c3f59b6f2732d7478a68e49003dc3b38c
hanna-skryl <hanna.skryl@gmail.com>
2026-01-21 13:16:19 -0500

    test(plugin-axe): add setupScript auth E2E test

--- e2e/plugin-axe-e2e/tests/auth.e2e.test.ts
+++ e2e/plugin-axe-e2e/tests/auth.e2e.test.ts
@@ -1,8 +1,7 @@
 import { cp } from 'node:fs/promises';
+import type { Server } from 'node:http';
 import path from 'node:path';
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
-import { type Report, reportSchema } from '@code-pushup/models';
-import { omitVariableReportData } from '@code-pushup/test-fixtures';
+import type { Report } from '@code-pushup/models';
 import { nxTargetProject } from '@code-pushup/test-nx-utils';
 import {
   E2E_ENVIRONMENTS_DIR,
@@ -11,32 +10,43 @@ import {
   teardownTestFolder,
 } from '@code-pushup/test-utils';
 import { executeProcess, readJsonFile } from '@code-pushup/utils';
+import {
+  createAuthServer,
+  startServer,
+  stopServer,
+} from '../mocks/fixtures/auth/server/server.js';
 
-describe('PLUGIN collect report with axe-plugin NPM package', () => {
+describe('PLUGIN collect with setupScript authentication', () => {
+  const PORT = 8080;
   const fixturesDir = path.join(
     'e2e',
     nxTargetProject(),
     'mocks',
     'fixtures',
-    'collect',
+    'auth',
   );
   const testFileDir = path.join(
     E2E_ENVIRONMENTS_DIR,
     nxTargetProject(),
     TEST_OUTPUT_DIR,
-    'collect',
+    'auth',
   );
+
+  let server: Server;
 
   beforeAll(async () => {
     await cp(fixturesDir, testFileDir, { recursive: true });
     await restoreNxIgnoredFiles(testFileDir);
+    server = createAuthServer(path.join(testFileDir, 'server'));
+    await startServer(server, PORT);
   });
 
   afterAll(async () => {
+    await stopServer(server);
     await teardownTestFolder(testFileDir);
   });
 
-  it('should run plugin over CLI and create report.json', async () => {
+  it('should analyze authenticated page using setupScript', async () => {
     const { code } = await executeProcess({
       command: 'npx',
       args: ['@code-pushup/cli', 'collect'],
@@ -49,7 +59,8 @@ describe('PLUGIN collect report with axe-plugin NPM package', () => {
       path.join(testFileDir, '.code-pushup', 'report.json'),
     );
 
-    expect(() => reportSchema.parse(report)).not.toThrow();
-    expect(omitVariableReportData(report)).toMatchSnapshot();
+    expect(report.plugins[0]!.audits).toSatisfyAny(
+      audit => audit.score < 1 && audit.slug === 'th-has-data-cells',
+    );
   });
 });
