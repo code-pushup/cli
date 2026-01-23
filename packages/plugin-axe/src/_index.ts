03ff1f26d67aedf03b75371e4b336a6a644ddee4
Michael Hladky <michael.hladky@push-based.io>
2026-01-23 08:49:03 +0100

    refactor: fix axe

---
+++ packages/plugin-axe/src/_index.ts
@@ -0,0 +1,12 @@
+export { axePlugin } from './lib/axe-plugin.js';
+
+export type { AxePluginOptions, AxePreset } from './lib/config.js';
+export type { AxeGroupSlug } from './lib/groups.js';
+
+export {
+  axeAuditRef,
+  axeAuditRefs,
+  axeGroupRef,
+  axeGroupRefs,
+} from './lib/utils.js';
+export { axeCategories } from './lib/categories.js';
--- packages/plugin-axe/src/index.ts
+++ packages/plugin-axe/src/index.ts
@@ -1,16 +1,6 @@
 import { axePlugin } from './lib/axe-plugin.js';
-import './lib/polyfills.dom';
 import './lib/polyfills.dom.js';
 
 export default axePlugin;
 
-export type { AxePluginOptions, AxePreset } from './lib/config.js';
-export type { AxeGroupSlug } from './lib/groups.js';
-
-export {
-  axeAuditRef,
-  axeAuditRefs,
-  axeGroupRef,
-  axeGroupRefs,
-} from './lib/utils.js';
-export { axeCategories } from './lib/categories.js';
+export * from './_index.js';
