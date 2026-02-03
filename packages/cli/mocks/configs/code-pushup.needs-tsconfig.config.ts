e8c45262b391b25cfd11c188af6d31aa44b55e37
John Doe <michael.hladky@push-based.io>
2026-02-03 19:21:18 +0100

    refactor: wip

--- packages/cli/mocks/configs/code-pushup.needs-tsconfig.config.ts
+++ packages/cli/mocks/configs/code-pushup.needs-tsconfig.config.ts
@@ -1,5 +1,6 @@
 // the point is to test runtime import which requires tsconfig for path aliases
 // eslint-disable-next-line import/no-unresolved
+// @ts-expect-error - test tsconfig paths missing in config
 import customPlugin from '@example/custom-plugin';
 
 const config = {
